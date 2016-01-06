bfaf47b399bf6059b2d157216b811e7f02a28652
Alex Dolski <alexd@n-ary.net>
2016-01-06 13:24:15 -0600

    Remove uiuc_* environments

--- db/seeds.rb
+++ db/seeds.rb
@@ -563,7 +563,7 @@ users[:admin] = User.create!(
     roles: [roles[:admin]],
     enabled: true)
 
-if Rails.env.development? or Rails.env.uiuc_development?
+if Rails.env.development?
   # Non-admin users
   users[:cataloger] = User.create!(
       email: 'cataloger@example.org',
@@ -579,44 +579,40 @@ if Rails.env.development? or Rails.env.uiuc_development?
       enabled: false)
 end
 
-if Rails.env.start_with?('uiuc')
+# Themes
+Theme.create!(name: 'UIUC', default: true)
 
-  # Themes
-  Theme.create!(name: 'UIUC', default: true)
+# Overwrite some default options for internal demo purposes
+option = Option.find_by_key(Option::Key::COPYRIGHT_STATEMENT)
+option.value = 'Copyright © 2015 The Board of Trustees at the '\
+'University of Illinois. All rights reserved.'
+option.save!
 
-  # Overwrite some default options for internal demo purposes
-  option = Option.find_by_key(Option::Key::COPYRIGHT_STATEMENT)
-  option.value = 'Copyright © 2015 The Board of Trustees at the '\
-  'University of Illinois. All rights reserved.'
-  option.save!
+option = Option.find_by_key(Option::Key::ORGANIZATION_NAME)
+option.value = 'University of Illinois at Urbana-Champaign Library'
+option.save!
 
-  option = Option.find_by_key(Option::Key::ORGANIZATION_NAME)
-  option.value = 'University of Illinois at Urbana-Champaign Library'
-  option.save!
+option = Option.find_by_key(Option::Key::WEBSITE_NAME)
+option.value = 'University of Illinois at Urbana-Champaign Library Digital '\
+'Image Collections'
+option.save!
 
-  option = Option.find_by_key(Option::Key::WEBSITE_NAME)
-  option.value = 'University of Illinois at Urbana-Champaign Library Digital '\
-  'Image Collections'
-  option.save!
-
-  option = Option.find_by_key(Option::Key::WEBSITE_INTRO_TEXT)
-  option.value = "The digital collections of the Library of the University of "\
-  "Illinois at Urbana-Champaign are built from the rich special collections "\
-  "of its Rare Book & Manuscript Library; Illinois History and Lincoln "\
-  "Collection, University Archives; Map Library; and Sousa Archives & Center "\
-  "for American Music, among other units.\n\n"\
-  "The collections include historic photographs; maps; prints and "\
-  "watercolors; bookplates; architectural drawings and blueprints; letters "\
-  "and other archival materials; videos; political cartoons; and "\
-  "advertisements. They cover a wide range of subject areas including "\
-  "Illinois and American history, music, theater history, and the history of "\
-  "the University of Illinois, among others. The Library’s digital "\
-  "collections provide access to some of its most unique holdings for "\
-  "teaching, learning, and research for students, scholars and the general "\
-  "public.\n\n"\
-  "The Library contributes collaboratively to local, national, and "\
-  "international digital initiatives, such as the Digital Public Library of "\
-  "America and the Biodiversity Heritage Library."
-  option.save!
-
-end
+option = Option.find_by_key(Option::Key::WEBSITE_INTRO_TEXT)
+option.value = "The digital collections of the Library of the University of "\
+"Illinois at Urbana-Champaign are built from the rich special collections "\
+"of its Rare Book & Manuscript Library; Illinois History and Lincoln "\
+"Collection, University Archives; Map Library; and Sousa Archives & Center "\
+"for American Music, among other units.\n\n"\
+"The collections include historic photographs; maps; prints and "\
+"watercolors; bookplates; architectural drawings and blueprints; letters "\
+"and other archival materials; videos; political cartoons; and "\
+"advertisements. They cover a wide range of subject areas including "\
+"Illinois and American history, music, theater history, and the history of "\
+"the University of Illinois, among others. The Library’s digital "\
+"collections provide access to some of its most unique holdings for "\
+"teaching, learning, and research for students, scholars and the general "\
+"public.\n\n"\
+"The Library contributes collaboratively to local, national, and "\
+"international digital initiatives, such as the Digital Public Library of "\
+"America and the Biodiversity Heritage Library."
+option.save!
