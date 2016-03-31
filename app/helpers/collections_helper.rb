ec6d4cf00107d44525118ffede148973c6d8066a
Alex Dolski <alexd@n-ary.net>
2016-03-31 14:32:29 -0500

    Hide collections that are published but not in DLS and have no access URL

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -8,6 +8,7 @@ module CollectionsHelper
     thumb_size = 140
     html = ''
     collections.each do |col|
+      next unless effective_collection_access_url(col)
       if col.representative_item
         img_url = item_image_url(col.representative_item, thumb_size)
       else
@@ -29,6 +30,16 @@ module CollectionsHelper
       html += '</div>'
     end
     raw(html)
+  end
+
+  def effective_collection_access_url(collection)
+    if collection.published_in_dls
+      return collection_items_path(collection)
+    elsif collection.published and collection.access_url and
+        collection.access_url.length > 0
+      return collection.access_url
+    end
+    nil
   end
 
 end
