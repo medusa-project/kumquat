b741bc77ff083977bb3c3e1703d4d1ba9ce858bd
Alex Dolski <alexd@n-ary.net>
2016-04-04 16:31:53 -0500

    Change the title field to title_sort_en_i (IMET-137)

--- app/medusa/medusa_collection.rb
+++ app/medusa/medusa_collection.rb
@@ -8,7 +8,7 @@ class MedusaCollection < Entity
     PUBLISHED_IN_DLS = 'published_in_dls_bi'
     REPRESENTATIVE_IMAGE = 'representative_image_si'
     REPRESENTATIVE_ITEM = 'representative_item_si'
-    TITLE = 'title_txt_en_i'
+    TITLE = 'title_sort_en_i'
   end
 
   # @!attribute access_url
