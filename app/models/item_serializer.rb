cdbb3dc06e13baa655a7bf474123c76cb74c51b8
Alex Dolski <alexd@n-ary.net>
2016-04-06 10:16:19 -0500

    Add collection, parent_item, representative_item (IMET-130)

--- app/models/item_serializer.rb
+++ app/models/item_serializer.rb
@@ -5,12 +5,18 @@ class ItemSerializer < ActiveModel::Serializer
   root false
   attributes :id, :bytestreams, :metadata, :page_number, :subpage_number,
              :subclass, :full_text, :date, :created, :last_indexed,
-             :last_modified, :parent_url
+             :last_modified, :collection, :representative_item, :parent_item
 
-  # TODO: parent URL, collection URL, representative item URL
-=begin
-  def parent_url
-    object.parent ? object.parent.url : nil
+  def collection
+    collection_url(object.collection, only_path: true)
   end
-=end
+
+  def parent_item
+    object.parent ? item_url(object.parent, only_path: true) : nil
+  end
+
+  def representative_item
+    item_url(object.representative_item, only_path: true)
+  end
+
 end
