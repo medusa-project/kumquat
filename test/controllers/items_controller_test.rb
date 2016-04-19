a8fd9e2157eb200ddeb84152baf96f6e6dbebbdc
Alex Dolski <alexd@n-ary.net>
2016-04-19 13:24:01 -0500

    Change into an integration test

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
-# TODO: try this: http://stackoverflow.com/questions/22673544/rails-integration-test-headers-not-being-set
-# otherwise try this: https://github.com/brynary/rack-test
-class ItemsControllerTest < ActionController::TestCase
+class ItemsControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @valid_xml = File.read(__dir__ + '/../fixtures/repository/image/item_1.xml')
@@ -11,77 +9,69 @@ class ItemsControllerTest < ActionController::TestCase
   # create()
 
   test 'create() with no credentials should return 401' do
-    post :create
+    post('/items?version=1', @valid_xml)
     assert_response :unauthorized
   end
 
   test 'create() with invalid credentials should return 401' do
-    send_invalid_credentials
-    post :create
+    headers = valid_headers.merge(
+        'Authorization' => ActionController::HttpAuthentication::Basic.
+            encode_credentials('bogus', 'bogus'))
+    post('/items?version=1', @valid_xml, headers)
     assert_response :unauthorized
   end
 
-  test 'create() with valid credentials but invalid content-type should return 415' do
-    send_valid_credentials
-    request.env['CONTENT_TYPE'] = 'text/plain'
-    post :create
+  test 'create() with invalid content-type should return 415' do
+    headers = valid_headers.merge('Content-Type' => 'text/plain')
+    post('/items?version=1', @valid_xml, headers)
     assert_response :unsupported_media_type
   end
 
-  test 'create() with valid credentials and content-type but missing body should return 400' do
-    send_valid_credentials
-    request.env['CONTENT_TYPE'] = 'application/xml'
-    post :create
+  test 'create() with missing body should return 400' do
+    post('/items?version=1', nil, valid_headers)
     assert_response :bad_request
   end
 
-  test 'create() with valid credentials and content-type but invalid body should return 400' do
-    send_valid_credentials
-    request.env['CONTENT_TYPE'] = 'application/xml'
-    post :create, '<?xml version="1.0"><bla></bla>'
+  test 'create() with invalid body should return 400' do
+    post('/items?version=1', '<?xml version="1.0"><bla></bla>', valid_headers)
+    assert_response :bad_request
+  end
+
+  test 'create() with invalid version should return 400' do
+    post('/items', @valid_xml, valid_headers)
+    assert_response :bad_request
+    post('/items?version=8', @valid_xml, valid_headers)
     assert_response :bad_request
   end
 
   test 'create() with valid credentials, content-type, and body should return 201' do
-    send_valid_credentials
-    request.env['CONTENT_TYPE'] = 'application/xml'
-    post :create, @valid_xml
+    post('/items?version=1', @valid_xml, valid_headers)
     assert_response :created
   end
 
   test 'create() with valid credentials and a new item should create the item' do
     assert_nil Item.find_by_repository_id('800379272_de0817d41a_tiff')
-
-    send_valid_credentials
-    request.env['CONTENT_TYPE'] = 'application/xml'
-    post :create, @valid_xml
-
+    post('/items?version=1', @valid_xml, valid_headers)
     assert_not_nil Item.find_by_repository_id('800379272_de0817d41a_tiff')
   end
 
   test 'create() with valid credentials and an existing item should update the item' do
     2.times do
-      send_valid_credentials
-      request.env['CONTENT_TYPE'] = 'application/xml'
-      post :create, @valid_xml
+      post('/items?version=1', @valid_xml, valid_headers)
     end
     assert_equal 1, Item.where(repository_id: '800379272_de0817d41a_tiff').count
   end
 
   private
 
-  def send_credentials(username, secret)
-    request.env['HTTP_AUTHORIZATION'] =
-        ActionController::HttpAuthentication::Basic.encode_credentials(username, secret)
-  end
-
-  def send_invalid_credentials
-    send_credentials('bogus', 'bogus')
-  end
-
-  def send_valid_credentials
+  def valid_headers
     config = PearTree::Application.peartree_config
-    send_credentials(config[:api_user], config[:api_secret])
+    creds = ActionController::HttpAuthentication::Basic.encode_credentials(
+        config[:api_user], config[:api_secret])
+    {
+        'Authorization' => creds,
+        'Content-Type' => 'application/xml'
+    }
   end
 
 end
