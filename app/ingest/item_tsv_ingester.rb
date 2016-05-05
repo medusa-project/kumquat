b5c76ff120f6af2f31865e31f65afb81ee1a45b0
Alex Dolski <alexd@n-ary.net>
2016-05-05 14:47:57 -0500

    ingest_tsv can take a Task argument (IMET-150)

--- app/ingest/item_tsv_ingester.rb
+++ app/ingest/item_tsv_ingester.rb
@@ -3,27 +3,32 @@ require 'csv'
 class ItemTsvIngester
 
   ##
-  # Creates or updates items from the given XML document.
+  # Creates or updates items from the given TSV string.
   #
   # @param tsv [String] TSV body string
+  # @param task [Task] Optional
   # @return [Integer] Number of items ingested
   #
-  def ingest_tsv(tsv)
+  def ingest_tsv(tsv, task = nil)
+    raise 'No TSV content specified.' unless tsv.present?
     # quote_char needs to be a character that the source data is guaranteed
     # not to contain: in this case, a unicode rocket ship.
     tsv = CSV.parse(tsv, headers: true, col_sep: "\t", quote_char: '🚀')
+    total_count = tsv.length
     count = 0
-    tsv.each do |row|
-      row_hash = row.to_hash
-      puts row_hash
-      item = Item.find_by_repository_id(row_hash['repositoryId'])
+    tsv.map{ |row| row.to_hash }.select{ |row| row['repositoryId'].present? }.each do |row|
+      item = Item.find_by_repository_id(row['repositoryId'])
       if item
-        item.update_from_tsv(row_hash)
+        item.update_from_tsv(row)
       else
-        Item.from_tsv(row_hash)
+        Item.from_tsv(row)
       end
       count += 1
+
+      task.progress = count / total_count.to_f if task
     end
+
+    task.succeeded if task
     count
   end
 
