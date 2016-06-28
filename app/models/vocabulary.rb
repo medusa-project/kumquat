e8d00eb4052908f0d664a9b05796784a2b52ea4c
Alex Dolski <alexd@n-ary.net>
2016-06-28 11:52:17 -0500

    Restrict vocabulary key to alphanumerics (IMET-155)

--- app/models/vocabulary.rb
+++ app/models/vocabulary.rb
@@ -2,7 +2,7 @@ class Vocabulary < ActiveRecord::Base
 
   has_and_belongs_to_many :element_defs
 
-  validates :key, presence: true, length: { maximum: 10 },
+  validates :key, presence: true, format: { with: /\A[-a-zA-Z0-9]+\Z/ },
             uniqueness: { case_sensitive: false }
   validates :name, presence: true, length: { maximum: 100 },
             uniqueness: { case_sensitive: false }
