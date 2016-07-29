48f8dafb86791a7885b292ecd787ba64ba40d2c4
Alex Dolski <alexd@n-ary.net>
2016-07-29 15:17:06 -0500

    Add a breadcrumb helper

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -87,4 +87,78 @@ module AdminHelper
     end
   end
 
+  ##
+  # @param options [Hash]
+  # @option options [Collection] :collection
+  # @option options [Item] :item
+  # @return [String]
+  #
+  def admin_breadcrumb(options = {})
+    case controller_name
+      when 'collections'
+        case action_name
+          when 'index'
+            return admin_collections_view_breadcrumb
+          when 'show'
+            return admin_collection_view_breadcrumb(options[:collection])
+        end
+      when 'items'
+        case action_name
+          when 'index'
+            return admin_results_breadcrumb(options[:collection])
+          when 'show'
+            return admin_item_view_breadcrumb(options[:item])
+        end
+    end
+    nil
+  end
+
+  def admin_collection_view_breadcrumb(collection)
+    html = "<ol class=\"breadcrumb\">"\
+      "<li>#{link_to 'Home', admin_root_path}</li>"\
+      "<li>#{link_to 'Collections', admin_collections_path}</li>"\
+      "<li class=\"active\">#{truncate(collection.title, length: 50)}</li>"\
+    "</ol>"
+    raw(html)
+  end
+
+  def admin_collections_view_breadcrumb
+    html = "<ol class=\"breadcrumb\">"\
+      "<li>#{link_to 'Home', admin_root_path}</li>"\
+      "<li class=\"active\">#{link_to 'Collections', admin_collections_path}</li>"\
+    "</ol>"
+    raw(html)
+  end
+
+  def admin_item_structure_breadcrumb(item)
+    html = ''
+    parent = item.parent
+    while parent
+      html = "<li>#{link_to parent.title, parent}</li>#{html}"
+      parent = parent.parent
+    end
+    html += "<li class=\"active\">#{truncate(item.title, length: 50)}</li>"
+    html
+  end
+
+  def admin_item_view_breadcrumb(item)
+    html = "<ol class=\"breadcrumb\">"
+    html += "<li>#{link_to 'Home', admin_root_path}</li>"
+    html += "<li>#{link_to 'Collections', admin_collections_path}</li>"
+    html += "<li>#{link_to item.collection.title, admin_collection_path(item.collection)}</li>"
+    html += admin_item_structure_breadcrumb(item)
+    html += "</ol>"
+    raw(html)
+  end
+
+  def admin_results_breadcrumb(collection)
+    html = "<ol class=\"breadcrumb\">"\
+      "<li>#{link_to('Home', admin_root_path)}</li>"\
+      "<li>#{link_to('Collections', admin_collections_path)}</li>"\
+      "<li>#{link_to(truncate(collection.title, length: 50), admin_collection_path(collection))}</li>"\
+      "<li class=\"active\">Items</li>"\
+    "</ol>"
+    raw(html)
+  end
+
 end
