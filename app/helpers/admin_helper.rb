18f42c32ab5504ddee21aa1e43ed49a2cb7ebb85
Alex Dolski <alexd@n-ary.net>
2016-07-29 14:51:00 -0500

    Work on item view

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -23,6 +23,55 @@ module AdminHelper
     raw(html)
   end
 
+  ##
+  # @param item [Item]
+  #
+  def admin_structure_of_item(item)
+    # 1. Build the item structure excluding parents
+    html = '<ul>'
+    html += "  <li><strong>#{icon_for(item)} #{item.title}</strong>"
+    if item.items.any?
+      html += '  <ul>'
+      item.items.each do |child|
+        link = link_to(child.title,
+                       admin_collection_item_path(child.collection, child))
+        html += "  <li>#{icon_for(child)} #{link}</li>"
+      end
+      html += '  </ul>'
+    end
+    html += '  </li>'
+    html += '</ul>'
+
+    # 2. Add the item context around the item tree
+    def add_parents(item, html)
+      parent = item.parent
+      phtml = html
+      if parent
+        phtml = '<ul>'
+        link = link_to(parent.title,
+                       admin_collection_item_path(parent.collection, parent))
+        phtml += "  <li>#{icon_for(parent)} #{link}"
+        phtml +=      html
+        phtml += '  </li>'
+        phtml += '</ul>'
+        phtml = add_parents(parent, phtml)
+      end
+      phtml
+    end
+    html = add_parents(item, html)
+
+    # 3. Add the collection context around the items
+    chtml = '<ul>'
+    link = link_to(item.collection.title,
+                   admin_collection_path(item.collection))
+    chtml += "  <li>#{icon_for(item.collection)} #{link}"
+    chtml +=      html
+    chtml += '  </li>'
+    chtml += '</ul>'
+
+    raw(chtml)
+  end
+
   def bootstrap_class_for_task_status(status)
     case status
       when ::Task::Status::SUCCEEDED
