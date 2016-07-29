medusa-project/kumquat

commit 747fb2b235c8adfd31b25b7b4a26d9d2e85d5013

Add item edit view (IMET-205)

app/assets/javascripts/admin/items.js
@@ -1,30 +1,47 @@
 /**
  * @constructor
  */
-var PTAdminItemsView = function() {
-
-    var ELEMENT_LIMIT = 4;
+var PTAdminItemEditView = function() {
 
     this.init = function() {
         $('button.pt-add-element').on('click', function() {
-            // limit to ELEMENT_LIMIT fields
-            if ($('.pt-elements .form-group').length < ELEMENT_LIMIT) {
-                var clone = $(this).prev('.form-group').clone(true);
-                $(this).before(clone);
-            }
+            var element = $(this).closest('.pt-element');
+
+            var clone = element.clone(true);
+            clone.find('input').val('');
+
+            element.after(clone);
+
+            return false;
         });
         $('button.pt-remove-element').on('click', function() {
-            if ($('.pt-elements .form-group').length > 1) {
-                $(this).closest('.form-group').remove();
+            var element = $(this).closest('.pt-element');
+            if (element.siblings().length > 0) {
+                element.remove();
             }
+            return false;
+        });
+
+        // Auto-vertical-resize the textareas...
+        var textareas = $('textarea');
+        var MAGIC_FUDGE = 12;
+        // ... initially
+        textareas.each(function() {
+            $(this).height('0px');
+            $(this).height((this.scrollHeight - MAGIC_FUDGE) + 'px');
+        });
+        // ... and on change
+        textareas.on('input propertychange keyup change', function() {
+            $(this).height('20px');
+            $(this).height((this.scrollHeight - MAGIC_FUDGE) + 'px');
         });
     };
 
 };
 
 var ready = function() {
-    if ($('body#items_index').length) {
-        PearTree.view = new PTAdminItemsView();
+    if ($('body#items_edit').length) {
+        PearTree.view = new PTAdminItemEditView();
         PearTree.view.init();
     }
 };

app/assets/stylesheets/admin/items.scss
@@ -190,3 +190,10 @@ body#items_show {
   }
 
 }
+
+body#items_edit {
+  #item_full_text {
+    font-family: monospace;
+    min-height: 200px;
+  }
+}

app/controllers/admin/items_controller.rb
@@ -3,6 +3,14 @@ module Admin
   class ItemsController < ControlPanelController
 
     ##
+    # Responds to GET /admin/collections/:collection_id/items/:id
+    #
+    def edit
+      @item = Item.find_by_repository_id(params[:id])
+      raise ActiveRecord::RecordNotFound unless @item
+    end
+
+    ##
     # Responds to GET /admin/collections/:collection_id/items
     #
     def index
@@ -154,6 +162,43 @@ module Admin
         'may take a while.'
         redirect_to admin_collection_items_url(col)
       end
+    end
+
+    def update
+      begin
+        item = Item.find_by_repository_id(params[:id])
+        raise ActiveRecord::RecordNotFound unless item
+
+        # If we are updating metadata, we will need to process the elements
+        # manually.
+        if params[:elements].respond_to?(:each)
+          ActiveRecord::Base.transaction do
+            item.elements.destroy_all
+            params[:elements].each do |name, values|
+              values.each do |value|
+                item.elements.build(name: name, value: value)
+              end
+            end
+            item.save!
+          end
+        end
+
+        if params[:item]
+          item.update!(sanitized_params)
+        end
+      rescue => e
+        flash['error'] = "#{e}"
+        redirect_to edit_admin_collection_item_path(item.collection, item)
+      else
+        flash['success'] = "Item \"#{item.title}\" updated."
+        redirect_to edit_admin_collection_item_path(item.collection, item)
+      end
+    end
+
+    private
+
+    def sanitized_params
+      params.require(:item).permit(:id, :full_text)
     end
 
   end

app/helpers/admin_helper.rb
@@ -1,5 +1,28 @@
 module AdminHelper
 
+  def admin_item_element_edit_tag(element_name, element_value)
+    html = '<table class="table-condensed pt-element" style="width:100%">
+      <tr>
+        <td>'
+    html += text_area_tag("elements[#{element_name}][]", element_value,
+                          id: "elements[#{element_name}]",
+                          class: 'form-control')
+    html += '</td>
+             <td style="width: 90px">
+               <div class="btn-group">
+                 <button class="btn btn-sm btn-default pt-add-element">
+                   <i class="fa fa-plus"></i>
+                 </button>
+                 <button class="btn btn-sm btn-danger pt-remove-element">
+                   <i class="fa fa-minus"></i>
+                 </button>
+               </div>
+             </td>
+           </tr>
+         </table>'
+    raw(html)
+  end
+
   def bootstrap_class_for_task_status(status)
     case status
       when ::Task::Status::SUCCEEDED

app/views/admin/items/_form.html.erb
@@ -1,0 +1,46 @@
+<ul class="nav nav-tabs" role="tablist">
+  <li role="presentation" class="active">
+    <a href="#pt-metadata" aria-controls="pt-metadata" role="tab" data-toggle="tab">Metadata</a>
+  </li>
+  <li role="presentation">
+    <a href="#pt-full-text" aria-controls="pt-full-text" role="tab" data-toggle="tab">Full Text</a>
+  </li>
+</ul>
+
+<div class="tab-content">
+  <div role="tabpanel" class="tab-pane active" id="pt-metadata">
+    <%= form_for([:admin, @item.collection, @item]) do |f| %>
+      <table class="table pt-elements">
+        <tbody>
+          <% @item.collection.metadata_profile.element_defs.each do |e_def| %>
+            <tr>
+              <td style="width:20%">
+                <label><%= e_def.label %></label>
+              </td>
+              <td>
+                <% elements = @item.elements.select{ |e| e.name == e_def.name} %>
+                <% if elements.any? %>
+                  <% elements.each do |elem| %>
+                    <%= admin_item_element_edit_tag(e_def.name, elem.value) %>
+                  <% end %>
+                <% else %>
+                  <%= admin_item_element_edit_tag(e_def.name, '') %>
+                <% end %>
+              </td>
+            </tr>
+          <% end %>
+        </tbody>
+      </table>
+      <%= f.submit 'Save Metadata', class: 'btn btn-primary' %>
+    <% end %>
+  </div>
+
+  <div role="tabpanel" class="tab-pane" id="pt-full-text">
+    <%= form_for([:admin, @item.collection, @item]) do |f| %>
+      <div class="form-group">
+        <%= f.text_area :full_text, class: 'form-control' %>
+      </div>
+      <%= f.submit 'Save Full Text', class: 'btn btn-primary' %>
+    <% end %>
+  </div>
+</div>

app/views/admin/items/edit.html.erb
@@ -1,0 +1,15 @@
+<% provide :body_id, 'items_edit' %>
+<% provide :active_nav, 'entities' %>
+
+<ol class="breadcrumb">
+  <li><%= link_to 'Home', admin_root_path %></li>
+  <li><%= link_to 'Collections', admin_collections_path %></li>
+  <li><%= link_to @item.collection.title, admin_collection_path(@item.collection) %></li>
+  <li><%= link_to truncate(@item.title, length: 50),
+                  admin_collection_item_path(@item.collection, @item) %></li>
+  <li class="active">Edit</li>
+</ol>
+
+<h1>Edit <%= @item.title %></h1>
+
+<%= render partial: 'form', locals: { item: @item, context: :edit } %>

app/views/admin/items/show.html.erb
@@ -11,6 +11,10 @@
 </ol>
 
 <div class="btn-group pull-right">
+  <%= link_to edit_admin_collection_item_path(@item.collection, @item),
+              class: 'btn btn-default' do %>
+    <i class="fa fa-pencil"></i> Edit
+  <% end %>
   <%= link_to @item, class: 'btn btn-default', target: '_blank' do %>
     <i class="fa fa-globe"></i> Public View
   <% end %>

app/views/admin/items/update.js.erb
@@ -1,0 +1,1 @@
+window.location.reload();

commit ad9243e62392667bb8e9b05ed571afec52e91ae9

Fix a regression from 747fb2b235c8adfd31b25b7b4a26d9d2e85d5013

app/assets/javascripts/admin/items.js
@@ -39,9 +39,37 @@ var PTAdminItemEditView = function() {
 
 };
 
+/**
+ * @constructor
+ */
+var PTAdminItemsView = function() {
+
+    var ELEMENT_LIMIT = 4;
+
+    this.init = function() {
+        $('button.pt-add-element').on('click', function() {
+            // limit to ELEMENT_LIMIT fields
+            if ($('.pt-elements .form-group').length < ELEMENT_LIMIT) {
+                var clone = $(this).prev('.form-group').clone(true);
+                $(this).before(clone);
+            }
+        });
+        $('button.pt-remove-element').on('click', function() {
+            if ($('.pt-elements .form-group').length > 1) {
+                $(this).closest('.form-group').remove();
+            }
+        });
+    };
+
+};
+
 var ready = function() {
     if ($('body#items_edit').length) {
         PearTree.view = new PTAdminItemEditView();
+        PearTree.view.init();
+    }
+    if ($('body#items_index').length) {
+        PearTree.view = new PTAdminItemsView();
         PearTree.view.init();
     }
 };
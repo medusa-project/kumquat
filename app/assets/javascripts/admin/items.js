ad9243e62392667bb8e9b05ed571afec52e91ae9
Alex Dolski <alexd@n-ary.net>
2016-07-29 16:11:54 -0500

    Fix a regression from 747fb2b235c8adfd31b25b7b4a26d9d2e85d5013

--- app/assets/javascripts/admin/items.js
+++ app/assets/javascripts/admin/items.js
@@ -39,9 +39,37 @@ var PTAdminItemEditView = function() {
 
 };
 
+/**
+ * @constructor
+ */
+var PTAdminItemsView = function() {
+
+    var ELEMENT_LIMIT = 4;
+
+    this.init = function() {
+        $('button.pt-add-element').on('click', function() {
+            // limit to ELEMENT_LIMIT fields
+            if ($('.pt-elements .form-group').length < ELEMENT_LIMIT) {
+                var clone = $(this).prev('.form-group').clone(true);
+                $(this).before(clone);
+            }
+        });
+        $('button.pt-remove-element').on('click', function() {
+            if ($('.pt-elements .form-group').length > 1) {
+                $(this).closest('.form-group').remove();
+            }
+        });
+    };
+
+};
+
 var ready = function() {
     if ($('body#items_edit').length) {
         PearTree.view = new PTAdminItemEditView();
+        PearTree.view.init();
+    }
+    if ($('body#items_index').length) {
+        PearTree.view = new PTAdminItemsView();
         PearTree.view.init();
     }
 };
