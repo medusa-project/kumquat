56462c556e62a79d831f6cfc29708ee7064c5ccf
Alex Dolski <alexd@n-ary.net>
2016-08-04 13:48:11 -0500

    Test items_to_tsv

--- test/models/collection_test.rb
+++ test/models/collection_test.rb
@@ -7,6 +7,18 @@ class CollectionTest < ActiveSupport::TestCase
     assert @col.valid?
   end
 
+  test 'items_to_tsv should work' do
+    expected = "uuid\tparentId\tpreservationMasterPathname\taccessMasterPathname\tvariant\tpageNumber\tsubpageNumber\tlatitude\tlongitude\ttitle\tdescription\tlcsh:subject\ttgm:subject
+6e406030-5ce3-0132-3334-0050569601ca-3\ta53add10-5ca8-0132-3334-0050569601ca-7\t\t\tFile\t\t\t\t\t\t\t\t
+d29950d0-c451-0133-1d17-0050569601ca-2\tbe8d3500-c451-0133-1d17-0050569601ca-9\tMyString\tMyString\t\t\t\t\t\t\t\t\t
+d29edba0-c451-0133-1d17-0050569601ca-c\tbe8d3500-c451-0133-1d17-0050569601ca-9\t\t\t\t\t\t\t\t\t\t\t
+a1234567-5ca8-0132-3334-0050569601ca-8\t\t/Volumes/Data/alexd/Projects/PearTree/test/fixtures/images/jpg-iptc.jpg\tMyString\t\t\t\t39.2524300\t-152.2342300\t\t\tCats\tMore cats
+be8d3500-c451-0133-1d17-0050569601ca-9\t\t\t\t\t\t\t\t\t\t\t\t
+a53add10-5ca8-0132-3334-0050569601ca-7\t\t\t\tDirectory\t\t\t\t\t\t\t\t
+cd2d4601-c451-0133-1d17-0050569601ca-8\t\t\t\t\t\t\t\t\t\t\t\t\n"
+    assert_equal expected, @col.items_to_tsv
+  end
+
   test 'medusa_cfs_directory_id must be a UUID' do
     @col.medusa_cfs_directory_id = 123
     assert !@col.valid?
