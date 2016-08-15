75cc39c319d0e6a21e88b389e8f39db478002d88
Alex Dolski <alexd@n-ary.net>
2016-08-15 12:42:56 -0500

    index() respects collection published status in item searches

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -6,25 +6,44 @@ class LandingController < WebsiteController
   def index
     # Get a random image item to show
     image_media_types = %w(image/jp2 image/jpeg image/png image/tiff).join(' OR ')
-    @random_item = Item.solr.where(Item::SolrFields::PUBLISHED => true).
+    @random_item = Item.solr.
+        where(Item::SolrFields::PUBLISHED => true).
+        where(Item::SolrFields::COLLECTION_PUBLISHED => true).
         filter(Item::SolrFields::ACCESS_MASTER_MEDIA_TYPE => "(#{image_media_types})").
         facet(false).order(:random).limit(1).first
 
     # Get some counts for the statistics table
-    @num_all_items = Item.solr.all.count
-    @num_top_level_items = Item.solr.where(Item::SolrFields::PARENT_ITEM => :null).count
+    @num_all_items = Item.solr.
+        where(Item::SolrFields::PUBLISHED => true).
+        where(Item::SolrFields::COLLECTION_PUBLISHED => true).count
+    @num_top_level_items = Item.solr.
+        where(Item::SolrFields::PUBLISHED => true).
+        where(Item::SolrFields::COLLECTION_PUBLISHED => true).
+        where(Item::SolrFields::PARENT_ITEM => :null).count
 
-    @num_audio_items = Item.solr.where("#{Item::SolrFields::ACCESS_MASTER_MEDIA_TYPE}:audio/* "\
-    "OR #{Item::SolrFields::PRESERVATION_MASTER_MEDIA_TYPE}:audio/*").count
+    @num_audio_items = Item.solr.
+        where(Item::SolrFields::PUBLISHED => true).
+        where(Item::SolrFields::COLLECTION_PUBLISHED => true).
+        where("#{Item::SolrFields::ACCESS_MASTER_MEDIA_TYPE}:audio/* "\
+        "OR #{Item::SolrFields::PRESERVATION_MASTER_MEDIA_TYPE}:audio/*").count
 
     doc_media_types = %w(application/pdf text/plain).join(' OR ')
-    @num_document_items = Item.solr.where("#{Item::SolrFields::ACCESS_MASTER_MEDIA_TYPE}:(#{doc_media_types}) "\
-    "OR #{Item::SolrFields::PRESERVATION_MASTER_MEDIA_TYPE}:(#{doc_media_types})").count
+    @num_document_items = Item.solr.
+        where(Item::SolrFields::PUBLISHED => true).
+        where(Item::SolrFields::COLLECTION_PUBLISHED => true).
+        where("#{Item::SolrFields::ACCESS_MASTER_MEDIA_TYPE}:(#{doc_media_types}) "\
+        "OR #{Item::SolrFields::PRESERVATION_MASTER_MEDIA_TYPE}:(#{doc_media_types})").count
 
-    @num_image_items = Item.solr.where("#{Item::SolrFields::ACCESS_MASTER_MEDIA_TYPE}:image/* "\
-    "OR #{Item::SolrFields::PRESERVATION_MASTER_MEDIA_TYPE}:image/*").count
+    @num_image_items = Item.solr.
+        where(Item::SolrFields::PUBLISHED => true).
+        where(Item::SolrFields::COLLECTION_PUBLISHED => true).
+        where("#{Item::SolrFields::ACCESS_MASTER_MEDIA_TYPE}:image/* "\
+        "OR #{Item::SolrFields::PRESERVATION_MASTER_MEDIA_TYPE}:image/*").count
 
-    @num_video_items = Item.solr.where("#{Item::SolrFields::ACCESS_MASTER_MEDIA_TYPE}:video/* "\
+    @num_video_items = Item.solr.
+        where(Item::SolrFields::PUBLISHED => true).
+        where(Item::SolrFields::COLLECTION_PUBLISHED => true).
+        where("#{Item::SolrFields::ACCESS_MASTER_MEDIA_TYPE}:video/* "\
         "OR #{Item::SolrFields::PRESERVATION_MASTER_MEDIA_TYPE}:video/*").count
 
     fresh_when(etag: @num_all_items) if Rails.env.production?
