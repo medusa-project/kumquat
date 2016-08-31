1401b242f95638ecd4eee4ada0b883a847ec543d
Alex Dolski <alexd@n-ary.net>
2016-08-31 15:07:44 -0500

    Disallow multiple ElementDefs with the same name in a profile

--- app/metadata/element_def.rb
+++ app/metadata/element_def.rb
@@ -7,6 +7,8 @@ class ElementDef < ActiveRecord::Base
   belongs_to :metadata_profile, inverse_of: :element_defs
   has_and_belongs_to_many :vocabularies
 
+  validates_uniqueness_of :name, scope: :metadata_profile_id
+
   after_save :adjust_profile_element_indexes_after_save
   after_destroy :adjust_profile_element_indexes_after_destroy
 
