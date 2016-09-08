b0641b65283ef19b1cae566e559ada67845cb183
Alex Dolski <alexd@n-ary.net>
2016-09-08 14:52:49 -0500

    Redirect to most relevant resource rather than returning 404s

--- app/controllers/contentdm_controller.rb
+++ app/controllers/contentdm_controller.rb
@@ -9,9 +9,14 @@ class ContentdmController < ApplicationController
   # Responds to GET /cdm/landingpage/collection/:alias
   #
   def redirect_to_dls_collection
+    # Try to redirect to the collection itself.
     col = Collection.find_by_contentdm_alias(params[:alias])
-    raise ActiveRecord::RecordNotFound unless col
-    redirect_to collection_url(col)
+    if col
+      redirect_to collection_url(col)
+    else
+      # If it doesn't exist, redirect to the landing page.
+      redirect_to root_url
+    end
   end
 
   ##
@@ -21,10 +26,22 @@ class ContentdmController < ApplicationController
   # GET /cdm/compoundobject/collection/:alias/id/:pointer
   #
   def redirect_to_dls_item
+    # Try to redirect to the item itself.
     item = Item.where(contentdm_alias: params[:alias],
                       contentdm_pointer: params[:pointer]).limit(1).first
-    raise ActiveRecord::RecordNotFound unless item
-    redirect_to item_url(item)
+    if item
+      redirect_to item_url(item)
+    else
+      # If there is no item with the given pointer, but the collection is
+      # valid, redirect to that.
+      col = Collection.find_by_contentdm_alias(params[:alias])
+      if col
+        redirect_to collection_url(col)
+      else
+        # Otherwise, redirect to the landing page.
+        redirect_to root_url
+      end
+    end
   end
 
 end
