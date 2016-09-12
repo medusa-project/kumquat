f7496f80679746308725e360071c9bec2629a5be
Alex Dolski <alexd@n-ary.net>
2016-09-12 09:21:08 -0500

    Remove statistics table (IMET-282)

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -12,46 +12,12 @@ class LandingController < WebsiteController
         filter(Item::SolrFields::ACCESS_MASTER_MEDIA_TYPE => "(#{image_media_types})").
         facet(false).order(:random).limit(1).first
 
-    # Get some counts for the statistics table
-    @num_all_items = Item.solr.
-        where(Item::SolrFields::PUBLISHED => true).
-        where(Item::SolrFields::COLLECTION_PUBLISHED => true).count
-    @num_top_level_items = Item.solr.
-        where(Item::SolrFields::PUBLISHED => true).
-        where(Item::SolrFields::COLLECTION_PUBLISHED => true).
-        where(Item::SolrFields::PARENT_ITEM => :null).count
-
-    @num_audio_items = Item.solr.
-        where(Item::SolrFields::PUBLISHED => true).
-        where(Item::SolrFields::COLLECTION_PUBLISHED => true).
-        where("#{Item::SolrFields::ACCESS_MASTER_MEDIA_TYPE}:audio/* "\
-        "OR #{Item::SolrFields::PRESERVATION_MASTER_MEDIA_TYPE}:audio/*").count
-
-    doc_media_types = %w(application/pdf text/plain).join(' OR ')
-    @num_document_items = Item.solr.
-        where(Item::SolrFields::PUBLISHED => true).
-        where(Item::SolrFields::COLLECTION_PUBLISHED => true).
-        where("#{Item::SolrFields::ACCESS_MASTER_MEDIA_TYPE}:(#{doc_media_types}) "\
-        "OR #{Item::SolrFields::PRESERVATION_MASTER_MEDIA_TYPE}:(#{doc_media_types})").count
-
-    @num_image_items = Item.solr.
-        where(Item::SolrFields::PUBLISHED => true).
-        where(Item::SolrFields::COLLECTION_PUBLISHED => true).
-        where("#{Item::SolrFields::ACCESS_MASTER_MEDIA_TYPE}:image/* "\
-        "OR #{Item::SolrFields::PRESERVATION_MASTER_MEDIA_TYPE}:image/*").count
-
-    @num_video_items = Item.solr.
-        where(Item::SolrFields::PUBLISHED => true).
-        where(Item::SolrFields::COLLECTION_PUBLISHED => true).
-        where("#{Item::SolrFields::ACCESS_MASTER_MEDIA_TYPE}:video/* "\
-        "OR #{Item::SolrFields::PRESERVATION_MASTER_MEDIA_TYPE}:video/*").count
-
     # Get DLS collections
     @dls_collections = Collection.solr.
         where(Collection::SolrFields::ACCESS_SYSTEMS => 'Medusa Digital Library').
         limit(100)
 
-    fresh_when(etag: @num_all_items) if Rails.env.production?
+    fresh_when(etag: @dls_collections) if Rails.env.production?
   end
 
 end
