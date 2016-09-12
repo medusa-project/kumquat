medusa-project/kumquat

commit 912c349c2daf0b9ea8ac2fbf7c004efe1f46c0ad

Add ApplicationController.handle_error()

app/controllers/application_controller.rb
@@ -8,8 +8,10 @@ class ApplicationController < ActionController::Base
   before_action :setup
   after_action :flash_in_response_headers
 
+=begin
   def setup
   end
+=end
 
   def admin_user
     unless current_user.is_admin?
@@ -27,6 +29,16 @@ class ApplicationController < ActionController::Base
   end
 
   protected
+
+  ##
+  # Logs the given error and sets the flash to it.
+  #
+  # @param e [Exception, String]
+  #
+  def handle_error(e)
+    Rails.logger.warn(e)
+    flash['error'] = "#{e}"
+  end
 
   ##
   # Normally the flash is discarded after being added to the response headers

app/controllers/admin/collections_controller.rb
@@ -58,7 +58,7 @@ module Admin
 
         collection.update!(sanitized_params)
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
         redirect_to edit_admin_collection_path(collection)
       else
         flash['success'] = "Collection \"#{collection.title}\" updated."

app/controllers/admin/elements_controller.rb
@@ -20,7 +20,7 @@ module Admin
                locals: { entity: @element }
       rescue => e
         response.headers['X-PearTree-Result'] = 'error'
-        flash['error'] = "#{e}"
+        handle_error(e)
         keep_flash
         render 'create'
       else
@@ -36,7 +36,7 @@ module Admin
       begin
         element.destroy!
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
       else
         flash['success'] = "Element \"#{element.name}\" deleted."
       ensure
@@ -76,7 +76,7 @@ module Admin
           end
         end
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
         redirect_to admin_elements_path
       else
         flash['success'] = "#{struct.length} elements created or updated."
@@ -118,7 +118,7 @@ module Admin
                locals: { entity: element }
       rescue => e
         response.headers['X-PearTree-Result'] = 'error'
-        flash['error'] = "#{e}"
+        handle_error(e)
         keep_flash
         render 'update'
       else

app/controllers/admin/items_controller.rb
@@ -97,7 +97,7 @@ module Admin
             ImportItemsFromTsvJob.perform_later(tempfile.path)
           rescue => e
             tempfile.unlink
-            flash['error'] = "#{e}"
+            handle_error(e)
             redirect_to admin_collection_items_url(col)
           else
             flash['success'] = 'Importing items in the background. This '\
@@ -121,7 +121,7 @@ module Admin
         MigrateItemMetadataJob.perform_later(
             col.repository_id, params[:source_element], params[:dest_element])
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
         redirect_to admin_collection_items_url(col)
       else
         flash['success'] = 'Migrating metadata elements in the background. '\
@@ -166,7 +166,7 @@ module Admin
                                    params[:ingest_mode],
                                    params[:options])
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
         redirect_to admin_collection_items_url(col)
       else
         flash['success'] = 'Syncing items in the background. This '\
@@ -205,7 +205,7 @@ module Admin
           item.update!(sanitized_params)
         end
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
         redirect_to edit_admin_collection_item_path(item.collection, item)
       else
         flash['success'] = "Item \"#{item.title}\" updated."

app/controllers/admin/metadata_profile_elements_controller.rb
@@ -15,7 +15,7 @@ module Admin
                locals: { entity: @element }
       rescue => e
         response.headers['X-PearTree-Result'] = 'error'
-        flash['error'] = "#{e}"
+        handle_error(e)
         keep_flash
         render 'create'
       else
@@ -31,7 +31,7 @@ module Admin
       begin
         element.destroy!
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
       else
         flash['success'] = "Element \"#{element.label}\" deleted."
       ensure
@@ -75,7 +75,7 @@ module Admin
                locals: { entity: element }
       rescue => e
         response.headers['X-PearTree-Result'] = 'error'
-        flash['error'] = "#{e}"
+        handle_error(e)
         keep_flash
         render 'update'
       else

app/controllers/admin/metadata_profiles_controller.rb
@@ -11,7 +11,7 @@ module Admin
         clone = profile.dup
         clone.save!
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
         redirect_to :back
       else
         flash['success'] = "Cloned #{profile.name} as \"#{clone.name}\"."
@@ -30,7 +30,7 @@ module Admin
                locals: { entity: @profile }
       rescue => e
         response.headers['X-PearTree-Result'] = 'error'
-        flash['error'] = "#{e}"
+        handle_error(e)
         keep_flash
         render 'create'
       else
@@ -64,7 +64,7 @@ module Admin
       begin
         profile.destroy!
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
       else
         flash['success'] = "Metadata profile \"#{profile.name}\" deleted."
       ensure
@@ -83,7 +83,7 @@ module Admin
         profile = MetadataProfile.from_json(json)
         profile.save!
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
         redirect_to admin_metadata_profiles_path
       else
         flash['success'] = "Profile imported as #{profile.name}."
@@ -133,7 +133,7 @@ module Admin
                  locals: { entity: @profile }
         rescue => e
           response.headers['X-PearTree-Result'] = 'error'
-          flash['error'] = "#{e}"
+          handle_error(e)
           keep_flash
           render 'update'
         else
@@ -150,7 +150,7 @@ module Admin
           render 'show'
         rescue => e
           response.headers['X-PearTree-Result'] = 'error'
-          flash['error'] = "#{e}"
+          handle_error(e)
           render 'show'
         else
           response.headers['X-PearTree-Result'] = 'success'

app/controllers/admin/settings_controller.rb
@@ -32,7 +32,7 @@ module Admin
           end
         end
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
         render :index
       else
         flash['success'] = 'Settings updated.'

app/controllers/admin/users_controller.rb
@@ -24,7 +24,7 @@ module Admin
       begin
         user.update_attributes!(tmp_params)
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
         render 'new'
       else
         flash['success'] = "User #{user.username} updated."
@@ -36,7 +36,7 @@ module Admin
       begin
         user = User.create!(sanitized_params)
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
         @user = User.new
         @roles = Role.all.order(:name)
         render 'new'
@@ -53,7 +53,7 @@ module Admin
       begin
         user.destroy!
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
         redirect_to admin_users_url
       else
         if user == current_user
@@ -78,7 +78,7 @@ module Admin
       begin
         user.save!
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
       else
         flash['success'] = "User #{user.username} disabled."
       ensure
@@ -103,7 +103,7 @@ module Admin
       begin
         user.save!
       rescue => e
-        flash['error'] = "#{e}"
+        handle_error(e)
       else
         flash['success'] = "User #{user.username} enabled."
       ensure
@@ -135,7 +135,7 @@ module Admin
         @user.update_attributes!(sanitized_params)
       rescue => e
         @roles = Role.all.order(:name)
-        flash['error'] = "#{e}"
+        handle_error(e)
         render 'edit'
       else
         flash['success'] = "User #{@user.username} updated."

commit ebd891ccc447cce70524c65987b4fb2a55579b50

Uncomment setup(), fixing a bug introduced in 912c349c2daf0b9ea8ac2fbf7c004efe1f46c0ad

app/controllers/application_controller.rb
@@ -8,10 +8,8 @@ class ApplicationController < ActionController::Base
   before_action :setup
   after_action :flash_in_response_headers
 
-=begin
   def setup
   end
-=end
 
   def admin_user
     unless current_user.is_admin?