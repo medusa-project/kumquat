a66506ec04ee0647b1046e32230cc6c0660b9979
Alex Dolski <alexd@n-ary.net>
2016-09-21 16:46:25 -0500

    Rename UPDATE_SETTINGS permission to MODIFY_SETTINGS and RBAC-enable Admin::SettingsController

--- app/access_control/permission.rb
+++ app/access_control/permission.rb
@@ -25,9 +25,8 @@ class Permission < ActiveRecord::Base
     MODIFY_COLLECTIONS = 'collections.modify'
     MODIFY_ITEMS = 'items.modify'
     MODIFY_ROLES = 'roles.modify'
-    #MODIFY_SETTINGS = 'settings.modify'
+    MODIFY_SETTINGS = 'settings.modify'
     MODIFY_USERS = 'users.modify'
-    UPDATE_SETTINGS = 'settings.update'            # TODO: rename to MODIFY_SETTINGS
   end
 
   has_and_belongs_to_many :roles
