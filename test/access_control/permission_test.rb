ba36924a3783b45a720139cfd7ea2ae756b306db
Alex Dolski <alexd@n-ary.net>
2016-09-21 16:48:15 -0500

    Fix references to obsolete permissions

--- test/access_control/permission_test.rb
+++ test/access_control/permission_test.rb
@@ -1,12 +1,10 @@
 require 'test_helper'
 
 class PermissionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
   test 'name() should return the name' do
-    p = Permission.new(key: Permission::Permissions::CREATE_USER)
-    assert_equal 'Create User', p.name
+    p = Permission.new(key: Permission::Permissions::MODIFY_USERS)
+    assert_equal 'Modify Users', p.name
   end
+
 end
