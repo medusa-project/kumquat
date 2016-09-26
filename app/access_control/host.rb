34d93d47a4ae61534be251765399342a9ea0a848
Alex Dolski <alexd@n-ary.net>
2016-09-26 14:27:01 -0500

    pattern_matches?() supports ranges (IMET-64)

--- app/access_control/host.rb
+++ app/access_control/host.rb
@@ -2,12 +2,35 @@
 # Encapsulates an internet host whose name or IP address can be covered by
 # a pattern.
 #
+# Wildcards are allowed in both IPs and hostnames, as in:
+# *.example.org
+# 10.0.*
+#
+# IP ranges are also allowed, as in:
+# 10.0.*-10.10.*
+#
 class Host < ActiveRecord::Base
 
-  # Allow any hostname, IPv4 or IPv6 address, as well as a wildcard character.
-  validates_format_of :pattern, with: /\A[a-zA-Z0-9\-.*_:]+\Z/i,
+  # Allow any hostname or IPv4 address, as well as a wildcard character.
+  validates_format_of :pattern, with: /\A[a-zA-Z0-9\-.*_]+\Z/i,
                       message: 'Pattern is invalid',
                       allow_blank: false
+
+  ##
+  # @param string [String] Any string
+  # @return [Boolean]
+  #
+  def ip?(string)
+    !(string =~ /\A[0-9.]/).nil?
+  end
+
+  ##
+  # @return [Boolean]
+  #
+  def ip_range?(string)
+    parts = string.split('-')
+    parts.select{ |p| ip?(p) }.length == 2
+  end
 
   ##
   # @param string [String] Hostname or IP address
@@ -16,6 +39,9 @@ class Host < ActiveRecord::Base
   def pattern_matches?(string)
     if self.pattern == string
       return true
+    elsif ip_range?(self.pattern)
+      parts = self.pattern.split('-')
+      return within_range?(string, parts[0], parts[1])
     elsif self.pattern.end_with?('*')
       filtered_pattern = self.pattern.gsub('*', '')
       return true if string.start_with?(filtered_pattern)
@@ -24,6 +50,27 @@ class Host < ActiveRecord::Base
       return true if string.end_with?(filtered_pattern)
     end
     false
+  end
+
+  ##
+  # @param ip [String] Full IP address
+  # @param start [String] Start of the range (wildcards allowed)
+  # @param end_ [String] End of the range (wildcards allowed)
+  # @return [Boolean]
+  #
+  def within_range?(ip, start, end_)
+    ip_groups = ip.gsub('*', '').split('.')
+    start_groups = start.gsub('*', '').split('.')
+    end_groups = end_.gsub('*', '').split('.')
+
+    4.times do |level|
+      ip_group = ip_groups[level].to_i
+      start_group = start_groups[level].to_i
+      end_group = end_groups[level].to_i
+      return false if (start_group > 0 and ip_group < start_group) or
+          (end_group > 0 and ip_group > end_group)
+    end
+    true
   end
 
 end
--- test/access_control/host_test.rb
+++ test/access_control/host_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class HostTest < ActiveSupport::TestCase
 
   test 'pattern can contain only certain characters' do
-    Host.create!(pattern: 'ABCabc123.*_-:')
+    Host.create!(pattern: 'ABCabc123.*_-')
 
     assert_raises ActiveRecord::RecordInvalid do
       Host.create!(pattern: 'abc abc')
@@ -13,16 +13,67 @@ class HostTest < ActiveSupport::TestCase
     end
   end
 
-  test 'patterh_matches?() should work' do
+  # ip?()
+
+  test 'ip?() should work' do
+    host = Host.new(pattern: '123.123.*')
+    assert host.ip?('123.123.*')
+    host = Host.new(pattern: '123.123.10.1')
+    assert host.ip?('123.123.10.1')
+    host = Host.new(pattern: 'example.org')
+    assert !host.ip?('example.org')
+    host = Host.new(pattern: '*.example.org')
+    assert !host.ip?('*.example.org')
+  end
+
+  # ip_range?()
+
+  test 'ip_range?() should work' do
+    host = Host.new(pattern: '123.123.*')
+    assert !host.ip_range?('123.123.*')
+    host = Host.new(pattern: '123.123.10.1')
+    assert !host.ip_range?('123.123.10.1')
+    host = Host.new(pattern: 'example.org')
+    assert !host.ip_range?('example.org')
+
+    host = Host.new(pattern: 'example-example.org')
+    assert !host.ip_range?('example-example.org')
+
+    host = Host.new(pattern: '123.123.*-123.124.*')
+    assert host.ip_range?('123.123.*-123.124.*')
+  end
+
+  # pattern_matches?()
+
+  test 'pattern_matches?() should work with IP addresses' do
     host = Host.new(pattern: '123.123.*')
     assert host.pattern_matches?('123.123.123.123')
     assert host.pattern_matches?('123.123.234.234')
     assert !host.pattern_matches?('214.123.123.123')
+  end
 
+  test 'pattern_matches?() should work with IP address ranges' do
+    host = Host.new(pattern: '123.123.*-123.130.*')
+    assert host.pattern_matches?('123.123.123.123')
+    assert host.pattern_matches?('123.123.127.*')
+    assert !host.pattern_matches?('123.120.123.*')
+    assert !host.pattern_matches?('123.131.123.*')
+  end
+
+  test 'pattern_matches?() should work with hostnames' do
     host = Host.new(pattern: '*.example.org')
     assert host.pattern_matches?('example.org')
     assert host.pattern_matches?('cats.example.org')
     assert !host.pattern_matches?('dogs.example.com')
+  end
+
+  # within_range?()
+
+  test 'within_range?() should work' do
+    host = Host.new(pattern: '123.123.*-123.130.*')
+    assert host.within_range?('123.126.12.10', '123.123.*', '123.130.*')
+    assert !host.within_range?('123.122.16.2', '123.123.*', '123.130.*')
+    assert !host.within_range?('123.131.16.2', '123.123.*', '123.130.*')
   end
 
 end
