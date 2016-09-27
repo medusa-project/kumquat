d2acea5642d97dfc93e606d0822ff63db57d84b7
Alex Dolski <alexd@n-ary.net>
2016-09-27 13:49:22 -0500

    Include JSON metadata files in zips

--- app/decorators/item_decorator.rb
+++ app/decorators/item_decorator.rb
@@ -1,6 +1,14 @@
 ##
 # Assists in creating an optimized JSON serialization.
 #
+# Examples:
+#
+# * Generate a web-ready representation
+#     Item.find(..).decorate
+#
+# * Generate a non-web representation
+#     Item.find(..).decorate(context: { web: false })
+#
 class ItemDecorator < Draper::Decorator
   delegate_all
   include Draper::LazyHelpers
@@ -17,18 +25,25 @@ class ItemDecorator < Draper::Decorator
   def serializable_hash(opts)
     struct = {
         id: self.repository_id,
-        collection: self.collection ? collection_url(self.collection) : nil,
+        collection: self.collection ? self.collection.repository_id : nil,
         page_number: self.page_number,
         subpage_number: self.subpage_number,
         variant: self.variant,
         full_text: self.full_text,
-        parent: self.parent ? item_url(self.parent) : nil,
+        parent: self.parent ? self.parent.repository_id : nil,
         representative_item: self.representative_item ?
-            item_url(self.representative_item) : nil,
+            self.representative_item.repository_id : nil,
         elements: [],
-        bytestreams: BytestreamDecorator.decorate_collection(self.bytestreams),
         children: []
     }
+
+    if context[:web]
+      struct[:collection] = self.collection ? collection_url(self.collection) : nil
+      struct[:parent] = self.parent ? item_url(self.parent) : nil
+      struct[:representative_item] = self.representative_item ?
+          item_url(self.representative_item) : nil
+      struct[:bytestreams] = BytestreamDecorator.decorate_collection(self.bytestreams)
+    end
 
     # Populate the elements array
     self.elements.each do |element|
@@ -48,10 +63,11 @@ class ItemDecorator < Draper::Decorator
 
     # Populate the subitems array
     self.items.each do |subitem|
-      struct[:subitems] << {
-          id: subitem.repository_id,
-          url: item_url(subitem)
-      }
+      subitem = { id: subitem.repository_id }
+      if context[:web]
+        subitem[:url] = item_url(subitem)
+      end
+      struct[:subitems] << subitem
     end
     struct
   end
--- app/rack/zip_downloader.rb
+++ app/rack/zip_downloader.rb
@@ -12,14 +12,27 @@ class ZipDownloader
 
       body = ZipTricks::RackBody.new do |zip|
         items.each do |item|
+          # Include the item's JSON metadata in the zip.
+          json = JSON.pretty_generate(item.decorate(context: { web: false }).as_json)
+          json_io = StringIO.new(json)
+
+          crc32 = ZipTricks::StreamCRC32.from_io(json_io)
+          json_io.rewind
+          filename = "#{item.repository_id}/metadata.json"
+          zip.add_stored_entry(filename: filename, size: json_io.size,
+                               crc32: crc32)
+          IO.copy_stream(json_io, zip)
+
+          # If the item has an access master, include it in the zip also.
           bs = item.access_master_bytestream
           if bs
             file = File.open(bs.absolute_local_pathname, 'rb')
             crc32 = ZipTricks::StreamCRC32.from_io(file)
             file.rewind
-            filename = "#{item.repository_id}#{File.extname(bs.absolute_local_pathname)}"
+            filename = "#{item.repository_id}/access#{File.extname(bs.absolute_local_pathname)}"
             begin
-              zip.add_stored_entry(filename: filename, size: file.size, crc32: crc32)
+              zip.add_stored_entry(filename: filename, size: file.size,
+                                   crc32: crc32)
               IO.copy_stream(file, zip)
             ensure
               file.close
