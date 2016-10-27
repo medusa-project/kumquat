be7167e2bf4ad0ae50633788a8fd583e18411a7e
Alex Dolski <alexd@n-ary.net>
2016-10-27 13:29:38 -0500

    Replace random image with a random collection (IMET-383)

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -6,17 +6,15 @@ class LandingController < WebsiteController
   # Responds to GET /
   #
   def index
-    # Get a random image item to show.
-    finder = ItemFinder.new.
+    # Get a random collection to show.
+    finder = CollectionFinder.new.
         client_hostname(request.host).
         client_ip(request.remote_ip).
         client_user(current_user).
-        include_children(true).
-        include_variants([Item::Variants::FILE, nil]).
-        media_types(IMAGE_MEDIA_TYPES).
-        sort(:random).
+        filter_queries(Collection::SolrFields::ACCESS_SYSTEMS => 'Medusa Digital Library').
+        order(:random).
         limit(1)
-    @random_item = finder.to_a.first
+    @random_collection = finder.to_a.first
 
     # Get DLS collections.
     finder = CollectionFinder.new.
