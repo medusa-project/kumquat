acee0017a99511c7c90dd706c6b48169e5141fa7
Alex Dolski <alexd@n-ary.net>
2016-11-02 13:46:49 -0500

    Rename get_panel() and move it to the top level of the module

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -6,47 +6,13 @@ module CollectionsHelper
   def collection_facets_as_panels(collections)
     return nil unless collections.facet_fields # nothing to do
 
-    def get_panel(title, terms)
-      panel = "<div class=\"panel panel-default\">
-      <div class=\"panel-heading\">
-        <h3 class=\"panel-title\">#{title}</h3>
-      </div>
-      <div class=\"panel-body\">
-        <ul>"
-      terms.each_with_index do |term, i|
-        break if i >= Option::integer(Option::Key::FACET_TERM_LIMIT)
-        next if term.count < 1
-        checked = (params[:fq] and params[:fq].include?(term.facet_query)) ?
-            'checked' : nil
-        checked_params = term.removed_from_params(params.deep_dup)
-        unchecked_params = term.added_to_params(params.deep_dup)
-        checked_params.delete(:start)
-        unchecked_params.delete(:start)
-        term_label = truncate(term.label, length: 80)
-
-        panel += "<li class=\"pt-term\">"
-        panel += "<div class=\"checkbox\">"
-        panel += "<label>"
-        panel += "<input type=\"checkbox\" name=\"pt-facet-term\" #{checked} "\
-        "data-checked-href=\"#{url_for(unchecked_params)}\" "\
-        "data-unchecked-href=\"#{url_for(checked_params)}\">"
-        panel += "<span class=\"pt-term-name\">#{term_label}</span> "
-        panel += "<span class=\"pt-count badge\">#{term.count}</span>"
-        panel += "</label>"
-        panel += "</div>"
-        panel += "</li>"
-      end
-      raw(panel + '</ul></div></div>')
-    end
-
     html = ''
     Collection::solr_facet_fields.each do |field|
       result_facet = collections.facet_fields.
           select{ |f| f.field == field[:name] }.first
       next unless result_facet and
           result_facet.terms.select{ |t| t.count > 0 }.any?
-
-      html += get_panel(field[:label], result_facet.terms)
+      html += collection_facet_panel(field[:label], result_facet.terms)
     end
     raw(html)
   end
@@ -84,6 +50,41 @@ module CollectionsHelper
       return collection.access_url
     end
     nil
+  end
+
+  private
+
+  def collection_facet_panel(title, terms)
+    panel = "<div class=\"panel panel-default\">
+      <div class=\"panel-heading\">
+        <h3 class=\"panel-title\">#{title}</h3>
+      </div>
+      <div class=\"panel-body\">
+        <ul>"
+    terms.each_with_index do |term, i|
+      break if i >= Option::integer(Option::Key::FACET_TERM_LIMIT)
+      next if term.count < 1
+      checked = (params[:fq] and params[:fq].include?(term.facet_query)) ?
+          'checked' : nil
+      checked_params = term.removed_from_params(params.deep_dup)
+      unchecked_params = term.added_to_params(params.deep_dup)
+      checked_params.delete(:start)
+      unchecked_params.delete(:start)
+      term_label = truncate(term.label, length: 80)
+
+      panel += "<li class=\"pt-term\">"
+      panel += "<div class=\"checkbox\">"
+      panel += "<label>"
+      panel += "<input type=\"checkbox\" name=\"pt-facet-term\" #{checked} "\
+        "data-checked-href=\"#{url_for(unchecked_params)}\" "\
+        "data-unchecked-href=\"#{url_for(checked_params)}\">"
+      panel += "<span class=\"pt-term-name\">#{term_label}</span> "
+      panel += "<span class=\"pt-count badge\">#{term.count}</span>"
+      panel += "</label>"
+      panel += "</div>"
+      panel += "</li>"
+    end
+    raw(panel + '</ul></div></div>')
   end
 
 end
