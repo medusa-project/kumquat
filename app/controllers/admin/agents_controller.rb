medusa-project/kumquat

commit e9e3e71dec8c025bba548f9043f7663ef468e325

Agents can have multiple URIs (IMET-275)

app/controllers/admin/agents_controller.rb
@@ -9,7 +9,12 @@ module Admin
       @agent = Agent.new(sanitized_agent_params)
       begin
         ActiveRecord::Base.transaction do
+          params[:agent_uris].each do |k, v|
+            @agent.agent_uris.build(uri: v[:uri],
+                                    primary: (v[:primary] == 'true'))
+          end
           @agent.save!
+
           if params[:agent_relation]
             relation = AgentRelation.new(sanitized_agent_relation_params)
             relation.related_agent = @agent
@@ -22,7 +27,7 @@ module Admin
                locals: { entity: @agent }
       rescue => e
         response.headers['X-PearTree-Result'] = 'error'
-        handle_error(e)
+        handlbunde_error(e)
         keep_flash
         render 'create'
       else
@@ -70,6 +75,7 @@ module Admin
       end
 
       @new_agent = Agent.new
+      @new_agent.agent_uris.build
     end
 
     ##
@@ -78,13 +84,14 @@ module Admin
     def show
       @agent = Agent.find(params[:id])
       @new_agent = Agent.new
+      @new_agent.agent_uris.build
       @new_agent_relation = @agent.agent_relations.build
       @relating_agents = AgentRelation.where(related_agent: @agent)
       @related_agents = AgentRelation.where(agent: @agent)
 
       @num_item_references = Item.
           joins('LEFT JOIN entity_elements ON entity_elements.item_id = items.id').
-          where('entity_elements.uri': @agent.uri).count
+          where('entity_elements.uri IN (?)', @agent.agent_uris.map(&:uri)).count
       @num_collection_references = 0 # TODO: fix
       @num_agent_references = @relating_agents.count + @related_agents.count
     end
@@ -95,16 +102,22 @@ module Admin
     def update
       agent = Agent.find(params[:id])
       begin
-        agent.update!(sanitized_agent_params)
+        ActiveRecord::Base.transaction do
+          agent.agent_uris.destroy_all
+          params[:agent_uris].each do |k, v|
+            agent.agent_uris.build(uri: v[:uri],
+                                   primary: (v[:primary] == 'true'))
+          end
+          agent.update!(sanitized_agent_params)
+        end
       rescue ActiveRecord::RecordInvalid
         response.headers['X-PearTree-Result'] = 'error'
         render partial: 'shared/validation_messages',
                locals: { entity: agent }
       rescue => e
         response.headers['X-PearTree-Result'] = 'error'
-        handle_error(e)
-        keep_flash
-        render 'update'
+        render partial: 'shared/validation_messages',
+               locals: { entity: e }
       else
         response.headers['X-PearTree-Result'] = 'success'
         flash['success'] = "Agent \"#{agent.name}\" updated."

app/assets/javascripts/admin/agents.js
@@ -1,4 +1,60 @@
+var PTAdminAgentEditForm = function() {
+
+    this.init = function() {
+        var updateRowIndices = function(table) {
+            table.find('tr').each(function(index, element) {
+                $(element).find('input').each(function() {
+                    var input = $(this);
+                    var newId = input.attr('id')
+                        .replace(/_[0-9]_/, '_' + index + '_');
+                    var newName = input.attr('name')
+                        .replace(/\[[0-9]]/, '[' + index + ']');
+                    input.attr('id', newId);
+                    input.attr('name', newName);
+                });
+            });
+        };
+        $('button.pt-add').on('click', function() {
+            var lastRow = $(this).prev('table').find('tr:last');
+            var clone = lastRow.clone(true);
+            clone.find('input[type=text]').val('');
+            clone.find('input[type=radio]').prop('checked', false);
+            lastRow.after(clone);
+            updateRowIndices($(this).prev('table'));
+            return false;
+        });
+        $('button.pt-remove').on('click', function() {
+            var form = $(this).closest('form');
+            var row = $(this).closest('tr');
+            var siblings = row.siblings();
+            if (siblings.length > 0) {
+                row.remove();
+                if (siblings.find('input[type=radio]:checked').length < 1) {
+                    console.log(siblings.filter(':first').find('input[type=radio]').length);
+                    siblings.filter(':first').find('input[type=radio]')
+                        .prop('checked', true);
+                }
+            }
+            updateRowIndices(row.closest('table'));
+            return false;
+        });
+        // When a radio is checked, uncheck all the others. The radios all have
+        // different names, so this won't happen automatically.
+        $('input[type=radio]').on('click', function() {
+            var clickedRadio = $(this);
+            $('table#pt-agent-uris').find('input[type=radio]').each(function() {
+                if ($(this).attr('name') != clickedRadio.attr('name')) {
+                    $(this).prop('checked', false);
+                }
+            });
+        });
+    };
+
+};
+
 /**
+ * Handles list-agents view.
+ *
  * @constructor
  */
 var PTAdminAgentsView = function() {
@@ -18,6 +74,21 @@ var PTAdminAgentsView = function() {
             });
         });
         $('a[disabled="disabled"]').on('click', function() { return false; });
+
+        new PTAdminAgentEditForm().init();
+    };
+
+};
+
+/**
+ * Handles show-agent view.
+ *
+ * @constructor
+ */
+var PTAdminAgentView = function() {
+
+    this.init = function() {
+        new PTAdminAgentEditForm().init();
     };
 
 };
@@ -25,6 +96,9 @@ var PTAdminAgentsView = function() {
 var ready = function() {
     if ($('body#admin_agents_index').length) {
         PearTree.view = new PTAdminAgentsView();
+        PearTree.view.init();
+    } else if ($('body#agents_show').length) {
+        PearTree.view = new PTAdminAgentView();
         PearTree.view.init();
     }
 };

app/assets/stylesheets/admin.scss
@@ -18,6 +18,7 @@
 @import "font-awesome";
 
 @import "sitewide";
+@import "admin/agents";
 @import "admin/common";
 @import "admin/collections";
 @import "admin/items";

app/assets/stylesheets/admin/agents.scss
@@ -1,0 +1,11 @@
+body#admin_agents_index, body#agents_show {
+  #pt-agent-uris {
+    width: 100%;
+    td {
+      padding: 4px 0px;
+    }
+    td:nth-child(2) {
+      padding-left: 8px;
+    }
+  }
+}

app/metadata/agent.rb
@@ -5,17 +5,30 @@ class Agent < ActiveRecord::Base
 
   has_many :agent_relations, class_name: 'AgentRelation',
            foreign_key: :agent_id, dependent: :destroy
-  has_many :related_agents, -> { order('name ASC') },
+  has_many :related_agents, -> { order(name: :asc) },
            through: :agent_relations, source: :related_agent
+  has_many :agent_uris, -> { order(primary: :desc) }, inverse_of: :agent,
+           dependent: :destroy
 
   before_validation :ascribe_default_uri, if: :new_record?
 
-  validates_presence_of :name, :uri
+  validates_presence_of :name
+
+  ##
+  # @return [String, nil] The agent's primary URI, or one if its URIs if none
+  #                       are marked as primary; or nil if the agent has no
+  #                       URIs.
+  #
+  def primary_uri
+    self.agent_uris.select(&:primary).first&.uri || self.agent_uris.first&.uri
+  end
 
   private
 
   def ascribe_default_uri
-    self.uri = "urn:uuid:#{SecureRandom.uuid}" if self.uri.blank?
+    if self.agent_uris.empty?
+      self.agent_uris.build(uri: "urn:uuid:#{SecureRandom.uuid}", primary: true)
+    end
   end
 
 end

app/metadata/agent_uri.rb
@@ -1,0 +1,11 @@
+class AgentUri < ActiveRecord::Base
+
+  belongs_to :agent, inverse_of: :agent_uris
+
+  validates_presence_of :uri
+
+  def to_s
+    "#{self.uri}"
+  end
+
+end

app/views/admin/agents/_agents.html.erb
@@ -3,7 +3,6 @@
     <th style="width: 1px"></th>
     <th style="width: 1px"></th>
     <th>Name</th>
-    <th>URI</th>
   </thead>
   <tbody>
     <% @agents.each do |agent| %>
@@ -24,7 +23,6 @@
           <% end %>
         </td>
         <td><%= link_to agent.name, admin_agent_path(agent) %></td>
-        <td><%= link_to agent.uri, admin_agent_path(agent) %></td>
       </tr>
     <% end %>
   </tbody>

app/views/admin/agents/_form.html.erb
@@ -12,20 +12,42 @@
   </div>
 
   <div class="form-group">
-    <%= f.label :uri, 'URI' %>
-    <%= f.text_field :uri, class: 'form-control',
-                     placeholder: (context == :new) ?
-                         'If left blank, a random URI will be generated automatically.' : '' %>
-    <% if context == :edit %>
-      <p class="help text-danger">
-        <i class="fa fa-warning"></i> Changing the URI may break entity relationships.
-      </p>
-    <% end %>
+    <%= f.label :name %>
+    <%= f.text_field :name, class: 'form-control' %>
   </div>
 
   <div class="form-group">
-    <%= f.label :name %>
-    <%= f.text_field :name, class: 'form-control' %>
+    <%= f.label 'uris', 'URIs' %>
+    <% if context == :edit %>
+      <span class="help text-danger">
+        <i class="fa fa-warning"></i> Changing URIs may break entity relationships.
+      </span>
+    <% end %>
+    <table id="pt-agent-uris" border="0">
+      <% for index in 0..(f.object.agent_uris.length - 1) %>
+        <% uri = f.object.agent_uris[index] %>
+        <tr>
+          <td>
+            <%= text_field_tag "agent_uris[#{index}][uri]", uri&.uri,
+                               class: 'form-control' %>
+          </td>
+          <td style="width:100px">
+            <%= label_tag do %>
+              <%= radio_button_tag "agent_uris[#{index}][primary]", 'true',
+                                   uri&.primary %> Primary
+            <% end %>
+          </td>
+          <td style="width:1px">
+            <button class="btn btn-sm btn-danger pt-remove">
+              <i class="fa fa-minus"></i>
+            </button>
+          </td>
+        </tr>
+      <% end %>
+    </table>
+    <button class="btn btn-sm btn-success pt-add">
+      <i class="fa fa-plus"></i>
+    </button>
   </div>
 
   <% if agent_relation %>

commit 322982d697090397a06819622ef98a59cec79329

Fix a typo introduced in e9e3e71

app/controllers/admin/agents_controller.rb
@@ -27,7 +27,7 @@ module Admin
                locals: { entity: @agent }
       rescue => e
         response.headers['X-PearTree-Result'] = 'error'
-        handlbunde_error(e)
+        handle_error(e)
         keep_flash
         render 'create'
       else