15960ce833fae9a024d10469f9dbdad118fb285d
Alex Dolski <alexd@n-ary.net>
2016-12-07 13:31:17 -0600

    Commit Solr on create/update/delete (IMET-275)

--- app/controllers/admin/agents_controller.rb
+++ app/controllers/admin/agents_controller.rb
@@ -31,6 +31,7 @@ module Admin
         keep_flash
         render 'create'
       else
+        Solr.instance.commit
         response.headers['X-PearTree-Result'] = 'success'
         flash['success'] = "Agent \"#{@agent.name}\" created."
         keep_flash
@@ -41,10 +42,11 @@ module Admin
     def destroy
       agent = Agent.find(params[:id])
       begin
-        agent.destroy!
+        ActiveRecord::Base.transaction { agent.destroy! }
       rescue => e
         handle_error(e)
       else
+        Solr.instance.commit
         flash['success'] = "Agent \"#{agent.name}\" deleted."
       ensure
         redirect_to admin_agents_path
@@ -119,6 +121,7 @@ module Admin
         render partial: 'shared/validation_messages',
                locals: { entity: e }
       else
+        Solr.instance.commit
         response.headers['X-PearTree-Result'] = 'success'
         flash['success'] = "Agent \"#{agent.name}\" updated."
         keep_flash
