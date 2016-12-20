2cf4ebd1d09d87b165b6b4dcda41bfadc0016197
Alex Dolski <alexd@n-ary.net>
2016-12-20 14:41:47 -0600

    Make redirects permanent

--- app/controllers/contentdm_controller.rb
+++ app/controllers/contentdm_controller.rb
@@ -11,9 +11,9 @@ class ContentdmController < ApplicationController
   def redirect_to_dls_collection
     col = Collection.find_by_contentdm_alias(params[:alias])
     if col
-      redirect_to collection_url(col)
+      redirect_to collection_url(col), status: 301
     else
-      redirect_to root_url
+      redirect_to root_url, status: 301
     end
   end
 
@@ -28,19 +28,19 @@ class ContentdmController < ApplicationController
     item = Item.where(contentdm_alias: params[:alias],
                       contentdm_pointer: params[:pointer]).limit(1).first
     if item
-      redirect_to item_url(item)
+      redirect_to item_url(item), status: 301
     else
       item = Item.joins('LEFT JOIN collections ON items.collection_repository_id = collections.repository_id').
           where('collections.contentdm_alias': params[:alias],
                 contentdm_pointer: params[:pointer]).limit(1).first
       if item
-        redirect_to item_url(item)
+        redirect_to item_url(item), status: 301
       else
         col = Collection.find_by_contentdm_alias(params[:alias])
         if col
-          redirect_to collection_url(col)
+          redirect_to collection_url(col), status: 301
         else
-          redirect_to root_url
+          redirect_to root_url, status: 301
         end
       end
     end
