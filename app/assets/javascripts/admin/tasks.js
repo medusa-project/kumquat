76722b61e63aa008e355df9d8003f5789a23b2e6
Alex Dolski <alexd@n-ary.net>
2017-01-25 13:50:17 -0600

    Add a task detail view (IMET-379)

--- app/assets/javascripts/admin/tasks.js
+++ app/assets/javascripts/admin/tasks.js
@@ -4,49 +4,39 @@ var PTTaskRefresher = function() {
 
     var self = this;
 
-    var refresh = function () {
+    var refresh = function() {
         console.log('Refreshing task list...');
 
-        var tasks_url = $('input[name="tasks-url"]').val();
+        var tasks_url = $('input[name="pt-tasks-url"]').val();
         $.get(tasks_url, function (data) {
             $('#pt-tasks-list').html(data);
-            $('[data-toggle=popover]').popover({ container: 'body', html: true });
-
-            // We need to handle opover-closing specially due to the way the
-            // tasks table auto-refreshes.
-            $('body').on('click', function (e) {
-                // If the user did not click a popover toggle or popover...
-                if ($(e.target).data('toggle') !== 'popover'
-                    && $(e.target).parents('.popover.in').length === 0) {
-                    // Close all popovers...
-                    $('[data-toggle="popover"]').popover('hide');
-                    // and destroy any remaining zombie popovers.
-                    $('.popover.in').fadeOut(function() { $(this).remove() });
-                }
-            });
         });
     };
 
     this.refreshTimer = null;
 
-    this.start = function () {
+    this.start = function() {
         self.refreshTimer = setInterval(refresh, FREQUENCY);
         refresh();
     };
+
+    this.stop = function() {
+        clearInterval(self.refreshTimer);
+    }
 
 };
 
 var task_refresher;
 
 var ready = function() {
-    if ($('body#tasks').length) {
+    if ($('body#admin_tasks, body#admin_task_show').length) {
         task_refresher = new PTTaskRefresher();
         task_refresher.start();
     }
 };
 
 var teardown = function() {
-    clearInterval(task_refresher.refreshTimer);
+    task_refresher.stop();
 };
 
 $(document).ready(ready);
--- app/controllers/admin/tasks_controller.rb
+++ app/controllers/admin/tasks_controller.rb
@@ -6,8 +6,8 @@ module Admin
     # Responds to GET /admin/tasks
     #
     def index
-      @tasks = Task.order(created_at: :desc).limit(100)
-
+      set_tasks_ivar
+      @selected_id = params[:selected_id]&.to_i
       render partial: 'tasks' if request.xhr?
     end
 
@@ -15,7 +15,15 @@ module Admin
     # Responds to GET /admin/tasks/:id
     #
     def show
+      set_tasks_ivar
       @task = Task.find(params[:id])
+      @selected_id = @task.id
+    end
+
+    private
+
+    def set_tasks_ivar
+      @tasks = Task.order(created_at: :desc).limit(100)
     end
 
   end
