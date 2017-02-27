9d1f9ee04168706a7ce6ce1052341bea152b641a
Alex Dolski <alexd@n-ary.net>
2017-02-27 12:04:02 -0600

    Update the page title when a repository facet is selected as well as when a repository-specific page is loaded (DLD-38)

--- app/assets/javascripts/collections.js
+++ app/assets/javascripts/collections.js
@@ -35,35 +35,41 @@ var PTCollectionsView = function() {
                         eval(result);
                     }
                 });
-
-                // IMET-404: Get a list of checked repositories...
-                var repositories = [];
-                $('#pt-repository-facet input:checked').each(function() {
-                    repositories.push($(this).next().text().trim());
-                });
-                // ... and then set the page title to the English-ized list.
-                var text = '';
-                var count = $('#pt-count');
-                if (repositories.length == 1) {
-                    text += repositories[0];
-                } else if (repositories.length > 1) {
-                    var last = repositories.pop();
-                    var others = repositories.join(', ');
-                    text += others + ' and ' + last;
-                } else {
-                    text = 'Collections';
-                }
-                $('#pt-page-title').text(text + ' ');
-                $('#pt-page-title').append(count);
             });
         };
 
         // When the filter field has been updated, it will recreate the facets.
         $(document).ajaxSuccess(function(event, request) {
             addFacetEventListeners();
+            updateTitle();
         });
         addFacetEventListeners();
+        updateTitle();
     }; init();
+
+    function updateTitle() {
+        // IMET-404: Get a list of checked repositories...
+        var repositories = [];
+        $('#pt-repository-facet input:checked').each(function() {
+            repositories.push($(this).next().text().trim());
+        });
+        // ... and then set the page title to the English-ized list.
+        var text = '';
+        var count = $('#pt-count');
+        if (repositories.length == 1) {
+            text += repositories[0];
+        } else if (repositories.length > 1) {
+            var last = repositories.pop();
+            var others = repositories.join(', ');
+            text += others + ' and ' + last;
+        } else {
+            text = 'Collections';
+        }
+        var title = $('#pt-page-title');
+        title.text(text + ' ');
+        title.append(count);
+    }
+
 };
 
 var ready = function() {
