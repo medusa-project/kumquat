4659ee53c00ea6e43a77ff9065ac23d606efaad8
Alex Dolski <alexd@n-ary.net>
2017-02-27 12:53:49 -0600

    Add TSV filename and item count to TSV import task title (DLD-24)

--- app/jobs/import_items_from_tsv_job.rb
+++ app/jobs/import_items_from_tsv_job.rb
@@ -3,13 +3,14 @@ class ImportItemsFromTsvJob < Job
   queue_as :default
 
   ##
-  # @param args [Array] One-element array with the pathname of the TSV to
-  #                     ingest at position 0.
+  # @param args [Array] Two-element array with the pathname of the TSV to
+  #                     ingest at position 0 and its original filename at
+  #                     position 1.
   #
   def perform(*args)
-    self.task.update(status_text: 'Importing items from TSV')
+    self.task.update(status_text: 'Importing item metadata from TSV')
 
-    ItemTsvIngester.new.ingest_pathname(args[0], self.task)
+    ItemTsvIngester.new.ingest_pathname(args[0], args[1], self.task)
     Solr.instance.commit
 
     File.delete(args[0]) if File.exist?(args[0])
