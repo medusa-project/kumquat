114d38a2aab0c443f00459da96db2453be9814d2
Alex Dolski <alexd@n-ary.net>
2017-04-05 09:25:15 -0500

    Limit the number of collections to 20 to improve page load time (currently there are 44 appearing on digital.library)

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,7 @@
 class LandingController < WebsiteController
 
+  COLLECTIONS_LIMIT = 20
+
   ##
   # Responds to GET /
   #
@@ -10,7 +12,7 @@ class LandingController < WebsiteController
         client_ip(request.remote_ip).
         client_user(current_user).
         filter_queries(Collection::SolrFields::ACCESS_SYSTEMS => 'Medusa Digital Library').
-        limit(100)
+        limit(COLLECTIONS_LIMIT)
     @dls_collections = finder.to_a
 
     fresh_when(etag: @dls_collections) if Rails.env.production?
