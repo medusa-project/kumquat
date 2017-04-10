884feac928096d03a3c890e44591d6e116cde864
Alex Dolski <alexd@n-ary.net>
2017-04-10 12:36:45 -0500

    Fix text_viewer_for() returning non-UTF-8 string content that causes an ArgumentError in the view

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1596,7 +1596,10 @@ module ItemsHelper
     html = ''
     if binary
       begin
-        html += raw("<pre>#{File.read(binary.absolute_local_pathname)}</pre>")
+        str = File.read(binary.absolute_local_pathname).to_s
+        if str.valid_encoding?
+          html += raw("<pre>#{str}</pre>")
+        end
       rescue Errno::ENOENT # File not found
       end
     end
