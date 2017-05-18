09e14b7de9638f128ed56d1bfc7239e51994478e
Alex Dolski <alexd@n-ary.net>
2017-05-18 15:59:24 -0500

    Add architecture for asynchronous downloads (DLD-69)

---
+++ app/assets/javascripts/downloads.js
@@ -0,0 +1,22 @@
+/**
+ * @constructor
+ */
+var PTDownloadView = function() {
+
+    var init = function() {
+        setTimeout(function() {
+            window.location.reload();
+        }, 5000);
+    };
+    init();
+
+};
+
+var ready = function() {
+    if ($('body#preparing_download').length) {
+        PearTree.view = new PTDownloadView();
+    }
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
