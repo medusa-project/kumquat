fb8cc6a1359faa5026a106aaa13d6a7d9c1bb759
Alex Dolski <alexd@n-ary.net>
2017-07-12 14:01:25 -0500

    Make binaries.byte_size not-null

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710195351) do
+ActiveRecord::Schema.define(version: 20170712185802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,7 +89,7 @@ ActiveRecord::Schema.define(version: 20170710195351) do
     t.integer  "item_id"
     t.string   "repository_relative_pathname"
     t.string   "cfs_file_uuid"
-    t.decimal  "byte_size",                    precision: 15
+    t.decimal  "byte_size",                    precision: 15,                             null: false
     t.decimal  "width",                        precision: 6
     t.decimal  "height",                       precision: 6
     t.integer  "media_category"
