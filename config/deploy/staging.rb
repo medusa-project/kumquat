7b6b07d421cfcce438981a3679000e2de15075b2
Alex Dolski <alexd@n-ary.net>
2017-07-18 09:33:29 -0500

    Change the staging service account

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'kumquat-dev.library.illinois.edu', user: 'peartree',
+server 'kumquat-dev.library.illinois.edu', user: 'lib-medusa-dls',
        roles: %w{web app db}, primary: true
 
 # role-based syntax
