67de4160ee680c9497f2421a1d6077731ea1f345
Alex Dolski <alexd@n-ary.net>
2017-08-10 09:50:46 -0500

    Change the service user to lib-medusa-dls

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'kumquat-prod.library.illinois.edu', user: 'peartree',
+server 'kumquat-prod.library.illinois.edu', user: 'lib-medusa-dls',
        roles: %w{web app db}, primary: true
 
 # role-based syntax
