medusa-project/kumquat

commit 92fcbc4abe1237e1f866d7325772a7f3b9869651

Fix field-specific searching (DLD-125)

app/controllers/admin/items_controller.rb
@@ -111,7 +111,7 @@ module Admin
 
       finder = ItemFinder.new.
           collection_id(@collection.repository_id).
-          query(params[:q]).
+          query("#{params[:df]}:#{params[:q]}").
           include_children(false).
           include_unpublished(true).
           only_described(false).

app/querying/abstract_finder.rb
@@ -89,18 +89,31 @@ class AbstractFinder
   end
 
   ##
-  # @param string [String]
+  # @param string [String] `fieldname:query` or `query`
   # @return [self]
   #
   def query(string) # TODO: separate query() and query_field() methods
     if string.present?
       parts = string.split(':')
-      value = parts.last.gsub(' ', '*')
-      value = "(#{value} OR *#{value}*)"
       if parts.length > 1
-        @query = "#{parts.first}:#{value}"
+        field = parts.first
+        query = parts[1..parts.length].join(':')
       else
-        @query = value
+        field = nil
+        query = string
+      end
+
+      # The search-all field has a different type from all the rest, so the
+      # query syntax will have to be different.
+      if (!field.nil? and field == Item::SolrFields::SEARCH_ALL) or
+          (field.nil? and (@default_field.blank? or @default_field == Item::SolrFields::SEARCH_ALL))
+        query = query.gsub(' ', '*')
+        query = "(#{query} OR *#{query}*)"
+        @query = field ? "#{field}:#{query}" : query
+      else # We are searching a text_general field.
+        query = "(#{query} OR #{query}*)"
+        @query = '{!complexphrase inOrder=true}'
+        @query += field ? "#{field}:#{query}" : query
       end
     end
     self

commit e21e5c7d28dd094fef2462f1be160976428dc17b

Fix a bug introduced in 92fcbc4abe1237e1f866d7325772a7f3b9869651

app/controllers/admin/items_controller.rb
@@ -111,7 +111,7 @@ module Admin
 
       finder = ItemFinder.new.
           collection_id(@collection.repository_id).
-          query("#{params[:df]}:#{params[:q]}").
+          query(params[:q].present? ? "#{params[:df]}:#{params[:q]}" : nil).
           include_children(false).
           include_unpublished(true).
           only_described(false).