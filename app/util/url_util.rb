109901713db52d86d53ff82c9d2cc8c291d38e09
Alex Dolski <alexd@n-ary.net>
2017-08-11 10:22:21 -0500

    Add UrlUtil

---
+++ app/util/url_util.rb
@@ -0,0 +1,14 @@
+class UrlUtil
+
+  ##
+  # Parses the query portion of a URL into a hash.
+  #
+  # @param url [String,URI] URL
+  # @return [Hash<String,String>]
+  #
+  def self.parse_query(url)
+    url = URI.parse(url) unless url.kind_of?(URI)
+    Rack::Utils.parse_nested_query(url.query).stringify_keys || {}
+  end
+
+end
