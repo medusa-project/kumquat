d7d3488499301917197722e7a080d9c61fecba54
Alex Dolski <alexd@n-ary.net>
2017-09-20 11:23:31 -0500

    Uncomment

--- db/migrate/20170912195504_add_unique_index_on_binaries_repository_relative_pathname.rb
+++ db/migrate/20170912195504_add_unique_index_on_binaries_repository_relative_pathname.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexOnBinariesRepositoryRelativePathname < ActiveRecord::Migration[4.2]
   def change
-    #add_index :binaries, :repository_relative_pathname, unique: true
+    add_index :binaries, :repository_relative_pathname, unique: true
   end
 end
