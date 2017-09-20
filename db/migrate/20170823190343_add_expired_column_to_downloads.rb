6703f4b72a93af37a3591ece18d3a45035d510f8
Alex Dolski <alexd@n-ary.net>
2017-09-20 11:15:12 -0500

    Add bracketed versions

--- db/migrate/20170823190343_add_expired_column_to_downloads.rb
+++ db/migrate/20170823190343_add_expired_column_to_downloads.rb
@@ -1,4 +1,4 @@
-class AddExpiredColumnToDownloads < ActiveRecord::Migration
+class AddExpiredColumnToDownloads < ActiveRecord::Migration[4.2]
   def change
     add_column :downloads, :expired, :boolean, default: false
     add_index :downloads, :expired
--- db/migrate/20170823193243_add_ip_address_column_to_downloads.rb
+++ db/migrate/20170823193243_add_ip_address_column_to_downloads.rb
@@ -1,4 +1,4 @@
-class AddIpAddressColumnToDownloads < ActiveRecord::Migration
+class AddIpAddressColumnToDownloads < ActiveRecord::Migration[4.2]
   def change
     add_column :downloads, :ip_address, :string
     add_index :downloads, :ip_address
