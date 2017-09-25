medusa-project/kumquat

commit 39558f7f3130e48a536bda883397835dccbfc0ce

Fix incorrect limit in item_finder_for() (DLD-158)

app/controllers/items_controller.rb
@@ -592,7 +592,7 @@ class ItemsController < WebsiteController
           filter_queries(session[:fq]).
           sort(session[:sort]).
           start(session[:start]).
-          limit(Option::integer(Option::Keys::RESULTS_PER_PAGE))
+          limit(99999)
     end
   end
 

commit 393393044bd08ea891371ded91610da357160c32

Fix a regression introduced in 39558f7f3130e48a536bda883397835dccbfc0ce

app/controllers/items_controller.rb
@@ -592,7 +592,7 @@ class ItemsController < WebsiteController
           filter_queries(session[:fq]).
           sort(session[:sort]).
           start(session[:start]).
-          limit(99999)
+          limit(@collection&.free_form? ? 99999 : Option::integer(Option::Keys::RESULTS_PER_PAGE))
     end
   end
 