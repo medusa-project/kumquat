673faa862d77ed11dc4ae318c31c9ead7421cd08
Alex Dolski <alexd@n-ary.net>
2017-09-28 12:12:13 -0500

    Display a user's sets on the dashboard (DLD-134)

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ module Admin
   class DashboardController < ControlPanelController
 
     def index
+      @item_sets = current_user.item_sets.
+          sort_by{ |s| "#{s.collection.title} #{s.name}" }
     end
 
   end
