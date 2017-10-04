cc068a031f8990331a6655fb01af3c7037c09e6a
Alex Dolski <alexd@n-ary.net>
2017-10-04 08:45:37 -0500

    Add SessionsControllerTest

---
+++ test/integration/sessions_controller_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+
+  # create()
+
+  test 'signing in should redirect to the admin page by default' do
+    sign_in_as(users(:admin))
+    assert_redirected_to admin_root_path
+  end
+
+  test 'signing in should redirect to the requested URL if provided' do
+=begin TODO: get this to work
+    session[:return_to] = 'http://example.org/cats'
+    sign_in_as(users(:admin))
+    assert_redirected_to session[:return_to]
+=end
+  end
+
+  test 'signing in should set the session cookie' do
+    sign_in_as(users(:admin))
+    assert_not_nil session[:user]
+  end
+
+  # destroy()
+
+  test 'signing out should redirect to root' do
+    sign_in_as(users(:admin))
+    sign_out
+    assert_redirected_to root_url
+  end
+
+  test 'signing out should unset the session cookie' do
+    sign_in_as(users(:admin))
+    sign_out
+    assert_nil session[:user]
+  end
+
+  # new()
+
+  test 'new() should redirect to the sign-in path' do
+    get '/signin'
+    assert_redirected_to '/auth/developer'
+  end
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,4 +6,14 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  def sign_in_as(user)
+    post '/auth/developer/callback', params: {
+        name: user.username, email: "#{user.username}@example.org"
+    }
+  end
+
+  def sign_out
+    delete signout_path
+  end
+
 end
