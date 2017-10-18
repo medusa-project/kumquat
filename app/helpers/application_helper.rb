fd282914e22c71b3e37293c5607b907d7647519f
Unknown <alexd@n-ary.net>
2017-10-18 14:49:39 -0500

    Move icons into root image dir

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -186,21 +186,21 @@ module ApplicationHelper
   #
   def favicon_link_tags
     # https://uofi.app.box.com/v/Illinois-Logo/file/209399852568
-    html = "<link rel=\"apple-touch-icon\" sizes=\"57x57\" href=\"#{asset_url('apple-icon-57x57.png')}\">
-    <link rel=\"apple-touch-icon\" sizes=\"60x60\" href=\"#{asset_url('apple-icon-60x60.png')}\">
-    <link rel=\"apple-touch-icon\" sizes=\"72x72\" href=\"#{asset_url('apple-icon-72x72.png')}\">
-    <link rel=\"apple-touch-icon\" sizes=\"76x76\" href=\"#{asset_url('apple-icon-76x76.png')}\">
-    <link rel=\"apple-touch-icon\" sizes=\"114x114\" href=\"#{asset_url('apple-icon-114x114.png')}\">
-    <link rel=\"apple-touch-icon\" sizes=\"120x120\" href=\"#{asset_url('apple-icon-120x120.png')}\">
-    <link rel=\"apple-touch-icon\" sizes=\"144x144\" href=\"#{asset_url('apple-icon-144x144.png')}\">
-    <link rel=\"apple-touch-icon\" sizes=\"152x152\" href=\"#{asset_url('apple-icon-152x152.png')}\">
-    <link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"#{asset_url('apple-icon-180x180.png')}\">
-    <link rel=\"icon\" type=\"image/png\" sizes=\"192x192\" href=\"#{asset_url('android-icon-192x192.png')}\">
-    <link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"#{asset_url('favicon-32x32.png')}\">
-    <link rel=\"icon\" type=\"image/png\" sizes=\"96x96\" href=\"#{asset_url('favicon-96x96.png')}\">
-    <link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"#{asset_url('favicon-16x16.png')}\">
+    html = "<link rel=\"apple-touch-icon\" sizes=\"57x57\" href=\"#{image_url('apple-icon-57x57.png')}\">
+    <link rel=\"apple-touch-icon\" sizes=\"60x60\" href=\"#{image_url('apple-icon-60x60.png')}\">
+    <link rel=\"apple-touch-icon\" sizes=\"72x72\" href=\"#{image_url('apple-icon-72x72.png')}\">
+    <link rel=\"apple-touch-icon\" sizes=\"76x76\" href=\"#{image_url('apple-icon-76x76.png')}\">
+    <link rel=\"apple-touch-icon\" sizes=\"114x114\" href=\"#{image_url('apple-icon-114x114.png')}\">
+    <link rel=\"apple-touch-icon\" sizes=\"120x120\" href=\"#{image_url('apple-icon-120x120.png')}\">
+    <link rel=\"apple-touch-icon\" sizes=\"144x144\" href=\"#{image_url('apple-icon-144x144.png')}\">
+    <link rel=\"apple-touch-icon\" sizes=\"152x152\" href=\"#{image_url('apple-icon-152x152.png')}\">
+    <link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"#{image_url('apple-icon-180x180.png')}\">
+    <link rel=\"icon\" type=\"image/png\" sizes=\"192x192\" href=\"#{image_url('android-icon-192x192.png')}\">
+    <link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"#{image_url('favicon-32x32.png')}\">
+    <link rel=\"icon\" type=\"image/png\" sizes=\"96x96\" href=\"#{image_url('favicon-96x96.png')}\">
+    <link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"#{image_url('favicon-16x16.png')}\">
     <meta name=\"msapplication-TileColor\" content=\"#ffffff\">
-    <meta name=\"msapplication-TileImage\" content=\"#{asset_url('ms-icon-144x144.png')}\">
+    <meta name=\"msapplication-TileImage\" content=\"#{image_url('ms-icon-144x144.png')}\">
     <meta name=\"theme-color\" content=\"#ffffff\">"
     raw(html)
   end
