5fecf35ecb53b498f3fb5ac7e444fe8ccbba1b1e
Unknown <alexd@n-ary.net>
2017-10-24 17:58:44 -0500

    Duplicate omniauth changes from staging

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
     opts = YAML.load_file(File.join(Rails.root, 'config', 'peartree.yml'))[Rails.env]
-    provider :shibboleth, opts.select{ |k,v| k.to_s.start_with?('shibboleth') }.symbolize_keys
+    provider :shibboleth, opts.
+        select{ |k,v| k.to_s.start_with?('shibboleth_') }.
+        map{ |k,v| { k.to_s.gsub('shibboleth_', '').to_sym => v } }.
+        reduce(:merge)
   else
     provider :developer
   end
