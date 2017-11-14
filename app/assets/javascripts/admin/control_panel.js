0d4d7cb069d1634a35b19df25d7f39cd9eb148e5
Alex Dolski <alexd@n-ary.net>
2017-11-14 10:21:20 -0600

    Save the last-selected tab in a cookie, and open it when the page is reloaded (DLD-186)

--- app/assets/javascripts/admin/control_panel.js
+++ app/assets/javascripts/admin/control_panel.js
@@ -6,7 +6,18 @@
  */
 var PTControlPanel = function() {
 
-    this.init = function() {};
+    this.init = function() {
+        // Save the last-clicked tab in a cookie.
+        $('a[data-toggle="tab"]').on('click', function(e) {
+            Cookies.set('last_tab', $(e.target).attr('href'));
+        });
+
+        // Activate the latest tab, if it exists.
+        var last_tab = Cookies.get('last_tab');
+        if (last_tab) {
+            $('a[href="' + last_tab + '"]').click();
+        }
+    };
 
 };
 
