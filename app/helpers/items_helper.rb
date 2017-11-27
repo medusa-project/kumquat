medusa-project/kumquat

commit 8e56e8af85629e5dfcfb0cffa7dd237bdd291614

Eliminate ImageServerController (which proxied the image server to add a Content-Disposition header) as Cantaloupe 3.4 supports the ?response-content-disposition query argument

app/helpers/items_helper.rb
@@ -8,6 +8,66 @@ module ItemsHelper
   ##
   # @param binary [Binary]
   # @param options [Hash<Symbol,Object>]
+  # @option options [String] :region
+  # @option options [String] :size
+  # @option options [Integer] :rotation
+  # @option options [String] :color
+  # @option options [String] :content_disposition
+  # @option options [String] :filename
+  # @option options [Boolean] :cache
+  # @return [String, nil] Image URL, or nil if the binary is not compatible
+  #                       with the image server or safe for it to serve.
+  #
+  def binary_image_url(binary, options = {})
+    url = nil
+    query = {}
+
+    if binary.iiif_safe?
+      options[:region] = 'full' if options[:region].blank?
+      if options[:size].blank?
+        options[:size] = 'full'
+      elsif options[:size].to_i == options[:size]
+        options[:size] = "!#{options[:size]},#{options[:size]}"
+      end
+      options[:rotation] = 0 if options[:rotation].blank?
+      options[:color] = 'default' if options[:color].blank?
+      options[:format] = 'jpg' if options[:format].blank?
+
+      url = sprintf('%s/%s/%s/%d/%s.%s',
+                    binary.iiif_image_url,
+                    options[:region],
+                    options[:size],
+                    options[:rotation],
+                    options[:color],
+                    options[:format])
+
+      if options[:content_disposition].present?
+        if options[:content_disposition] == 'attachment'
+          if options[:filename].present?
+            filename = options[:filename]
+          else
+            filename = File.basename(binary.filename, File.extname(binary.filename)) +
+                '.' + options[:format]
+          end
+          value = "attachment; filename=\"#{filename}\""
+        else
+          value = options[:content_disposition]
+        end
+        query['response-content-disposition'] = value
+      end
+
+      if options.keys.include?(:cache) and !options[:cache]
+        query['cache'] = 'false'
+      end
+    end
+
+    url += '?' + query.to_query if query.keys.any?
+    url
+  end
+
+  ##
+  # @param binary [Binary]
+  # @param options [Hash<Symbol,Object>]
   # @option options [Boolean] :admin
   # @return [String]
   #
@@ -809,14 +869,20 @@ module ItemsHelper
     url = nil
     if Option::string(Option::Keys::SERVER_STATUS) != 'storage_offline'
       if entity.kind_of?(Binary) and entity.iiif_safe?
-        url = binary_image_url(entity, options[:size], options[:shape])
+        url = binary_image_url(entity,
+                               region: options[:shape],
+                               size: options[:size])
       elsif entity.kind_of?(Collection)
         bin = entity.effective_representative_image_binary
         if bin&.iiif_safe?
-          url = binary_image_url(bin, options[:size], options[:shape])
+          url = binary_image_url(bin,
+                                 region: options[:shape],
+                                 size: options[:size])
         end
       elsif entity.kind_of?(Item) and entity.effective_image_binary&.iiif_safe?
-        url = iiif_image_url(entity, options[:shape], options[:size])
+        url = iiif_image_url(entity,
+                             region: options[:shape],
+                             size: options[:size])
       end
     end
 
@@ -844,7 +910,7 @@ module ItemsHelper
   def thumbnail_url(entity, size = DEFAULT_THUMBNAIL_SIZE, shape = :default)
     url = nil
     if entity.kind_of?(Binary)
-      url = binary_image_url(entity, size, shape)
+      url = binary_image_url(entity, region: shape, size: size)
     elsif entity.kind_of?(Item)
       url = iiif_image_url(entity, shape, size)
     end
@@ -945,22 +1011,6 @@ module ItemsHelper
       </audio>"
     end
     raw(html)
-  end
-
-  ##
-  # @param binary [Binary]
-  # @param size [Integer]
-  # @param shape [Symbol] :default or :square
-  # @return [String, nil] Image URL or nil if the item is not an image
-  #
-  def binary_image_url(binary, size, shape = :default)
-    url = nil
-    if binary.is_image? or binary.is_pdf?
-      shape = (shape == :square) ? 'square' : 'full'
-      url = sprintf('%s/%s/!%d,%d/0/default.jpg',
-                    binary.iiif_image_url, shape, size, size)
-    end
-    url
   end
 
   ##

app/controllers/image_server_controller.rb
@@ -1,37 +1,0 @@
-class ImageServerController < WebsiteController
-
-  ##
-  # Streams an image from the image server, overriding the Content-Disposition
-  # response header to 'attachment'.
-  #
-  # Responds to GET /download-image/:identifier/:region/:scale/:rotation/:quality.:format
-  #
-  def download_image
-    binary = Binary.find_by_cfs_file_uuid(params[:identifier])
-    raise ActiveRecord::RecordNotFound unless binary
-
-    region = params[:region].gsub(/[^a-z0-9,]/, '')
-    size = params[:size].gsub(/[^a-z0-9,!]/, '')
-    rotation = params[:rotation].gsub(/[^0-9,!]/, '')
-    quality = params[:quality].gsub(/[^a-z]/, '')
-    format = params[:format].gsub(/[^a-z]/, '')
-
-    raise ArgumentError, 'Illegal region argument' if region != params[:region]
-    raise ArgumentError, 'Illegal size argument' if size != params[:size]
-    raise ArgumentError, 'Illegal rotation argument' if rotation != params[:rotation]
-    raise ArgumentError, 'Illegal quality argument' if quality != params[:quality]
-    raise ArgumentError, 'Illegal format argument' if format != params[:format]
-
-    url = sprintf("%s/%s/%s/%s/%s/%s.%s",
-                  ::Configuration.instance.iiif_url,
-                  CGI::escape(params[:identifier]),
-                  region, size, rotation, quality, format)
-
-    filename = File.basename(binary.filename, File.extname(binary.filename)) +
-        '.' + format
-    client = ImageServer.instance.client
-
-    send_data client.get(url).body, disposition: 'attachment', filename: filename
-  end
-
-end

app/helpers/application_helper.rb
@@ -94,7 +94,7 @@ module ApplicationHelper
         end
       end
       if bs
-        img_url = binary_image_url(bs, CARD_IMAGE_SIZE, :square)
+        img_url = binary_image_url(bs, region: 'square', size: CARD_IMAGE_SIZE)
       else
         case entity.class.to_s
           when 'Collection'

app/views/items/_download_table_rows.html.erb
@@ -15,7 +15,9 @@
   <td>
     <div class="btn-group">
       <% if item&.effective_image_binary&.iiif_image_identifier %>
-        <%= link_to download_image_url(item.effective_image_binary.iiif_image_identifier, 'full', 'full', 0, 'default', 'jpg'),
+        <%= link_to binary_image_url(item.effective_image_binary,
+                                     content_disposition: 'attachment',
+                                     cache: false),
                     class: 'btn btn-default btn-sm' do %>
           <i class="fa fa-image"></i> JPEG
         <% end %>

config/routes.rb
@@ -80,8 +80,6 @@ Rails.application.routes.draw do
   resources :downloads, only: :show, param: :key do
     match '/file', to: 'downloads#file', via: :get, as: 'file'
   end
-  match '/download-image/:identifier/:region/:size/:rotation/:quality',
-        to: 'image_server#download_image', via: :get, as: 'download_image'
   resources :favorites, only: :index
   resources :items, only: [:index, :show] do
     match '/treedata', to: 'items#item_tree_node', via: [:get, :post]

commit aca346bc290e6b3ac5feec5515efaf3e115b1be4

Fix a bug in thumbnail_tag() introduced in 8e56e8af85629e5dfcfb0cffa7dd237bdd291614

app/helpers/items_helper.rb
@@ -863,7 +863,7 @@ module ItemsHelper
   def thumbnail_tag(entity, options = {})
     options = {} unless options.kind_of?(Hash)
     options[:size] = DEFAULT_THUMBNAIL_SIZE unless options.keys.include?(:size)
-    options[:shape] = :default unless options.keys.include?(:shape)
+    options[:shape] = 'full' unless options.keys.include?(:shape)
     options[:lazy] = false unless options.keys.include?(:lazy)
 
     html = ''