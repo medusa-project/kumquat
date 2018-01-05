medusa-project/kumquat

commit 2071bb577263473839931bc279c206db24dcc723

Tighten a selector in FilterField to fix a bug that causes item checkboxes to reload the results in admin item list view

app/assets/javascripts/kumquat.js
@@ -173,7 +173,7 @@ var Application = {
         });
         // When form controls accompanying the filter field are changed,
         // resubmit the form via AJAX.
-        $('form.pt-filter select, form.pt-filter input[type=checkbox], ' +
+        $('form.pt-filter select, form.pt-filter .pt-facets input[type=checkbox], ' +
             'form.pt-filter input[type=radio]').on('change', function() {
             submitForm();
         });

commit 4255596180f99d4f23fce12c0e8e2931aa1e24d7

Fix a regression in 2071bb577263473839931bc279c206db24dcc723

app/assets/javascripts/kumquat.js
@@ -173,7 +173,7 @@ var Application = {
         });
         // When form controls accompanying the filter field are changed,
         // resubmit the form via AJAX.
-        $('form.pt-filter select, form.pt-filter .pt-facets input[type=checkbox], ' +
+        $('form.pt-filter select, ' +
             'form.pt-filter input[type=radio]').on('change', function() {
             submitForm();
         });