d1b28cb27c09e4c181c5e41559fe527bf46cee9e
Alex Dolski <alexd@n-ary.net>
2018-02-12 14:34:03 -0600

    Restore task list auto-refresh

--- app/assets/javascripts/admin/tasks.js
+++ app/assets/javascripts/admin/tasks.js
@@ -8,6 +8,8 @@ var PTAdminTasksView = function() {
     this.init = function() {
         new Application.FilterField();
 
+        new TaskRefresher().start();
+
         $('#pt-task-panel').on('show.bs.modal', function(event) {
             var modal = $(this);
             var button = $(event.relatedTarget);
@@ -25,6 +27,40 @@ var PTAdminTasksView = function() {
                 }
             });
         });
+    };
+
+    var TaskRefresher = function() {
+
+        var FREQUENCY = 5000;
+
+        var refreshTimer;
+
+        var refresh = function() {
+            console.debug('Refreshing task list...');
+
+            var current_page = $('.pagination li.active > a:first')
+                .text().replace(/[/\D]/g, '');
+            var start = (current_page - 1) * $('[name=pt-limit]').val();
+            var url = TASKS_URL + '?start=' + start;
+
+            $.ajax({
+                url: url,
+                data: $('form.pt-filter').serialize(),
+                success: function (data) {
+                    // this will be handled by index.js.erb
+                }
+            });
+        };
+
+        this.start = function() {
+            refreshTimer = setInterval(refresh, FREQUENCY);
+            refresh();
+        };
+
+        this.stop = function() {
+            clearInterval(refreshTimer);
+        }
+
     };
 
 };
