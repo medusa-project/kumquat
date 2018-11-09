1fcb3f0b81066e4335c2e9a54810ae21e3fa4198
Alex Dolski <alexd@n-ary.net>
2018-11-09 15:44:18 -0600

    show() supports HTTP ranges

--- app/controllers/binaries_controller.rb
+++ app/controllers/binaries_controller.rb
@@ -13,7 +13,35 @@ class BinariesController < WebsiteController
     if request.format == 'json'
       render json: @binary.decorate
     else
-      send_file(@binary.absolute_local_pathname)
+      file_begin = 0
+      file_size = @binary.byte_size
+      file_end = file_size - 1
+
+      if !request.headers['Range']
+        status_code = '200 OK'
+      else
+        status_code = '206 Partial Content'
+        match = request.headers['Range'].match(/bytes=(\d+)-(\d*)/)
+        if match
+          file_begin = match[1]
+          file_end = match[2] if match[2] && match[2].present?
+        end
+        response.header['Content-Range'] = sprintf('bytes %d-%d/%d',
+                                                   file_begin, file_end, file_size)
+      end
+      response.header['Content-Length'] = (file_end.to_i - file_begin.to_i + 1).to_s
+      response.header['Last-Modified'] = @binary.updated_at.to_s
+      response.header['Cache-Control'] = 'public, must-revalidate, max-age=0'
+      response.header['Pragma'] = 'no-cache'
+      response.header['Accept-Ranges'] =  'bytes'
+      response.header['Content-Transfer-Encoding'] = 'binary'
+      send_file(@binary.absolute_local_pathname,
+                filename: File.basename(@binary.repository_relative_pathname),
+                type: @binary.media_type,
+                disposition: 'attachment',
+                status: status_code,
+                stream: 'true',
+                buffer_size: 8192)
     end
   end
 
