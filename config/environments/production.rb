861279a2487d9170e7f369fbbf1ffa0c3ce93b08
Alex Dolski <alexd@n-ary.net>
2019-02-25 12:35:46 -0600

    Unset the ActiveRecord logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,8 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.active_record.logger = nil
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
