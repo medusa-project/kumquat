a15f7073909454b37794182415a0ba5da5b17881
Alex Dolski <alexd@n-ary.net>
2019-03-22 15:09:02 -0500

    Scroll up on page change (DLD-285)

--- app/assets/javascripts/collections.js
+++ app/assets/javascripts/collections.js
@@ -8,44 +8,12 @@ var PTCollectionsView = function() {
     var init = function() {
         new Application.FilterField();
 
-        var addFacetEventListeners = function() {
-            $('[name="pt-facet-term"]').on('change', function() {
-                // Create hidden element counterparts of each checked checkbox,
-                // as checkboxes can't have values.
-                var form = $(this).parents('form:first');
-                form.find('[name="fq[]"]').remove();
-                form.find('[name=pt-facet-term]:checked').each(function() {
-                    var input = $('<input type="hidden" name="fq[]">');
-                    input.val($(this).data('query'));
-                    form.append(input);
-                });
-
-                var query = form.serialize();
-
-                window.history.pushState(
-                    { "html": null, "pageTitle": document.title },
-                    '', '/collections?' + query);
-
-                console.debug("Requesting /collections?" + query);
-
-                $.ajax({
-                    url: '/collections?',
-                    method: 'GET',
-                    data: query,
-                    dataType: 'script',
-                    success: function(result) {
-                        eval(result);
-                    }
-                });
-            });
-        };
-
         // When the filter field has been updated, it will recreate the facets.
         $(document).ajaxSuccess(function(event, request) {
-            addFacetEventListeners();
+            attachEventListeners();
             updateTitle();
         });
-        addFacetEventListeners();
+        attachEventListeners();
         updateTitle();
     }; init();
 
@@ -58,7 +26,7 @@ var PTCollectionsView = function() {
         // ... and then set the page title to the English-ized list.
         var text = '';
         var count = $('#pt-count');
-        if (repositories.length == 1) {
+        if (repositories.length === 1) {
             text += repositories[0];
         } else if (repositories.length > 1) {
             var last = repositories.pop();
@@ -71,6 +39,42 @@ var PTCollectionsView = function() {
         title.text(text + ' ');
         title.append(count);
     }
+
+    function attachEventListeners() {
+        $('.pagination a').off().on('click', function() {
+            $('form.pt-filter')[0].scrollIntoView({behavior: "smooth", block: "start"});
+        });
+
+        $('[name="pt-facet-term"]').off().on('change', function() {
+            // Create hidden element counterparts of each checked checkbox,
+            // as checkboxes can't have values.
+            var form = $(this).parents('form:first');
+            form.find('[name="fq[]"]').remove();
+            form.find('[name=pt-facet-term]:checked').each(function() {
+                var input = $('<input type="hidden" name="fq[]">');
+                input.val($(this).data('query'));
+                form.append(input);
+            });
+
+            var query = form.serialize();
+
+            window.history.pushState(
+                { "html": null, "pageTitle": document.title },
+                '', '/collections?' + query);
+
+            console.debug("Requesting /collections?" + query);
+
+            $.ajax({
+                url: '/collections?',
+                method: 'GET',
+                data: query,
+                dataType: 'script',
+                success: function(result) {
+                    eval(result);
+                }
+            });
+        });
+    };
 
 };
 
