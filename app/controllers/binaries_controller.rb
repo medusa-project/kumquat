7dc363c3d701f20194d744cafa16f03bb96cdc08
Alex Dolski <alexd@n-ary.net>
2019-10-23 14:24:01 -0500

    Improve AuthorizationError handling

--- app/controllers/binaries_controller.rb
+++ app/controllers/binaries_controller.rb
@@ -4,6 +4,8 @@ class BinariesController < WebsiteController
 
   before_action :load_binary, :authorize_item
 
+  rescue_from AuthorizationError, with: :rescue_unauthorized
+
   ##
   # Retrieves a binary.
   #
@@ -30,6 +32,11 @@ class BinariesController < WebsiteController
   def load_binary
     @binary = Binary.find_by_cfs_file_uuid(params[:id])
     raise ActiveRecord::RecordNotFound unless @binary
+  end
+
+  def rescue_unauthorized
+    render plain: 'You are not authorized to access this binary.',
+           status: :forbidden
   end
 
 end
