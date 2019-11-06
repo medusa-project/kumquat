medusa-project/kumquat

commit 0880dee3b043c821b155fa6f60b81ff73944deb4

Fix filtering in admin collections view

app/helpers/items_helper.rb
@@ -550,7 +550,7 @@ module ItemsHelper
   # @param max_links [Integer] (ideally odd)
   #
   def paginate_agent_items(agent, count, per_page, current_page, max_links = 9)
-    do_paginate(count, per_page, current_page, max_links, agent,
+    do_paginate(count, per_page, current_page, [], max_links, agent,
                 :agent_item)
   end
 
@@ -563,7 +563,10 @@ module ItemsHelper
   # @param max_links [Integer] (ideally odd)
   #
   def paginate_items(count, per_page, current_page, max_links = 9)
-    do_paginate(count, per_page, current_page, max_links)
+    do_paginate(count, per_page, current_page,
+                ItemsController::PERMITTED_PARAMS +
+                    Admin::ItemsController::PERMITTED_PARAMS,
+                max_links)
   end
 
   ##
@@ -1180,27 +1183,28 @@ module ItemsHelper
   # @param count [Integer] Total number of items in the result set
   # @param per_page [Integer]
   # @param current_page [Integer]
+  # @param permitted_params [ActionController::Parameters]
   # @param max_links [Integer] (ideally odd)
   # @param owning_entity [Item]
   # @param item_variant [Item::Variants, Symbol, nil] One of the Item::Variants
   #                     constants, or :agent_item, or nil.
   #
-  def do_paginate(count, per_page, current_page,
+  def do_paginate(count, per_page, current_page, permitted_params,
                   max_links = ApplicationHelper::MAX_PAGINATION_LINKS,
                   owning_entity = nil, item_variant = nil)
     return '' if count <= per_page
-    num_pages = (count / per_page.to_f).ceil
-    first_page = [1, current_page - (max_links / 2.0).floor].max
-    last_page = [first_page + max_links - 1, num_pages].min
-    first_page = last_page - max_links + 1 if
-        last_page - first_page < max_links and num_pages > max_links
-    prev_page = [1, current_page - 1].max
-    next_page = [last_page, current_page + 1].min
-    prev_start = (prev_page - 1) * per_page
-    next_start = (next_page - 1) * per_page
-    last_start = (num_pages - 1) * per_page
-    allowed_params = params.permit(ItemsController::PERMITTED_PARAMS +
-                                       Admin::ItemsController::PERMITTED_PARAMS).except(:start)
+    num_pages      = (count / per_page.to_f).ceil
+    first_page     = [1, current_page - (max_links / 2.0).floor].max
+    last_page      = [first_page + max_links - 1, num_pages].min
+    if last_page - first_page < max_links and num_pages > max_links
+      first_page     = last_page - max_links + 1
+    end
+    prev_page      = [1, current_page - 1].max
+    next_page      = [last_page, current_page + 1].min
+    prev_start     = (prev_page - 1) * per_page
+    next_start     = (next_page - 1) * per_page
+    last_start     = (num_pages - 1) * per_page
+    allowed_params = permitted_params.except(:start)
 
     case item_variant
       when :agent_item

app/controllers/admin/collections_controller.rb
@@ -2,6 +2,8 @@ module Admin
 
   class CollectionsController < ControlPanelController
 
+    PERMITTED_PARAMS = [:q, :public_in_medusa, :published_in_dls, :start]
+
     before_action :modify_collections_rbac, only: [:edit, :update, :sync]
 
     ##

app/helpers/admin_helper.rb
@@ -315,6 +315,20 @@ module AdminHelper
     html.string
   end
 
+  ##
+  # Returns pagination for collection results view.
+  #
+  # @param count [Integer]
+  # @param per_page [Integer]
+  # @param current_page [Integer]
+  # @param max_links [Integer] (ideally odd)
+  #
+  def admin_paginate_collections(count, per_page, current_page, max_links = 9)
+    do_paginate(count, per_page, current_page,
+                params.permit(Admin::CollectionsController::PERMITTED_PARAMS),
+                max_links)
+  end
+
   private
 
   ##

app/views/admin/collections/_collections.html.erb
@@ -1,6 +1,6 @@
 <div class="row justify-content-md-center">
   <div class="col-md-auto">
-    <%= paginate_items(@count, @limit, @current_page) %>
+    <%= admin_paginate_collections(@count, @limit, @current_page) %>
   </div>
 </div>
 
@@ -35,6 +35,6 @@
 
 <div class="row justify-content-md-center">
   <div class="col-md-auto">
-    <%= paginate_items(@count, @limit, @current_page) %>
+    <%= admin_paginate_collections(@count, @limit, @current_page) %>
   </div>
 </div>

commit 8e3a484fb29deaf8dcde79418e6c8330664c3184

Fix bug introduced in 0880dee3b043c821b155fa6f60b81ff73944deb4

app/helpers/items_helper.rb
@@ -564,8 +564,8 @@ module ItemsHelper
   #
   def paginate_items(count, per_page, current_page, max_links = 9)
     do_paginate(count, per_page, current_page,
-                ItemsController::PERMITTED_PARAMS +
-                    Admin::ItemsController::PERMITTED_PARAMS,
+                params.permit(ItemsController::PERMITTED_PARAMS +
+                    Admin::ItemsController::PERMITTED_PARAMS),
                 max_links)
   end
 