medusa-project/kumquat

commit d7290b470be08e0a9d05f6436e4dfa8ea6cdc051

Copy spinners and fading-in thumbnails from metaslurp

app/helpers/application_helper.rb
@@ -130,8 +130,9 @@ module ApplicationHelper
       html << '<li class="media my-4">'
 
       # Thumbnail area
+      html <<   '<div class="dl-thumbnail-container">'
       if options[:show_checkboxes]
-        html << check_box_tag('dl-selected-items[]', entity.repository_id)
+        html <<   check_box_tag('dl-selected-items[]', entity.repository_id)
       end
 
       if options[:link_to_admin] and entity.kind_of?(Item)
@@ -139,10 +140,14 @@ module ApplicationHelper
       else
         link_target = polymorphic_path(entity)
       end
-      html << link_to(link_target, class: 'dl-thumbnail-link') do
+      html << link_to(link_target) do
         thumbnail_tag(entity.effective_representative_entity,
                       shape: :square)
       end
+      # N.B.: this was made by https://loading.io with the following settings:
+      # rolling, color: #cacaca, radius: 25, stroke width: 10, speed: 5, size: 150
+      html <<   image_tag('thumbnail-spinner.svg', class: 'dl-load-indicator')
+      html << '</div>'
 
       html << '<div class="media-body">'
 

app/helpers/items_helper.rb
@@ -920,9 +920,10 @@ module ItemsHelper
     if url
       # No alt because it may appear in a huge font size if the image is 404.
       if options[:lazy]
-        html << lazy_image_tag(url, class: 'dl-thumbnail', alt: '')
+        html << lazy_image_tag(url, class: 'dl-thumbnail mr-3', alt: '')
       else
-        html << image_tag(url, class: 'dl-thumbnail', alt: '')
+        html << image_tag(url, class: 'dl-thumbnail mr-3', alt: '',
+                          data: { location: 'remote' })
       end
     else
       html << icon_for(entity) # ApplicationHelper

app/assets/images/thumbnail-spinner.svg
@@ -1,0 +1,1 @@
+<svg width="150px"  height="150px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-rolling" style="background-image: none; background-position: initial initial; background-repeat: initial initial;"><circle cx="50" cy="50" fill="none" ng-attr-stroke="{{config.color}}" ng-attr-stroke-width="{{config.width}}" ng-attr-r="{{config.radius}}" ng-attr-stroke-dasharray="{{config.dasharray}}" stroke="#f0f0f0" stroke-width="10" r="25" stroke-dasharray="117.80972450961724 41.269908169872416"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="5s" begin="0s" repeatCount="indefinite"></animateTransform></circle></svg>

app/assets/javascripts/admin/items.js
@@ -261,6 +261,8 @@ var PTAdminItemsView = function() {
     };
 
     this.attachEventListeners = function() {
+        Application.initThumbnails();
+
         $('.pagination a').on('click', function() {
             $('form.dl-filter')[0].scrollIntoView({behavior: "smooth", block: "start"});
         });

app/assets/javascripts/items.js
@@ -418,6 +418,8 @@ var PTItemsView = function() {
      * This needs to be public as it's called from index.js.
      */
     this.attachEventListeners = function() {
+        Application.initThumbnails();
+
         $('.pagination a').on('click', function() {
             $('form.dl-filter')[0].scrollIntoView({behavior: "smooth", block: "start"});
         });

app/assets/javascripts/kumquat.js
@@ -2,8 +2,6 @@ var Application = {
 
     /**
      * Enables the facets returned by one of the facets_as_x() helpers.
-     *
-     * @constructor
      */
     initFacets: function() {
         var addFacetEventListeners = function() {
@@ -41,6 +39,19 @@ var Application = {
             addFacetEventListeners();
         });
         addFacetEventListeners();
+    },
+
+    initThumbnails: function() {
+        $('.dl-thumbnail-container img[data-location="remote"]').one('load', function() {
+            $(this).parent().next('.dl-load-indicator').hide();
+            $(this).animate({'opacity': 1}, 300);
+        }).on('error', function() {
+            $(this).parent().next('.dl-load-indicator').hide();
+        }).each(function() {
+            if (this.complete) {
+                $(this).trigger('load');
+            }
+        });
     },
 
     /**

app/assets/stylesheets/admin/items.scss
@@ -2,6 +2,9 @@ body#admin_items_index {
   div.dl-filter {
     margin-bottom: 1em;
   }
+  .dl-thumbnail-container {
+    margin-right: 1em;
+  }
 }
 
 body#admin_items_show {

app/assets/stylesheets/sitewide.scss
@@ -101,30 +101,52 @@ div.alert ul {
 .dl-results {
   input[type=checkbox] {
     margin-right: 1em;
-    margin-top: 70px;
   }
-  .dl-thumbnail-link {
-    color: #808080;
-    margin: 0 1em 0 0;
+  .dl-thumbnail-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 190px;
+    height: 180px;
     img {
-      max-width: 160px;
+      max-height: 180px;
+      max-width: 180px;
     }
-    svg {
-      width: 160px;
-      height: 160px;
+    img[data-location="remote"] {
+      opacity: 0; // will be displayed onload via JS
+    }
+    img[data-type="svg"] {
+      opacity: 0.4;
+      padding: 0.8em;
     }
   }
-  h5 {
-    font-size: 1.2em;
-    font-weight: 500;
-  }
-  .dl-info-line {
-    color: #808080;
-    display: block;
-    font-size: 0.9em;
-    margin-bottom: 0.2em;
-    a {
-      color: #808080;
+  .media-body {
+    .mt-0 {
+      margin-bottom: 0.25em;
+    }
+    .dl-info-line {
+      color: gray;
+      display: block;
+      font-size: 0.9em;
+      padding: 0.2em 0.2em 0.8em 0;
+      a {
+        color: gray;
+        text-decoration: none;
+      }
+      a:hover {
+        text-decoration: underline;
+      }
+    }
+    .collapse, .collapsing {
+      font-size: 0.9em;
+      padding-top: 0.4em;
+      dd {
+        ul {
+          margin-left: 0.5em;
+          padding-left: 0.5em;
+          list-style-type: disc;
+        }
+      }
     }
   }
 }

commit f9fe8b5cb64c8db50b0d6e27b428cf10b4584132

Fix a regression from d7290b470be08e0a9d05f6436e4dfa8ea6cdc051

app/assets/images/fontawesome-circle.svg
@@ -1,0 +1,2 @@
+<?xml version="1.0" encoding="utf-8"?>
+<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M896 352q-148 0-273 73t-198 198-73 273 73 273 198 198 273 73 273-73 198-198 73-273-73-273-198-198-273-73zm768 544q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"/></svg>

app/assets/images/fontawesome-cube.svg
@@ -1,0 +1,2 @@
+<?xml version="1.0" encoding="utf-8"?>
+<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M896 1629l640-349v-636l-640 233v752zm-64-865l698-254-698-254-698 254zm832-252v768q0 35-18 65t-49 47l-704 384q-28 16-61 16t-61-16l-704-384q-31-17-49-47t-18-65v-768q0-40 23-73t61-47l704-256q22-8 44-8t44 8l704 256q38 14 61 47t23 73z"/></svg>

app/assets/images/fontawesome-cubes.svg
@@ -1,0 +1,2 @@
+<?xml version="1.0" encoding="utf-8"?>
+<svg width="2304" height="1792" viewBox="0 0 2304 1792" xmlns="http://www.w3.org/2000/svg"><path d="M640 1632l384-192v-314l-384 164v342zm-64-454l404-173-404-173-404 173zm1088 454l384-192v-314l-384 164v342zm-64-454l404-173-404-173-404 173zm-448-293l384-165v-266l-384 164v267zm-64-379l441-189-441-189-441 189zm1088 518v416q0 36-19 67t-52 47l-448 224q-25 14-57 14t-57-14l-448-224q-4-2-7-4-2 2-7 4l-448 224q-25 14-57 14t-57-14l-448-224q-33-16-52-47t-19-67v-416q0-38 21.5-70t56.5-48l434-186v-400q0-38 21.5-70t56.5-48l448-192q23-10 50-10t50 10l448 192q35 16 56.5 48t21.5 70v400l434 186q36 16 57 48t21 70z"/></svg>

app/assets/images/fontawesome-file-alt.svg
@@ -1,0 +1,2 @@
+<?xml version="1.0" encoding="utf-8"?>
+<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1596 380q28 28 48 76t20 88v1152q0 40-28 68t-68 28h-1344q-40 0-68-28t-28-68v-1600q0-40 28-68t68-28h896q40 0 88 20t76 48zm-444-244v376h376q-10-29-22-41l-313-313q-12-12-41-22zm384 1528v-1024h-416q-40 0-68-28t-28-68v-416h-768v1536h1280zm-1024-864q0-14 9-23t23-9h704q14 0 23 9t9 23v64q0 14-9 23t-23 9h-704q-14 0-23-9t-9-23v-64zm736 224q14 0 23 9t9 23v64q0 14-9 23t-23 9h-704q-14 0-23-9t-9-23v-64q0-14 9-23t23-9h704zm0 256q14 0 23 9t9 23v64q0 14-9 23t-23 9h-704q-14 0-23-9t-9-23v-64q0-14 9-23t23-9h704z"/></svg>

app/assets/images/fontawesome-file-pdf.svg
@@ -1,0 +1,2 @@
+<?xml version="1.0" encoding="utf-8"?>
+<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1596 380q28 28 48 76t20 88v1152q0 40-28 68t-68 28h-1344q-40 0-68-28t-28-68v-1600q0-40 28-68t68-28h896q40 0 88 20t76 48zm-444-244v376h376q-10-29-22-41l-313-313q-12-12-41-22zm384 1528v-1024h-416q-40 0-68-28t-28-68v-416h-768v1536h1280zm-514-593q33 26 84 56 59-7 117-7 147 0 177 49 16 22 2 52 0 1-1 2l-2 2v1q-6 38-71 38-48 0-115-20t-130-53q-221 24-392 83-153 262-242 262-15 0-28-7l-24-12q-1-1-6-5-10-10-6-36 9-40 56-91.5t132-96.5q14-9 23 6 2 2 2 4 52-85 107-197 68-136 104-262-24-82-30.5-159.5t6.5-127.5q11-40 42-40h22q23 0 35 15 18 21 9 68-2 6-4 8 1 3 1 8v30q-2 123-14 192 55 164 146 238zm-576 411q52-24 137-158-51 40-87.5 84t-49.5 74zm398-920q-15 42-2 132 1-7 7-44 0-3 7-43 1-4 4-8-1-1-1-2-1-2-1-3-1-22-13-36 0 1-1 2v2zm-124 661q135-54 284-81-2-1-13-9.5t-16-13.5q-76-67-127-176-27 86-83 197-30 56-45 83zm646-16q-24-24-140-24 76 28 124 28 14 0 18-1 0-1-2-3z"/></svg>

app/assets/images/fontawesome-file.svg
@@ -1,0 +1,2 @@
+<?xml version="1.0" encoding="utf-8"?>
+<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1596 380q28 28 48 76t20 88v1152q0 40-28 68t-68 28h-1344q-40 0-68-28t-28-68v-1600q0-40 28-68t68-28h896q40 0 88 20t76 48zm-444-244v376h376q-10-29-22-41l-313-313q-12-12-41-22zm384 1528v-1024h-416q-40 0-68-28t-28-68v-416h-768v1536h1280z"/></svg>

app/assets/images/fontawesome-film.svg
@@ -1,0 +1,2 @@
+<?xml version="1.0" encoding="utf-8"?>
+<svg width="2048" height="1792" viewBox="0 0 2048 1792" xmlns="http://www.w3.org/2000/svg"><path d="M448 1600v-128q0-26-19-45t-45-19h-128q-26 0-45 19t-19 45v128q0 26 19 45t45 19h128q26 0 45-19t19-45zm0-384v-128q0-26-19-45t-45-19h-128q-26 0-45 19t-19 45v128q0 26 19 45t45 19h128q26 0 45-19t19-45zm0-384v-128q0-26-19-45t-45-19h-128q-26 0-45 19t-19 45v128q0 26 19 45t45 19h128q26 0 45-19t19-45zm1024 768v-512q0-26-19-45t-45-19h-768q-26 0-45 19t-19 45v512q0 26 19 45t45 19h768q26 0 45-19t19-45zm-1024-1152v-128q0-26-19-45t-45-19h-128q-26 0-45 19t-19 45v128q0 26 19 45t45 19h128q26 0 45-19t19-45zm1408 1152v-128q0-26-19-45t-45-19h-128q-26 0-45 19t-19 45v128q0 26 19 45t45 19h128q26 0 45-19t19-45zm-384-768v-512q0-26-19-45t-45-19h-768q-26 0-45 19t-19 45v512q0 26 19 45t45 19h768q26 0 45-19t19-45zm384 384v-128q0-26-19-45t-45-19h-128q-26 0-45 19t-19 45v128q0 26 19 45t45 19h128q26 0 45-19t19-45zm0-384v-128q0-26-19-45t-45-19h-128q-26 0-45 19t-19 45v128q0 26 19 45t45 19h128q26 0 45-19t19-45zm0-384v-128q0-26-19-45t-45-19h-128q-26 0-45 19t-19 45v128q0 26 19 45t45 19h128q26 0 45-19t19-45zm128-160v1344q0 66-47 113t-113 47h-1600q-66 0-113-47t-47-113v-1344q0-66 47-113t113-47h1600q66 0 113 47t47 113z"/></svg>

app/assets/images/fontawesome-folder-open.svg
@@ -1,0 +1,2 @@
+<?xml version="1.0" encoding="utf-8"?>
+<svg width="2048" height="1792" viewBox="0 0 2048 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1845 931q0-35-53-35h-1088q-40 0-85.5 21.5t-71.5 52.5l-294 363q-18 24-18 40 0 35 53 35h1088q40 0 86-22t71-53l294-363q18-22 18-39zm-1141-163h768v-160q0-40-28-68t-68-28h-576q-40 0-68-28t-28-68v-64q0-40-28-68t-68-28h-320q-40 0-68 28t-28 68v853l256-315q44-53 116-87.5t140-34.5zm1269 163q0 62-46 120l-295 363q-43 53-116 87.5t-140 34.5h-1088q-92 0-158-66t-66-158v-960q0-92 66-158t158-66h320q92 0 158 66t66 158v32h544q92 0 158 66t66 158v160h192q54 0 99 24.5t67 70.5q15 32 15 68z"/></svg>

app/assets/images/fontawesome-image.svg
@@ -1,0 +1,2 @@
+<?xml version="1.0" encoding="utf-8"?>
+<svg width="2048" height="1792" viewBox="0 0 2048 1792" xmlns="http://www.w3.org/2000/svg"><path d="M704 576q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm1024 384v448h-1408v-192l320-320 160 160 512-512zm96-704h-1600q-13 0-22.5 9.5t-9.5 22.5v1216q0 13 9.5 22.5t22.5 9.5h1600q13 0 22.5-9.5t9.5-22.5v-1216q0-13-9.5-22.5t-22.5-9.5zm160 32v1216q0 66-47 113t-113 47h-1600q-66 0-113-47t-47-113v-1216q0-66 47-113t113-47h1600q66 0 113 47t47 113z"/></svg>

app/assets/images/fontawesome-user-circle.svg
@@ -1,0 +1,2 @@
+<?xml version="1.0" encoding="utf-8"?>
+<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1523 1339q-22-155-87.5-257.5t-184.5-118.5q-67 74-159.5 115.5t-195.5 41.5-195.5-41.5-159.5-115.5q-119 16-184.5 118.5t-87.5 257.5q106 150 271 237.5t356 87.5 356-87.5 271-237.5zm-243-699q0-159-112.5-271.5t-271.5-112.5-271.5 112.5-112.5 271.5 112.5 271.5 271.5 112.5 271.5-112.5 112.5-271.5zm512 256q0 182-71 347.5t-190.5 286-285.5 191.5-349 71q-182 0-348-71t-286-191-191-286-71-348 71-348 191-286 286-191 348-71 348 71 286 191 191 286 71 348z"/></svg>

app/assets/images/fontawesome-user.svg
@@ -1,0 +1,2 @@
+<?xml version="1.0" encoding="utf-8"?>
+<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1536 1399q0 109-62.5 187t-150.5 78h-854q-88 0-150.5-78t-62.5-187q0-85 8.5-160.5t31.5-152 58.5-131 94-89 134.5-34.5q131 128 313 128t313-128q76 0 134.5 34.5t94 89 58.5 131 31.5 152 8.5 160.5zm-256-887q0 159-112.5 271.5t-271.5 112.5-271.5-112.5-112.5-271.5 112.5-271.5 271.5-112.5 271.5 112.5 112.5 271.5z"/></svg>

app/assets/images/fontawesome-volume-up.svg
@@ -1,0 +1,2 @@
+<?xml version="1.0" encoding="utf-8"?>
+<svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M832 352v1088q0 26-19 45t-45 19-45-19l-333-333h-262q-26 0-45-19t-19-45v-384q0-26 19-45t45-19h262l333-333q19-19 45-19t45 19 19 45zm384 544q0 76-42.5 141.5t-112.5 93.5q-10 5-25 5-26 0-45-18.5t-19-45.5q0-21 12-35.5t29-25 34-23 29-36 12-56.5-12-56.5-29-36-34-23-29-25-12-35.5q0-27 19-45.5t45-18.5q15 0 25 5 70 27 112.5 93t42.5 142zm256 0q0 153-85 282.5t-225 188.5q-13 5-25 5-27 0-46-19t-19-45q0-39 39-59 56-29 76-44 74-54 115.5-135.5t41.5-173.5-41.5-173.5-115.5-135.5q-20-15-76-44-39-20-39-59 0-26 19-45t45-19q13 0 26 5 140 59 225 188.5t85 282.5zm256 0q0 230-127 422.5t-338 283.5q-13 5-26 5-26 0-45-19t-19-45q0-36 39-59 7-4 22.5-10.5t22.5-10.5q46-25 82-51 123-91 192-227t69-289-69-289-192-227q-36-26-82-51-7-4-22.5-10.5t-22.5-10.5q-39-23-39-59 0-26 19-45t45-19q13 0 26 5 211 91 338 283.5t127 422.5z"/></svg>

app/helpers/application_helper.rb
@@ -292,6 +292,56 @@ module ApplicationHelper
   end
 
   ##
+  # N.B.: {icon_for} should generally be used instead.
+  #
+  def fontawesome_icon_for(entity)
+    icon = %w(fas cube)
+    if entity == Item
+      icon = %w(fas cube)
+    elsif entity.kind_of?(Item)
+      viewer_binary = entity.effective_viewer_binary
+      if viewer_binary&.is_audio?
+        icon = %w(fas volume-up)
+      elsif viewer_binary&.is_image?
+        icon = %w(far image)
+      elsif viewer_binary&.is_pdf?
+        icon = %w(far file-pdf)
+      elsif viewer_binary&.is_text? or viewer_binary&.is_document?
+        icon = %w(far file-alt)
+      elsif viewer_binary&.is_video?
+        icon = %w(fas film)
+      elsif entity.variant == Item::Variants::DIRECTORY
+        icon = %w(far folder-open)
+      elsif entity.variant == Item::Variants::FILE
+        icon = %w(far file)
+      elsif entity.items.any?
+        icon = %w(fas cubes)
+      end
+    elsif entity.kind_of?(Binary)
+      if entity.is_audio?
+        icon = %w(fas volume-up)
+      elsif entity.is_image?
+        icon = %w(fas image)
+      elsif entity.is_pdf?
+        icon = %w(far file-pdf)
+      elsif entity.is_text? or entity.is_document?
+        icon = %w(far file-alt)
+      elsif entity.is_video?
+        icon = %w(fas film)
+      end
+    elsif entity == Collection or entity.kind_of?(Collection)
+      icon = %w(far folder-open)
+    elsif entity == Agent or entity.kind_of?(Agent)
+      icon = %w(fas user-circle)
+    elsif entity == ItemSet or entity.kind_of?(ItemSet)
+      icon = %w(far circle)
+    elsif entity == User or entity.kind_of?(User)
+      icon = %w(fas user)
+    end
+    icon
+  end
+
+  ##
   # Returns the most appropriate icon for the given object, which may be an
   # Item, Binary, Collection, etc. If the object is unrecognized, a generic
   # icon is returned.
@@ -300,50 +350,8 @@ module ApplicationHelper
   # @return [String] HTML `i` tag.
   #
   def icon_for(entity)
-    icon = 'fas fa-cube'
-    if entity == Item
-      icon = 'fas fa-cube'
-    elsif entity.kind_of?(Item)
-      viewer_binary = entity.effective_viewer_binary
-      if viewer_binary&.is_audio?
-        icon = 'fas fa-volume-up'
-      elsif viewer_binary&.is_image?
-        icon = 'fas fa-image'
-      elsif viewer_binary&.is_pdf?
-        icon = 'fas fa-file-pdf'
-      elsif viewer_binary&.is_text? or viewer_binary&.is_document?
-        icon = 'fas fa-file-alt'
-      elsif viewer_binary&.is_video?
-        icon = 'fas fa-film'
-      elsif entity.variant == Item::Variants::DIRECTORY
-        icon = 'fas fa-folder-open'
-      elsif entity.variant == Item::Variants::FILE
-        icon = 'fas fa-file'
-      elsif entity.items.any?
-        icon = 'fas fa-cubes'
-      end
-    elsif entity.kind_of?(Binary)
-      if entity.is_audio?
-        icon = 'fas fa-volume-up'
-      elsif entity.is_image?
-        icon = 'fas fa-image'
-      elsif entity.is_pdf?
-        icon = 'fas fa-file-pdf'
-      elsif entity.is_text? or entity&.is_document?
-        icon = 'fas fa-file-alt'
-      elsif entity.is_video?
-        icon = 'fas fa-film'
-      end
-    elsif entity == Collection or entity.kind_of?(Collection)
-      icon = 'fas fa-folder-open'
-    elsif entity == Agent or entity.kind_of?(Agent)
-      icon = 'fas fa-user-circle'
-    elsif entity == ItemSet or entity.kind_of?(ItemSet)
-      icon = 'far fa-circle'
-    elsif entity == User or entity.kind_of?(User)
-      icon = 'fas fa-user'
-    end
-    raw("<i class=\"#{icon}\" aria-hidden=\"true\"></i>")
+    icon = fontawesome_icon_for(entity)
+    raw("<i class=\"#{icon[0]} fa-#{icon[1]}\" aria-hidden=\"true\"></i>")
   end
 
   ##

app/helpers/items_helper.rb
@@ -926,7 +926,14 @@ module ItemsHelper
                           data: { location: 'remote' })
       end
     else
-      html << icon_for(entity) # ApplicationHelper
+      # N.B.: instead of using ApplicationHelper.icon_for(), we have
+      # pre-downloaded some Font Awesome icons as SVGs and saved in them in the
+      # assets directory. This results in them appearing in <img> tags which
+      # helps make our CSS more concise. The files are available at:
+      # https://github.com/encharm/Font-Awesome-SVG-PNG/tree/master/black/svg
+      html << image_tag('fontawesome-' + fontawesome_icon_for(entity)[1] + '.svg',
+                        options.merge('data-type': 'svg',
+                                      'data-location': 'local'))
     end
     raw(html.string)
   end