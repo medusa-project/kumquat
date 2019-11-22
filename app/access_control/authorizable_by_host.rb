389ecc5145c034f1eb213a5de0a77db1e4192a5b
Alex Dolski <alexd@n-ary.net>
2019-11-22 09:59:47 -0600

    Bugfix in authorized_by_any_host_groups?()

--- app/access_control/authorizable_by_host.rb
+++ app/access_control/authorizable_by_host.rb
@@ -4,16 +4,16 @@
 module AuthorizableByHost
 
   ##
-  # @param host_groups [Enumerable<HostGroup>]
+  # @param host_groups [Enumerable<HostGroup>] Client host group(s).
   # @return [Boolean] True if no host groups are provided or if any of them
   #                   authorize the entity; false otherwise.
   #
   def authorized_by_any_host_groups?(host_groups)
     return self.effective_allowed_host_groups.empty? if host_groups&.empty?
     host_groups.each do |group|
-      return true if authorized_by_host_group?(group)
+      return false unless authorized_by_host_group?(group)
     end
-    false
+    true
   end
 
   ##
--- test/access_control/authorizable_by_host_test.rb
+++ test/access_control/authorizable_by_host_test.rb
@@ -8,24 +8,37 @@ class AuthorizableByHostTest < ActiveSupport::TestCase
 
   # authorized_by_any_host_groups?()
 
+  test 'authorized_by_any_host_groups?() returns true if the instance has no
+  allowed or denied host groups' do
+    assert @collection.authorized_by_any_host_groups?([])
+    assert @collection.authorized_by_any_host_groups?([host_groups(:red)])
+  end
+
+  test 'authorized_by_any_host_groups?() returns true if the instance has a
+  non-matching denied host group' do
+    @collection.denied_host_groups << host_groups(:yellow)
+    assert @collection.authorized_by_any_host_groups?([host_groups(:red)])
+  end
+
   test 'authorized_by_any_host_groups?() returns false if given no host groups
   but the instance has allowed host groups' do
-    @collection.allowed_host_groups << host_groups(:uiuc)
+    @collection.allowed_host_groups << host_groups(:yellow)
     assert !@collection.authorized_by_any_host_groups?([])
   end
 
   test 'authorized_by_any_host_groups?() returns false if given a denied host
   group' do
-    @collection.denied_host_groups << host_groups(:uiuc)
-    assert !@collection.authorized_by_any_host_groups?([host_groups(:uiuc)])
+    @collection.denied_host_groups << host_groups(:yellow)
+    assert !@collection.authorized_by_any_host_groups?([host_groups(:yellow)])
   end
 
-  test 'authorized_by_any_host_groups?() returns true if given an allowed host
+  test 'authorized_by_any_host_groups?() returns false if given an allowed host
   group and a denied host group' do
-    @collection.allowed_host_groups << host_groups(:blue)
-    @collection.denied_host_groups << host_groups(:uiuc)
-    assert @collection.authorized_by_any_host_groups?([host_groups(:uiuc),
-                                                 host_groups(:blue)])
+    g1 = host_groups(:blue)
+    g2 = host_groups(:yellow)
+    @collection.allowed_host_groups << g1
+    @collection.denied_host_groups << g2
+    assert !@collection.authorized_by_any_host_groups?([g1, g2])
   end
 
   # authorized_by_host_group?()
@@ -39,7 +52,7 @@ class AuthorizableByHostTest < ActiveSupport::TestCase
   test 'authorized_by_host_group?() returns false if the given host group is
   not in the list of allowed host groups' do
     @collection.allowed_host_groups << host_groups(:red)
-    assert !@collection.authorized_by_host_group?(host_groups(:uiuc))
+    assert !@collection.authorized_by_host_group?(host_groups(:blue))
   end
 
   test 'authorized_by_host_group?() returns true if the given host group is
@@ -51,7 +64,7 @@ class AuthorizableByHostTest < ActiveSupport::TestCase
   test 'authorized_by_host_group?() returns true if there are no allowed host
   groups and the given host group is not explicitly denied' do
     @collection.denied_host_groups << host_groups(:red)
-    assert @collection.authorized_by_host_group?(host_groups(:uiuc))
+    assert @collection.authorized_by_host_group?(host_groups(:blue))
   end
 
 end
