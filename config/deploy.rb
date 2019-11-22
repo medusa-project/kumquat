64265abaf7da519c2951144794b5e94c4134b8cf
Alex Dolski <alexd@n-ary.net>
2019-11-22 16:59:52 -0600

    Comment out linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,8 +27,8 @@ set :rails_env, 'production'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push(
-    'config/database.yml', 'config/master.key', 'config/shibboleth.yml')
+#set :linked_files, fetch(:linked_files, []).push(
+#    'config/database.yml', 'config/master.key', 'config/shibboleth.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push(
