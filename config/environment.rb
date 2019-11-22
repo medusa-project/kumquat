3b9423c0c14b635297c5abfe889deeec797ba04b
Alex Dolski <alexd@n-ary.net>
2019-11-22 17:31:20 -0600

    Fix logging in demo

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
-Rails.logger = ActiveSupport::Logger.new(STDOUT) unless Rails.env.production?
+Rails.logger = ActiveSupport::Logger.new(STDOUT) if Rails.env.development? or Rails.env.test?
 
 # Initialize the Rails application.
 Rails.application.initialize!
