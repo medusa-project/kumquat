ee73d2ff142f9430035d2238dbedfb384680f7f7
Alex Dolski <alexd@n-ary.net>
2019-12-02 09:36:22 -0600

    OmniAuth/Shibboleth fixes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 # We need a config/shibboleth.yml file with the options
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.production?
-    opts = YAML.load_file(File.join(Rails.root, 'config', 'shibboleth.yml'))[Rails.env]
+  if Rails.env.production? or Rails.env.demo?
+    opts = YAML.load(ERB.new(File.read(File.join(Rails.root, 'config', 'shibboleth.yml'))).result)[Rails.env]
     provider :shibboleth, opts.symbolize_keys
     Kumquat::Application.shibboleth_host = opts['host']
   else
