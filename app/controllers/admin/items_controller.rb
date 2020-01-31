bb801cb678e32184e3eadc88dd878ca5986717d4
Alex Dolski <alexd@n-ary.net>
2020-01-31 15:31:12 -0600

    Fix representative item ID in PERMITTED_PARAMS

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,7 +4,7 @@ module Admin
 
     PERMITTED_PARAMS = [:id, :contentdm_alias, :contentdm_pointer, :df,
                         :embed_tag, :'fq[]', :item_set, :page_number,
-                        :published, :q, :representative_item_id,
+                        :published, :q, :representative_item_repository_id,
                         :subpage_number, :variant, allowed_host_group_ids: [],
                         denied_host_group_ids: []]
 
