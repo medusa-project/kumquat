medusa-project/kumquat

commit 1129a550553ac1bd22a413f2d6b35d9df75a48be

Fixes for restricted items

app/helpers/iiif_presentation_helper.rb
@@ -73,12 +73,12 @@ module IiifPresentationHelper
   def iiif_canvases_for(item)
     # Directory and 3D items are not viewable, and composite and supplement
     # items are included in the annotation list instead.
-    finder = item.finder.exclude_variants(Item::Variants::COMPOSITE,
-                                          Item::Variants::DIRECTORY,
-                                          Item::Variants::SUPPLEMENT,
-                                          Item::Variants::THREE_D_MODEL)
-    if finder.count > 0
-      canvases = finder.to_a.map { |child| iiif_canvas_for(child, true) }
+    result = item.items.where.not(variant: [Item::Variants::COMPOSITE,
+                                            Item::Variants::DIRECTORY,
+                                            Item::Variants::SUPPLEMENT,
+                                            Item::Variants::THREE_D_MODEL])
+    if result.count > 0
+      canvases = result.map { |child| iiif_canvas_for(child, true) }
     else
       canvases = [ iiif_canvas_for(item, !(item.file? or item.directory?)) ]
     end

app/controllers/items_controller.rb
@@ -599,7 +599,7 @@ class ItemsController < WebsiteController
   end
 
   def check_publicly_accessible
-    raise UnpublishedError unless @item.publicly_accessible?
+    raise UnpublishedError if !@item.publicly_accessible? && !@item.restricted
   end
 
   ##

app/models/item.rb
@@ -715,7 +715,14 @@ class Item < ApplicationRecord
         if self.variant == Variants::SUPPLEMENT
           bin = self.binaries.first
         elsif self.is_compound?
-          first_child = self.finder.limit(1).to_a.first
+          # Restricted items aren't indexed, so we have to do this differently.
+          # (In hindsight they probably should have been designed to be indexed)
+          if self.restricted
+            first_child = self.items.order(:page_number, :subpage_number).
+                limit(1).first
+          else
+            first_child = self.finder.limit(1).to_a.first
+          end
           # This should always be true, but just to make sure we prevent a
           # circular reference...
           if first_child and first_child.repository_id != self.repository_id

commit 9b0b36fafbc58ef01616e1927ed536d14d2274ba

Fix a regression in iiif_canvases_for() introduced in 1129a550553ac1bd22a413f2d6b35d9df75a48be

app/helpers/iiif_presentation_helper.rb
@@ -73,10 +73,18 @@ module IiifPresentationHelper
   def iiif_canvases_for(item)
     # Directory and 3D items are not viewable, and composite and supplement
     # items are included in the annotation list instead.
-    result = item.items.where.not(variant: [Item::Variants::COMPOSITE,
-                                            Item::Variants::DIRECTORY,
-                                            Item::Variants::SUPPLEMENT,
-                                            Item::Variants::THREE_D_MODEL])
+    exclude_variants = [Item::Variants::COMPOSITE,
+                        Item::Variants::DIRECTORY,
+                        Item::Variants::SUPPLEMENT,
+                        Item::Variants::THREE_D_MODEL]
+    # Restricted items aren't indexed, so have to be retrieved from the
+    # database.
+    if item.restricted
+      result = item.items.where.not(variant: exclude_variants)
+    else
+      result = item.finder.exclude_variants(*exclude_variants).to_a
+    end
+
     if result.count > 0
       canvases = result.map { |child| iiif_canvas_for(child, true) }
     else