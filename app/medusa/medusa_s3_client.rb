6f51e89ae088f2f37a0fb0834c0ae6b0bdfa59b3
Alex Dolski <alexd@n-ary.net>
2020-09-24 10:14:59 -0500

    MedusaS3Client supports a custom endpoint

--- app/medusa/medusa_s3_client.rb
+++ app/medusa/medusa_s3_client.rb
@@ -12,7 +12,17 @@ class MedusaS3Client
 
   def method_missing(method, *args, &block)
     unless @client
-      @client = Aws::S3::Client.new(region: ::Configuration.instance.aws_region)
+      config = ::Configuration.instance
+      opts   = { region: config.aws_region }
+      # In development & test, these may be drawn from the configuration.
+      # In demo & production, we use IAM instance credentials instead.
+      if config.medusa_s3_endpoint
+        opts[:force_path_style] = true
+        opts[:endpoint]         = config.medusa_s3_endpoint
+        opts[:credentials]      = Aws::Credentials.new(config.medusa_s3_access_key_id,
+                                                       config.medusa_s3_secret_access_key)
+      end
+      @client = Aws::S3::Client.new(opts)
     end
     @client.send(method, *args, &block)
   end
