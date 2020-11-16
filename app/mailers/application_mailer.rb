60e89a0fa3482e5048effa9ef0dbcc66f512f419
Alex Dolski <alexd@n-ary.net>
2020-11-16 15:10:29 -0600

    Use a reply-to address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "University of Illinois at Urbana-Champaign Library Digital Collections <#{::Configuration.instance.mail[:from]}>"
+  default from: "University of Illinois at Urbana-Champaign Library Digital Collections <#{::Configuration.instance.mail[:from]}>",
+          reply_to: ::Configuration.instance.mail[:reply_to]
   layout "mailer"
 end
