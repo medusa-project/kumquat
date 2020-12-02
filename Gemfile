e56b8e0da5a2798b7c287ce63fbc741666658a88
Alex Dolski <alexd@n-ary.net>
2020-12-02 13:21:47 -0600

    Switch to Puma

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'pg'
 # warning about an already-initialized constant.
 # See: https://github.com/prawnpdf/prawn/issues/1024
 gem 'prawn', git: 'https://github.com/prawnpdf/prawn.git', ref: '9250c8675342872603332784f77263fcb1cf72a2'
+gem 'puma'
 gem 'rails', '6.0.3.1'
 gem 'rails_autolink'
 gem 'sassc'
@@ -40,12 +41,10 @@ group :development do
   gem 'capistrano-bundler'
   gem 'capistrano-rails'
   gem 'capistrano-rbenv'
-  gem 'puma'
   gem 'rails-erd', require: false # generate a model diagram with `bundle exec erd`
   gem 'yard'
 end
 
 group :production do
   gem "omniauth-rails_csrf_protection"
-  gem 'passenger'
 end
--- config/puma.rb
+++ config/puma.rb
@@ -1,26 +1,22 @@
-# Puma configuration file
-# For all Rails environments.
-#
-# bundle exec puma -C config/puma.rb
-#
-# See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
-
 # Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers a minimum and maximum.
+# The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum, this matches the default thread size of Active Record.
+# and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { (ENV['RAILS_ENV'] == 'production') ? 5 : 1 }.to_i
+# alexd note: there is probably a point at which increasing this number slows
+# things down due to increased contention for the Global Interpreter Lock.
+# But I don't know what that point is yet.
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests, default is 3000.
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { 'development' }
+environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -28,7 +24,8 @@ environment ENV.fetch("RAILS_ENV") { 'development' }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-workers ENV.fetch("WEB_CONCURRENCY") { (ENV['RAILS_ENV'] == 'production') ? 4 : 1 }
+# alexd note: one worker per t3.xlarge CPU
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
@@ -39,11 +36,19 @@ workers ENV.fetch("WEB_CONCURRENCY") { (ENV['RAILS_ENV'] == 'production') ? 4 : 
 #
 preload_app!
 
+# If you are preloading your application and using Active Record, it's
+# recommended that you close any connections to the database before workers
+# are forked to prevent connection leakage.
+#
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
+end
+
 # The code in the `on_worker_boot` will be called if you are using
 # clustered mode by specifying a number of `workers`. After each worker
-# process is booted this block will be run, if you are using `preload_app!`
-# option you will want to use this block to reconnect to any threads
-# or connections that may have been created at application boot, Ruby
+# process is booted, this block will be run. If you are using the `preload_app!`
+# option, you will want to use this block to reconnect to any threads
+# or connections that may have been created at application boot, as Ruby
 # cannot share connections between processes.
 #
 on_worker_boot do
@@ -52,8 +57,3 @@ end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
-
-if ENV['RAILS_ENV'] == 'production'
-  daemonize
-  pidfile File.join(__dir__, '..', 'tmp', 'pids', 'puma.pid')
-end
