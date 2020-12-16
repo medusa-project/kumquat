42443674fd77c8e8ac51c6e1ea98cffb1f28445d
Alex Dolski <alexd@n-ary.net>
2020-12-16 09:30:00 -0600

    Add ReindexAllContentJob

---
+++ test/jobs/reindex_all_content_job_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class ReindexAllContentJobTest < ActiveSupport::TestCase
+
+  # perform()
+
+  test 'perform() should work properly' do
+    # TODO: write this
+  end
+
+end
