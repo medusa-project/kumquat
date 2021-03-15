78fecd9601490358a650a927300112fd03e7e28b
Alex Dolski <alexd@n-ary.net>
2021-03-15 11:06:46 -0500

    Don't check ES as that should have its own health check

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -7,9 +7,6 @@ class HealthController < ApplicationController
     # Touch the database... except in demo, where the database is running in
     # Aurora and we can save money by letting it go idle.
     Collection.count unless Rails.env.demo?
-
-    # touch Elasticsearch
-    Item.search.aggregations(false).limit(0).count
   rescue => e
     render plain: "RED: #{e}", status: :internal_server_error
   else
