49212730ea942e1fa453ca1468afb0f4ac3232b8
Alex Dolski <alexd@n-ary.net>
2021-03-29 11:13:11 -0500

    Swallow errors from GatewayClient.num_items()

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -6,7 +6,13 @@ class LandingController < WebsiteController
   def index
     @gateway_item_count = Rails.cache.fetch('gateway.item_count',
                                             expires_in: 12.hours) do
-      GatewayClient.instance.num_items
+      begin
+        GatewayClient.instance.num_items
+      rescue
+        # The gateway is probably down. This is a problem that is better dealt
+        # with elsewhere; failing here should not fail the request.
+        nil
+      end
     end
   end
 
