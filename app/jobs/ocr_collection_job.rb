776e2fe1b56d2cb45b6c41bc6c7a4cf4691ce25e
Alex Dolski <alexd@n-ary.net>
2021-05-20 09:05:00 -0500

    Parallelize jobs and increase database connection pool size to accommodate

--- app/jobs/ocr_collection_job.rb
+++ app/jobs/ocr_collection_job.rb
@@ -17,23 +17,56 @@ class OcrCollectionJob < Job
     collection = Collection.find_by_repository_id(args[0])
     raise ActiveRecord::RecordNotFound unless collection
 
-    self.task&.update(status_text: "Running OCR on #{collection.title}")
+    # Divide the total number of results into num_threads segments, and have
+    # each thread work on a segment.
+    mutex               = Mutex.new
+    threads             = Set.new
+    # N.B.: these conditions must be kept in sync with Binary.ocrable?()
+    binaries            = Binary.joins(:item).
+      where('items.collection_repository_id': collection.repository_id).
+      where(full_text: nil).
+      where(master_type: Binary::MasterType::ACCESS).
+      where('media_type LIKE ? OR media_type = ?', 'image/%', 'application/pdf')
+    binary_count        = binaries.count
+    binary_index        = 0
+    binaries_per_thread = (binary_count / num_threads.to_f).ceil
+    return if binaries_per_thread < 1
 
-    Binary.uncached do
-      # N.B.: these conditions must be kept in sync with Binary.ocrable?()
-      binaries = Binary.joins(:item).
-        where('items.collection_repository_id': collection.repository_id).
-        where(full_text: nil).
-        where(master_type: Binary::MasterType::ACCESS).
-        where('media_type LIKE ? OR media_type = ?', 'image/%', 'application/pdf')
-      binary_count = binaries.count
-      binaries.each_with_index do |binary, index|
-        binary.detect_text
-        binary.save!
-        self.task&.update(percent_complete: index / binary_count.to_f)
+    self.task&.update(status_text: "Running OCR on #{binary_count} "\
+        "binaries in #{collection.title} using #{num_threads} threads")
+
+    num_threads.times do |thread_num|
+      threads << Thread.new do
+        batch_size  = [1000, binaries_per_thread].min
+        num_batches = (binaries_per_thread / batch_size.to_f).ceil
+        num_batches.times do |batch_index|
+          batch_offset = batch_index * batch_size
+          q_offset     = thread_num * binaries_per_thread + batch_offset
+          q_limit      = [batch_size, binaries_per_thread - batch_offset].min
+          Binary.uncached do
+            binaries.limit(q_limit).offset(q_offset).each do |binary|
+              binary.detect_text
+              binary.save!
+              binary.item.reindex
+              mutex.synchronize do
+                binary_index += 1
+                self.task&.update(percent_complete: binary_index / binary_count.to_f)
+              end
+            end
+          end
+        end
       end
     end
+    threads.each(&:join)
     self.task&.succeeded
+  end
+
+
+  private
+
+  def num_threads
+    ActiveRecord::Base.connection_pool.instance_eval { @size } -
+      ENV.fetch("RAILS_MAX_THREADS") { 5 }
   end
 
 end
--- app/jobs/ocr_item_job.rb
+++ app/jobs/ocr_item_job.rb
@@ -18,25 +18,54 @@ class OcrItemJob < Job
     main_item = Item.find_by_repository_id(args[0])
     raise ActiveRecord::RecordNotFound unless main_item
 
-    Binary.uncached do
-      items = [main_item] + main_item.all_children
-      item_count = items.length
-      self.task&.update(status_text: "Running OCR on #{main_item.title} and "\
-          "#{item_count - 1} child items")
-      items.each_with_index do |item, index|
-        # N.B.: these conditions must be kept in sync with Binary.ocrable?()
-        binaries = item.binaries.
-          where(full_text: nil).
-          where(master_type: Binary::MasterType::ACCESS).
-          where('media_type LIKE ? OR media_type = ?', 'image/%', 'application/pdf')
-        binaries.each do |binary|
-          binary.detect_text
-          binary.save!
+    # Divide the total number of results into num_threads segments, and have
+    # each thread work on a segment.
+    mutex            = Mutex.new
+    threads          = Set.new
+    items            = [main_item] + main_item.all_children
+    item_count       = items.length
+    item_index       = 0
+    items_per_thread = (item_count / num_threads.to_f).ceil
+    return if items_per_thread < 1
+
+    self.task&.update(status_text: "Running OCR on #{main_item.title} and "\
+          "#{item_count - 1} child items using #{num_threads} threads")
+
+    num_threads.times do |thread_num|
+      threads << Thread.new do
+        batch_size  = [1000, items_per_thread].min
+        num_batches = (items_per_thread / batch_size.to_f).ceil
+        num_batches.times do |batch_index|
+          batch_offset = batch_index * batch_size
+          q_offset     = thread_num * items_per_thread + batch_offset
+          q_limit      = [batch_size, items_per_thread - batch_offset].min
+          items[q_offset..(q_offset + q_limit)]&.each do |item|
+            # N.B.: these conditions are roughly in sync with Binary.ocrable?()
+            item.binaries.
+                where(master_type: Binary::MasterType::ACCESS).
+                where('media_type LIKE ? OR media_type = ?', 'image/%', 'application/pdf').each do |binary|
+              binary.detect_text
+              binary.save!
+              item.reindex
+              mutex.synchronize do
+                item_index += 1
+                self.task&.update(percent_complete: item_index / item_count.to_f)
+              end
+            end
+          end
         end
-        self.task&.update(percent_complete: index / item_count.to_f)
       end
     end
+    threads.each(&:join)
     self.task&.succeeded
+  end
+
+
+  private
+
+  def num_threads
+    ActiveRecord::Base.connection_pool.instance_eval { @size } -
+      ENV.fetch("RAILS_MAX_THREADS") { 5 }
   end
 
 end
