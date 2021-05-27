medusa-project/kumquat

commit fcb4b17bb4b721aa7a3c8502283214cf25ae30ff

Ensure that config.action_controller.default_url_options is set correctly

config/environments/test.rb
@@ -31,12 +31,17 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.perform_caching = false
 
+  config.action_controller.default_url_options = {
+    protocol: 'http://',
+    host: "localhost:3000"
+  }
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  config.action_mailer.default_url_options = {host: "localhost:3000"}
+  config.action_mailer.default_url_options = config.action_controller.default_url_options
 
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random

config/environments/demo.rb
@@ -65,7 +65,8 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_controller.default_url_options = {
-      protocol: 'https://'
+    protocol: 'https://',
+    host: "demo.digital.library.illinois.edu"
   }
 
   # Use a different logger for distributed setups.
@@ -94,8 +95,5 @@ Rails.application.configure do
   config.action_mailer.perform_caching       = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.logger                = Rails.logger
-  config.action_mailer.default_url_options = {
-      protocol: 'https://',
-      host: "demo.digital.library.illinois.edu"
-  }
+  config.action_mailer.default_url_options   = config.action_controller.default_url_options
 end

config/environments/development.rb
@@ -42,6 +42,11 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_controller.default_url_options = {
+    protocol: 'http://',
+    host: "localhost:3000"
+  }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
@@ -63,5 +68,5 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries    = !mail_config[:smtp][:address].nil?
   config.action_mailer.raise_delivery_errors = !mail_config[:smtp][:address].nil?
   config.action_mailer.logger                = Rails.logger
-  config.action_mailer.default_url_options   = {host: "localhost:3000"}
+  config.action_mailer.default_url_options   = config.action_controller.default_url_options
 end

config/environments/production.rb
@@ -65,7 +65,8 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_controller.default_url_options = {
-      protocol: 'https://'
+    protocol: 'https://',
+    host: "digital.library.illinois.edu"
   }
 
   # Use a different logger for distributed setups.
@@ -94,8 +95,5 @@ Rails.application.configure do
   config.action_mailer.perform_caching       = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.logger                = Rails.logger
-  config.action_mailer.default_url_options = {
-      protocol: 'https://',
-      host: "digital.library.illinois.edu"
-  }
+  config.action_mailer.default_url_options   = config.action_controller.default_url_options
 end

commit 202c639814c2bd98a309b43309fa8e7ac1783202

Fix a test regression introduced in fcb4b17bb4b721aa7a3c8502283214cf25ae30ff

config/environments/test.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
   config.action_controller.default_url_options = {
     protocol: 'http://',
-    host: "localhost:3000"
+    host: "www.example.com"
   }
 
   # Tell Action Mailer not to deliver emails to the real world.