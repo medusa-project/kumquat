41f582f8d70de29c95dbe4127129f0ef3e7568d1
Alex Dolski <alexd@n-ary.net>
2021-07-01 15:22:40 -0500

    Break out client instantiation into its own method

--- app/medusa/medusa_s3_client.rb
+++ app/medusa/medusa_s3_client.rb
@@ -11,19 +11,7 @@ class MedusaS3Client
   BUCKET = Configuration.instance.medusa_s3_bucket
 
   def method_missing(method, *args, &block)
-    unless @client
-      config = ::Configuration.instance
-      opts   = { region: config.aws_region }
-      # In development & test, these may be drawn from the configuration.
-      # In demo & production, we use IAM instance credentials instead.
-      if config.medusa_s3_endpoint
-        opts[:force_path_style] = true
-        opts[:endpoint]         = config.medusa_s3_endpoint
-        opts[:credentials]      = Aws::Credentials.new(config.medusa_s3_access_key_id,
-                                                       config.medusa_s3_secret_access_key)
-      end
-      @client = Aws::S3::Client.new(opts)
-    end
+    get_client
     @client.send(method, *args, &block)
   end
 
@@ -55,6 +43,26 @@ class MedusaS3Client
           object.key.delete_prefix(src_prefix)
       move_object(object.key, dest_key)
     end
+  end
+
+
+  private
+
+  def get_client
+    unless @client
+      config = ::Configuration.instance
+      opts   = { region: config.aws_region }
+      # In development & test, these may be drawn from the configuration.
+      # In demo & production, we use IAM instance credentials instead.
+      if config.medusa_s3_endpoint
+        opts[:force_path_style] = true
+        opts[:endpoint]         = config.medusa_s3_endpoint
+        opts[:credentials]      = Aws::Credentials.new(config.medusa_s3_access_key_id,
+                                                       config.medusa_s3_secret_access_key)
+      end
+      @client = Aws::S3::Client.new(opts)
+    end
+    @client
   end
 
 end
