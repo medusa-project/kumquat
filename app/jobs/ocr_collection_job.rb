1825a62ecaf1bfd77a61f15738ba647bd0dcafec
Alex Dolski <alexd@n-ary.net>
2021-07-14 09:11:25 -0500

    Rescue OCR errors

--- app/jobs/ocr_collection_job.rb
+++ app/jobs/ocr_collection_job.rb
@@ -6,7 +6,8 @@
 #
 class OcrCollectionJob < Job
 
-  QUEUE = Job::Queue::ADMIN
+  LOGGER = CustomLogger.new(OcrCollectionJob)
+  QUEUE  = Job::Queue::ADMIN
 
   queue_as QUEUE
 
@@ -34,11 +35,13 @@ class OcrCollectionJob < Job
     binary_count        = binaries.count
     binary_index        = 0
     num_threads         = self.class.num_threads
+    num_errors          = 0
     binaries_per_thread = (binary_count / num_threads.to_f).ceil
     return if binaries_per_thread < 1
 
-    self.task&.update(status_text: "Running OCR on #{binary_count} "\
-        "binaries in #{collection.title} using #{num_threads} threads")
+    status_text = "Running OCR on #{binary_count} binaries in "\
+        "#{collection.title} using #{num_threads} threads"
+    self.task&.update(status_text: status_text)
 
     num_threads.times do |thread_num|
       threads << Thread.new do
@@ -50,9 +53,16 @@ class OcrCollectionJob < Job
           q_limit      = [batch_size, binaries_per_thread - batch_offset].min
           Binary.uncached do
             binaries.limit(q_limit).offset(q_offset).each do |binary|
-              binary.detect_text
-              binary.save!
-              binary.item.reindex
+              begin
+                binary.detect_text
+                binary.save!
+                binary.item.reindex
+              rescue
+                num_errors += 1
+                self.task&.update(status_text: "#{status_text} (#{num_errors} errors)")
+                LOGGER.error("Failed to run OCR on binary UUID %s",
+                             binary.medusa_uuid)
+              end
               mutex.synchronize do
                 binary_index += 1
                 self.task&.update(percent_complete: binary_index / binary_count.to_f)
