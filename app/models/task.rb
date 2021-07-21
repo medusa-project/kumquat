8d74332ab418fc72408ea07cd7bc1958ef6005d9
Alex Dolski <alexd@n-ary.net>
2021-07-21 09:37:02 -0500

    estimated_completion() uses TimeUtils.eta()

--- app/models/task.rb
+++ app/models/task.rb
@@ -92,13 +92,11 @@ class Task < ApplicationRecord
   # @return [Time,nil]
   #
   def estimated_completion
-    now = Time.zone.now
     if self.percent_complete < 0.0000001 || self.percent_complete > 0.9999999 ||
         self.started_at.blank? || self.stopped_at.present?
       nil
     else
-      runtime = now - self.started_at
-      Time.zone.at(now + ((1 - self.percent_complete) * runtime))
+      TimeUtils.eta(self.started_at, self.percent_complete)
     end
   end
 
