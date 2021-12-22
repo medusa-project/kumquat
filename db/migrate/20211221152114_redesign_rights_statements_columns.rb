65ad68dd106e81b47d498a7e3e60603d9b0405a9
Alex Dolski <alexd@n-ary.net>
2021-12-22 10:58:38 -0600

    Fix a potential migration problem

--- db/migrate/20211221152114_redesign_rights_statements_columns.rb
+++ db/migrate/20211221152114_redesign_rights_statements_columns.rb
@@ -2,6 +2,7 @@ class RedesignRightsStatementsColumns < ActiveRecord::Migration[6.1]
   def change
     rename_column :collections, :rightsstatements_org_uri, :rights_term_uri
     remove_index :vocabulary_terms, :uri
+    execute "UPDATE vocabulary_terms SET uri = NULL WHERE uri = '';"
     add_index :vocabulary_terms, :uri, unique: true
   end
 end
