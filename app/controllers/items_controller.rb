medusa-project/kumquat

commit b7f96030b47fdf23c494547c0892415bbabd728a

Remove the display=leaves query argument

app/controllers/items_controller.rb
@@ -675,8 +675,7 @@ class ItemsController < WebsiteController
     session[:sort]          = query[:sort]
     session[:start]         = [0, query[:start].to_i].max
     session[:limit]         = query[:limit].to_i
-    if session[:limit].to_i < MIN_RESULT_WINDOW ||
-        session[:limit].to_i > MAX_RESULT_WINDOW
+    if session[:limit] < MIN_RESULT_WINDOW || session[:limit] > MAX_RESULT_WINDOW
       session[:limit] = Option::integer(Option::Keys::DEFAULT_RESULT_WINDOW)
     end
 
@@ -693,8 +692,8 @@ class ItemsController < WebsiteController
         order(sort).
         start(session[:start])
 
-    # display=leaves is used in free-form collections to show files flattened.
-    if params[:display] == 'leaves'
+    # Return results flattened if not viewing a file tree.
+    if action_name != 'tree_data'
       relation.search_children(true).
           include_variants(Item::Variants::FILE).
           limit(session[:limit])

app/views/collections/_description.html.haml
@@ -10,8 +10,7 @@
       - if @num_public_objects == 1
         - link_target = item_path(@collection.items.first)
       - else
-        - options = @collection.free_form? ? { display: 'leaves' } : nil
-        - link_target = collection_items_path(@collection, options)
+        - link_target = collection_items_path(@collection)
       = link_to(link_target,
                 class: 'btn btn-lg btn-outline-primary',
                 target: @collection.published_in_dls ? '' : '_blank') do

app/views/collections/show.html.haml
@@ -15,8 +15,6 @@
                          params[:q],
                          class: 'form-control',
                          placeholder: 'Search This Collection'
-      - if @collection.free_form?
-        = hidden_field_tag(:display, 'leaves')
     .input-group-append
       %button.btn.btn-light.dropdown-toggle{"aria-expanded": "false",
                                             "aria-haspopup": "true",

app/views/items/tree.html.haml
@@ -11,7 +11,7 @@
       %i.fa.fa-level-down-alt
       Tree
   %li.nav-item
-    = link_to collection_items_path(display: 'leaves'), class: 'nav-link' do
+    = link_to collection_items_path, class: 'nav-link' do
       %i.fa.fa-bars
       List
 

commit 65bb1aa893f1be49ddcfb132acaa887d4f33d3b5

Fix a bug in item_relation_for() introduced in b7f96030b47fdf23c494547c0892415bbabd728a

app/controllers/items_controller.rb
@@ -695,7 +695,7 @@ class ItemsController < WebsiteController
     # Return results flattened if not viewing a file tree.
     if action_name != 'tree_data'
       relation.search_children(true).
-          include_variants(Item::Variants::FILE).
+          exclude_variants(Item::Variants::DIRECTORY).
           limit(session[:limit])
     else
       relation.search_children(@collection&.package_profile != PackageProfile::FREE_FORM_PROFILE).