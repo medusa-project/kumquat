medusa-project/kumquat

commit 98c5d78809a906e91894c965ee35e71c23205b63

Make ItemUpdater.change_element_values() more efficient

app/controllers/admin/items_controller.rb
@@ -84,7 +84,7 @@ module Admin
       raise ActiveRecord::RecordNotFound unless col
       begin
         relation = editing_item_relation_for(col)
-        BatchChangeItemMetadataJob.perform_later(relation.to_a.select(&:present?),
+        BatchChangeItemMetadataJob.perform_later(relation.to_id_a,
                                                  params[:element].to_s,
                                                  params[:replace_values].map(&:to_unsafe_hash))
       rescue => e

app/jobs/batch_change_item_metadata_job.rb
@@ -16,18 +16,18 @@ class BatchChangeItemMetadataJob < Job
   def perform(*args)
     if args[0].kind_of?(Collection)
       items = args[0].items
-      what = args[0].title
+      what  = args[0].title
     elsif args[0].kind_of?(ItemSet)
       items = args[0].items
-      what = args[0].name
+      what  = args[0].name
     elsif args[0].respond_to?(:each)
-      items = args[0]
-      what = "#{args[0].length} items"
+      items = Item.where("repository_id IN (?)", args[0])
+      what  = "#{args[0].length} items"
     else
       raise ArgumentError, 'Illegal first argument'
     end
 
-    element_name = args[1]
+    element_name   = args[1]
     replace_values = args[2]
 
     self.task.update!(status_text: "Changing #{element_name} element values "\

app/metadata/item_updater.rb
@@ -9,27 +9,28 @@ class ItemUpdater
   MAX_TSV_VALUE_LENGTH = 10000
 
   ##
-  # @param items [Enumerable<Item>]
+  # @param items [ActiveRecord::Relation<Item>]
   # @param element_name [String] Element to replace.
-  # @param replace_values [Enumerable<Hash<Symbol,String>] Enumerable of hashes
-  #                                                        with `:string` and
-  #                                                        `:uri` keys.
+  # @param replace_values [Enumerable<Hash<Symbol,String>>] Enumerable of hashes
+  #                                                         with `:string` and
+  #                                                         `:uri` keys.
   # @param task [Task] Supply to receive progress updates.
   # @return [void]
   #
   def change_element_values(items, element_name, replace_values, task = nil)
-    ActiveRecord::Base.transaction do
-      items.each_with_index do |item, index|
-        item.elements.where(name: element_name).destroy_all
-        replace_values.each do |hash|
-          hash = hash.symbolize_keys
-          item.elements.build(name: element_name,
-                              value: hash[:string],
-                              uri: hash[:uri])
+    Item.uncached do
+      items.find_each.with_index do |item, index|
+        Item.transaction do
+          item.elements.where(name: element_name).destroy_all
+          replace_values.each do |hash|
+            hash = hash.symbolize_keys
+            item.elements.build(name:  element_name,
+                                value: hash[:string],
+                                uri:   hash[:uri])
+          end
+          item.save!
         end
-        item.save!
-
-        if task and index % 10 == 0
+        if task && index % 10 == 0
           task.update(percent_complete: index / items.length.to_f)
         end
       end
@@ -134,7 +135,7 @@ class ItemUpdater
   ##
   # Updates items from the given TSV file.
   #
-  # Items will not be created or deleted. (For that, use {MedusaIngester}.)
+  # Items will not be created or deleted. (For that, use [MedusaIngester].)
   #
   # @param pathname [String] Absolute pathname of a TSV file.
   # @param original_filename [String] Filename of the TSV file as it was

test/jobs/batch_change_item_metadata_job_test.rb
@@ -4,7 +4,7 @@ class BatchChangeItemMetadataJobTest < ActiveSupport::TestCase
 
   # perform()
 
-  test 'perform() should change all matching elements in a collection' do
+  test 'perform() changes all matching elements in a Collection' do
     col = collections(:free_form)
     assert col.items.count > 0
 
@@ -31,7 +31,7 @@ class BatchChangeItemMetadataJobTest < ActiveSupport::TestCase
     end
   end
 
-  test 'perform() should change all matching elements in an ItemSet' do
+  test 'perform() changes all matching elements in an ItemSet' do
     set = item_sets(:one)
     assert set.items.count > 0
 
@@ -58,8 +58,7 @@ class BatchChangeItemMetadataJobTest < ActiveSupport::TestCase
     end
   end
 
-  test 'perform() should change all matching elements in an Enumerable of
-  Items' do
+  test 'perform() changes all matching elements in a Relation of Items' do
     items = collections(:compound_object).items
     assert items.count > 0
 
@@ -75,7 +74,9 @@ class BatchChangeItemMetadataJobTest < ActiveSupport::TestCase
         }
     ]
 
-    BatchChangeItemMetadataJob.perform_now(items, element_name, new_values)
+    BatchChangeItemMetadataJob.perform_now(items.map(&:repository_id),
+                                           element_name,
+                                           new_values)
 
     items.each do |item|
       titles = item.elements.select{ |e| e.name == 'title' }

commit 1d5dd5b848c40546bb4a425bc000b39ca0a1bbad

Fix a bug introduced in 98c5d78809a906e91894c965ee35e71c23205b63

app/controllers/admin/items_controller.rb
@@ -84,7 +84,7 @@ module Admin
       raise ActiveRecord::RecordNotFound unless col
       begin
         relation = editing_item_relation_for(col)
-        BatchChangeItemMetadataJob.perform_later(relation.to_id_a,
+        BatchChangeItemMetadataJob.perform_later(relation.to_a.select(&:present?).map(&:repository_id),
                                                  params[:element].to_s,
                                                  params[:replace_values].map(&:to_unsafe_hash))
       rescue => e