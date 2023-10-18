b03bcbfe7bcca16d0ab2337380086e71c463dfa2
Alex Dolski <alexd@n-ary.net>
2023-10-18 14:40:36 -0500

    Change server host to new VM

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'aws-dls-demo.library.illinois.edu', user: 'dls',
+server 'dls-demo-rocky.library.illinois.edu', user: 'dls',
        roles: %w{web app db}, primary: true
 
 # role-based syntax
