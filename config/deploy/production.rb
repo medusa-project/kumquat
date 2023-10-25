0206f2cdda433977ceed90ed0698597cfe86f10e
Alex Dolski <alexd@n-ary.net>
2023-10-25 16:03:22 -0500

    Update server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'aws-dls-prod.library.illinois.edu', user: 'dls',
+server 'dls-prod-rocky.library.illinois.edu', user: 'dls',
        roles: %w{web app db}, primary: true
 
 # role-based syntax
