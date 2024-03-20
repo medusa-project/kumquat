3b69b2a24e228f75d659b4b92aef229713879368
Gauri Joshi <gauri@illinois.edu>
2024-03-20 10:23:22 -0700

    Add dismiss_banner action inside Landing Controller, that sets session variable and redirects to root

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -6,15 +6,19 @@ class LandingController < WebsiteController
   def index
     authorize(:landing)
     @gateway_item_count = Rails.cache.fetch('gateway.item_count',
-                                            expires_in: 12.hours) do
-      begin
-        GatewayClient.instance.num_items
-      rescue
-        # The gateway is probably down. This is a problem that is better dealt
-        # with elsewhere; failing here should not fail the request.
-        nil
+      expires_in: 12.hours) do
+        begin
+          GatewayClient.instance.num_items
+        rescue
+          # The gateway is probably down. This is a problem that is better dealt
+          # with elsewhere; failing here should not fail the request.
+          nil
+        end
       end
-    end
   end
 
+  def dismiss_banner 
+    session[:dismiss_banner] = true 
+    redirect_to root_path 
+  end
 end
