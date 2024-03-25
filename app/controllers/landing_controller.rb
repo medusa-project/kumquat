8b40c45e5dc51093c0f7be3c7a3ea7cf8d50640a
Gauri Joshi <gauri@illinois.edu>
2024-03-25 14:54:03 -0700

    Remove modified landing controller - not needed

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,18 +1,20 @@
 class LandingController < WebsiteController
+
   ##
   # Responds to GET /
   #
   def index
     authorize(:landing)
     @gateway_item_count = Rails.cache.fetch('gateway.item_count',
-      expires_in: 12.hours) do
-        begin
-          GatewayClient.instance.num_items
-        rescue
-          # The gateway is probably down. This is a problem that is better dealt
-          # with elsewhere; failing here should not fail the request.
-          nil
-        end
+                                            expires_in: 12.hours) do
+      begin
+        GatewayClient.instance.num_items
+      rescue
+        # The gateway is probably down. This is a problem that is better dealt
+        # with elsewhere; failing here should not fail the request.
+        nil
       end
     end
+  end
+
 end
