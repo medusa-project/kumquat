37112b64729b187699fad2102685ad9c556a5034
Gauri Joshi <gauri@illinois.edu>
2024-08-21 11:31:22 -0700

    Contact method controls Kumquat Mailer form delivery

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,6 +3,31 @@ class LandingController < WebsiteController
   ##
   # Responds to GET /
   #
+  #
+  def contact
+    if !check_captcha
+      render plain: "Incorrect math question response.", status: :bad_request
+      return
+    elsif params[:comment]&.blank?
+      render plain: "Please enter a comment.", status: :bad_request
+      return 
+    end
+
+    feedback_email = Setting.string(Setting::Keys::ADMINISTRATOR_EMAIL)
+    begin 
+      KumquatMailer.contact_form_message(page_url: params[:page_url],
+                                          from_name: params[:name],
+                                          from_email: params[:email],
+                                          comment:    params[:comment],
+                                          to_email:  feedback_email).deliver_later
+    rescue => e
+      LOGGER.error("#{e}")
+        render plain: "An error occurred on the server.", status: :internal_server_error 
+    else
+        render plain: "OK"
+    end
+  end 
+
   def index
     authorize(:landing)
     @gateway_item_count = Rails.cache.fetch('gateway.item_count',
