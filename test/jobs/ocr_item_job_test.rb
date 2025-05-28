6b39dc2083af3ff65562a40a34c5de8073dc539a
Gauri Joshi <gauri@illinois.edu>
2025-05-28 14:48:22 -0700

    Test: OcrItemJob returns item.ocred? after performing

--- test/jobs/ocr_item_job_test.rb
+++ test/jobs/ocr_item_job_test.rb
@@ -5,7 +5,15 @@ class OcrItemJobTest < ActiveSupport::TestCase
   # perform()
 
   test 'perform()' do
-    # TODO: write this
+    item = Item.create!(title: "Test Item")
+    Binary.create!(item: item, master_type: Binary::MasterType::ACCESS,
+                            media_type: 'image/png')
+
+    Binary.any_instance.stubs(:detect_text).returns(true)
+    OcrItemJob.perform_now(item: item, language_code: 'eng', include_already_ocred: true)
+
+    item.reload 
+    assert_equal true, item.ocred?
   end
 
 end
