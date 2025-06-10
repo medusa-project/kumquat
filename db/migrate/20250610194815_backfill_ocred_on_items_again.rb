5990bf59acf541ec6fc4d414f8d3ac75c6efe679
Gauri Joshi <gauri@illinois.edu>
2025-06-10 12:51:23 -0700

    Run new backfill migration so the changes are applied when deployed

---
+++ db/migrate/20250610194815_backfill_ocred_on_items_again.rb
@@ -0,0 +1,11 @@
+class BackfillOcredOnItemsAgain < ActiveRecord::Migration[7.1]
+  def up
+    # This migration is a workaround for the fact that some items were not
+    # marked as ocred when they should have been, due to a bug in the OCR jobs.
+    Item.find_each do |item|
+      if item.ocred_binaries.exists?
+        item.update_column(:ocred, true)
+      end
+    end
+  end
+end
