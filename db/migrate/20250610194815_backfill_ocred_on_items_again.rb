f91e2e09eff343badbd3c55aeb41192feeff1a3e
Gauri Joshi <gauri@illinois.edu>
2025-06-10 15:22:28 -0700

    Includes binaries from child items in migration

--- db/migrate/20250610194815_backfill_ocred_on_items_again.rb
+++ db/migrate/20250610194815_backfill_ocred_on_items_again.rb
@@ -3,7 +3,7 @@ class BackfillOcredOnItemsAgain < ActiveRecord::Migration[7.1]
     # This migration is a workaround for the fact that some items were not
     # marked as ocred when they should have been, due to a bug in the OCR jobs.
     Item.find_each do |item|
-      if item.ocred_binaries.exists?
+      if item.ocred_binaries(recursive: true).exists?
         item.update_column(:ocred, true)
       end
     end
