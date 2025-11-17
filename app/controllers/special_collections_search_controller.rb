f9525c6c0fe48f9e49fe938b6cb1e15b4d780040
Gauri Joshi <gauri@illinois.edu>
2025-11-17 15:03:03 -0800

    Controller for special collections search page - needs to become DRY

---
+++ app/controllers/special_collections_search_controller.rb
@@ -0,0 +1,38 @@
+class SpecialCollectionsSearchController < WebsiteController 
+  PERMITTED_PARAMS = [{ fq: [] }, :q, :sort, :start, :utf8, :commit]
+  before_action :set_sanitized_params
+
+
+  def index 
+    @start = [@permitted_params[:start].to_i.abs, max_start].min
+    @limit = window_size
+
+    # Initialize simple search
+    search = SimpleItemSearch.new(query: @permitted_params[:q])
+
+    search.start(@start).limit(@limit)
+
+    @items = search.results
+    @count = search.count
+
+    search.aggregations(true)
+    @facets = search.facets
+
+    @current_page = (@start / @limit) + 1
+    @num_results_shown = [@items.count, @limit].min
+  end
+
+  private 
+
+  def set_sanitized_params
+    @permitted_params = params.permit(PERMITTED_PARAMS)
+  end
+
+  def window_size 
+    40 
+  end
+
+  def max_start 
+    9960 
+  end
+end
