5005cfc4630586c10d30f6c343dabde1248e91bc
Gauri Joshi <gauri@illinois.edu>
2025-11-26 13:25:18 -0800

    Create Advanced Item Search class extending on ItemRelation with hash map of searchable fields as search criteria

---
+++ app/search/advanced_item_search.rb
@@ -0,0 +1,58 @@
+# TODO
+# Provides advanced field-specific search with boolean operators.
+# Extends ItemRelation to work with Kumquat's OpenSearch schema.
+#
+# Usage:
+#   search = AdvancedItemSearch.new(
+#     criteria: [
+#       { field: 'title', query: 'lincoln', operator: 'AND' },
+#       { field: 'subject', query: 'president', operator: 'OR' }
+#     ],
+#     fuzzy: false
+#   )
+#   results = search.results
+#   count = search.count
+#
+class AdvancedItemSearch < ItemRelation
+
+#   # Map user-friendly field names to indexed field names
+  SEARCHABLE_FIELDS = {
+    'all_fields'   => OpensearchIndex::StandardFields::SEARCH_ALL,
+    'title'        => ItemElement.new(name: 'title').indexed_field,
+    'creator'      => ItemElement.new(name: 'creator').indexed_field,
+    'contributor'  => ItemElement.new(name: 'contributor').indexed_field,
+    'subject'      => ItemElement.new(name: 'subject').indexed_field,
+    'description'  => ItemElement.new(name: 'description').indexed_field,
+    'publisher'    => ItemElement.new(name: 'publisher').indexed_field,
+    'date'         => ItemElement.new(name: 'date').indexed_field,
+    'type'         => ItemElement.new(name: 'type').indexed_field,
+    'format'       => ItemElement.new(name: 'format').indexed_field,
+    'identifier'   => ItemElement.new(name: 'identifier').indexed_field,
+    'source'       => ItemElement.new(name: 'source').indexed_field,
+    'language'     => ItemElement.new(name: 'language').indexed_field,
+    'relation'     => ItemElement.new(name: 'relation').indexed_field,
+    'coverage'     => ItemElement.new(name: 'coverage').indexed_field,
+    'rights'       => ItemElement.new(name: 'rights').indexed_field,
+    'full_text'    => Item::IndexFields::FULL_TEXT
+  }.freeze
+
+  attr_reader :criteria
+
+#   ##
+#   # @param criteria [Array<Hash>] Array of search criteria hashes with keys:
+#   #   - field: field name to search (see SEARCHABLE_FIELDS)
+#   #   - query: search term
+#   #   - operator: 'AND' or 'OR' (how to combine with previous criterion)
+#   # TODO: @param fuzzy [Boolean] Enable fuzzy matching (default: false)
+#   # @param published_only [Boolean] Limit to published items (default: true)
+#   # @param accessible_only [Boolean] Limit to publicly accessible items (default: true)
+#   # @param dls_only [Boolean] Limit to DLS collections only (default: true)
+#   #
+  def initialize(criteria:[], published_only: true, accessible_only: true, dls_only: true)
+    super()
+    @criteria = criteria 
+    @published_only = published_only
+    @accessible_only = accessible_only
+    @dls_only = dls_only
+  end
+end
