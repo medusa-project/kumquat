28f1e412b74157eadde4e2695e099548fb109a4c
Gauri Joshi <gauri@illinois.edu>
2025-12-16 23:01:23 -0800

    Instantiate Collection Search from controller

--- app/controllers/special_collections_search_controller.rb
+++ app/controllers/special_collections_search_controller.rb
@@ -11,6 +11,19 @@ class SpecialCollectionsSearchController < WebsiteController
 
     @start = [@permitted_params[:start].to_i.abs, max_start].min 
     @limit = window_size
+
+    search = SimpleCollectionSearch.new(query: @permitted_params[:q])
+    search.facet_filters(@permitted_params[:fq])
+    search.start(@start).limit(@limit)
+
+    @collections = search.results
+    @count = search.count
+
+    search.aggregations(true)
+    @facets = search.facets 
+
+    @current_page = (@start / @limit) + 1
+    @num_results_shown = [@collections.count, @limit].min
   end
 
   private 
