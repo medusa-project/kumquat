b2609680196f03e4b465b87f2febac84bbb03161
Gauri Joshi <gauri@illinois.edu>
2025-12-16 22:49:28 -0800

    Set up special collections search landing page

---
+++ app/controllers/special_collections_search_controller.rb
@@ -0,0 +1,28 @@
+class SpecialCollectionsSearchController < WebsiteController 
+  PERMITTED_PARAMS = [{ fq: [] }, :q, :sort, :start, :utf8, :commit]
+  before_action :set_sanitized_params
+
+  def index 
+    # Redirect to browse all collections if search submitted with empty query 
+    if @permitted_params[:q].blank? && @permitted_params[:commit].present?
+      redirect_to search_landing_path 
+      return 
+    end
+
+    @start = [@permitted_params[:start].to_i.abs, max_start].min 
+    @limit = window_size
+  end
+
+  private 
+  def set_sanitized_params
+    @permitted_params = params.permit(PERMITTED_PARAMS)
+  end
+
+  def window_size 
+    40 
+  end
+
+  def max_start
+    9960
+  end
+end
