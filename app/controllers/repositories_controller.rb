eba34aaacbb0829d36054a896090557f08be74f6
Gauri Joshi <gauri@illinois.edu>
2026-01-23 13:17:03 -0800

    Repoisitories Controller show() retrieves repos matching parameterized title and all public collections from it

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -22,6 +22,31 @@ class RepositoriesController < WebsiteController
   end
 
   def show 
-    
+    begin
+      # Get all repositories to find the one matching the parameterized title
+      collections = Collection.where(public_in_medusa: true, published_in_dls: true)
+                              .where.not(medusa_repository_id: nil)
+      
+      @repository = nil
+      collections.each do |collection|
+        repository = collection.medusa_repository
+        if repository && repository.title.parameterize == params[:id]
+          @repository = repository
+          break
+        end
+      end
+      
+      raise ActiveRecord::RecordNotFound unless @repository
+      
+      # Get all public collections belonging to this repository
+      @collections = Collection.where(medusa_repository_id: @repository.id,
+                                      public_in_medusa: true, 
+                                      published_in_dls: true)
+                               .order('title ASC')
+      
+    rescue => e
+      Rails.logger.error("Repository with title '#{params[:id]}' not found: #{e.message}")
+      raise ActiveRecord::RecordNotFound
+    end
   end
 end
