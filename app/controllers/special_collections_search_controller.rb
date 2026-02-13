c18b2bc2c5bfc249fc6e9020d2ec97abeda1cae1
Gauri Joshi <gauri@illinois.edu>
2026-02-13 10:20:52 -0800

    Set up private method to obtain dls collection and dls item counts

--- app/controllers/special_collections_search_controller.rb
+++ app/controllers/special_collections_search_controller.rb
@@ -43,4 +43,25 @@ class SpecialCollectionsSearchController < WebsiteController
   def max_start
     9960
   end
+
+  def get_total_available_count
+    # Get total count of DLS collections
+    collection_count = Collection.search
+      .aggregations(false)
+      .include_unpublished(false)
+      .include_restricted(false) 
+      .filter(Collection::IndexFields::PUBLISHED_IN_DLS, true)
+      .count
+
+    # Get total count of DLS items
+    item_count = Item.search
+      .aggregations(false)
+      .include_unpublished(false)
+      .include_restricted(false)
+      .include_publicly_inaccessible(false)
+      .filter(Item::IndexFields::PUBLISHED, true)
+      .count
+
+    collection_count + item_count
+  end
 end
