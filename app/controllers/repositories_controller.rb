ed38cb27e206cc5189a925fe1a1965fd1945532c
Gauri Joshi <gauri@illinois.edu>
2026-03-26 14:44:57 -0700

    Set private sanitized params, window size, offset, and response formats

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -83,5 +83,23 @@ class RepositoriesController < WebsiteController
       Rails.logger.error("Repository with title '#{params[:id]}' not found: #{e.message}")
       raise ActiveRecord::RecordNotFound
     end
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  private 
+  def set_sanitized_params
+    @permitted_params = params.permit(PERMITTED_PARAMS)
+  end
+
+  def window_size
+    40 
+  end
+
+  def max_start
+    9960 
   end
 end
