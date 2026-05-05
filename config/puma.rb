7814b82f584ecfedf19fd78e24e4107b74ba7fbb
Gauri Joshi <gauri@illinois.edu>
2026-05-05 08:55:05 -0700

    update path to find and kill stale PID file on new deploy

--- config/puma.rb
+++ config/puma.rb
@@ -17,7 +17,7 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the path to the PID file so stop scripts can find and kill the process.
 #
-pidfile     ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+pidfile     ENV.fetch("PIDFILE") { File.join(ENV.fetch("DLS_HOME", "."), "tmp", "pids", "server.pid") }
 
 # Specifies the `environment` that Puma will run in.
 #
