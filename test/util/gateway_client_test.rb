819fffdc07cc7216a44335234c4bbe1cab5a1869
Gauri Joshi <gauri@illinois.edu>
2026-06-24 08:56:55 -0700

    Test: Stub HTTP call in GatewayClient to avoid 403 forbidden block in CI

--- test/util/gateway_client_test.rb
+++ test/util/gateway_client_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
+require 'mocha/minitest'
 
 class GatewayClientTest < ActiveSupport::TestCase
 
   test 'num_items works' do
-    assert GatewayClient.instance.num_items > 1000000
+    fake_response = stub(body: '{"numResults":2000000}')
+    HTTPClient.any_instance.stubs(:get).returns(fake_response)
+    assert GatewayClient.instance.num_items > 1_000_000
   end
 
 end
