6984dd59259740c028cd9471c73a73da3d03b35c
Gauri Joshi <gauri@illinois.edu>
2026-07-02 15:18:32 -0700

    Add self.matches? for Crawler class bot User-Agent detection Return false for blank User-Agent, otherwise downcase the header and find if it contains any crawler token from the list loaded via self.user_agents

--- app/util/crawler.rb
+++ app/util/crawler.rb
@@ -5,11 +5,23 @@
 class Crawler 
   ## 
   # begin..end block to memoize user_agents array to avoid reloading multiple times
-  # load crawlers.yml content or return an empty array if no file found 
+  # load crawlers.yml content; empty file yields an empty array,
+  # while a missing file raises Errno::ENOENT (fails loudly). This would happen if the file was deleted/moved, 
+  # or not deployed for some reason.
   # iterate over the crawler.yml file entries, convert to string, strip whitespace, downcase and reject blank entries
   def self.user_agents
     @user_agents ||= begin
       entries = YAML.load_file(Rails.root.join('config', 'crawlers.yml')) || []
       entries.map { |user_agent| user_agent.to_s.strip.downcase }.reject(&:blank?)
     end
+  end
+
+  def self.matches?(user_agent)
+    # false for blank user agent
+    return false if user_agent.blank?
+
+    # otherwise downcase the given user agent 
+    # and check if it includes any of the elements in the user_agents array
+    ua = user_agent.downcase 
+    user_agents.any? { | crawler| ua.include?(crawler) }
   end
