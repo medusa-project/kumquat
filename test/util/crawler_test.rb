3d8b0b724e808ffa459772c059a8bf333be52633
Gauri Joshi <gauri@illinois.edu>
2026-07-07 13:21:27 -0700

    Test: stub empty yaml file yields an empty array

--- test/util/crawler_test.rb
+++ test/util/crawler_test.rb
@@ -1,17 +1,33 @@
 require 'test_helper'
+require 'mocha/minitest'
 
 class CrawlerTest < ActiveSupport::TestCase 
 
   test 'user_agents returns non empty array based on crawlers.yml content' do 
     
     crawlers = Crawlers.user_agents
+
     assert_instance_of Array, crawlers
     assert_not_empty crawlers
+  end
 
+  test 'empty file yields empty array' do
+
+    Crawler.instance_variable_set(:@user_agents, nil)
+    YAML.stubs(:load_file).returns(nil)
+  
+    assert_empty Crawler.user_agents
+  ensure
+    Crawler.instance_variable_set(:@user_agents, nil) # memoized nil value is cleared to avoid affecting other tests
+  end
+
+  test 'missing file raises Errno::ENOENT' do
+    #todo
   end
 
   test 'matches? returns false if user agent is blank' do 
-    #todo
+    ua = ''
+    assert false, Crawler.matches?(ua)
   end
 
   test 'matches? returns true if user agent matches any crawler element' do 
